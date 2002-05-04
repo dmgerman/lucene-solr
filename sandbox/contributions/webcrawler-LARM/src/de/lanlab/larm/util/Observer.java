3e5759f25e03ded96348a9b63301e5f34e9f34ab
Otis Gospodnetic <otis@apache.org>
2002-05-04 13:58:45 +0000

    Initial revision
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150751 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: cf2fa142c8bdffd1a90d371483eeb2401e371dfc

---
+++ sandbox/contributions/webcrawler-LARM/src/de/lanlab/larm/util/Observer.java
@@ -0,0 +1,28 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+DECL|package|de.lanlab.larm.util
+package|package
+name|de
+operator|.
+name|lanlab
+operator|.
+name|larm
+operator|.
+name|util
+package|;
+end_package
+
+begin_comment
+comment|/**  *  not used  */
+end_comment
+
+begin_interface
+DECL|interface|Observer
+specifier|public
+interface|interface
+name|Observer
+block|{ }
+end_interface
+
+end_unit
+
