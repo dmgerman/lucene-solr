46898bfb0643b3a294fa96d23517c4ecfb5093f2
cmarschner <cmarschner@noemail>
2002-05-22 23:09:22 +0000

    added license info; added anchor text extraction
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150775 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7a3b5acf374361023979b529d6daf0a5e66e35f1

--- sandbox/contributions/webcrawler-LARM/src/de/lanlab/larm/fetcher/FetcherTaskQueue.java
+++ sandbox/contributions/webcrawler-LARM/src/de/lanlab/larm/fetcher/FetcherTaskQueue.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/* ====================================================================  * The Apache Software License, Version 1.1  *  * Copyright (c) 2001 The Apache Software Foundation.  All rights  * reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  *  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in  *    the documentation and/or other materials provided with the  *    distribution.  *  * 3. The end-user documentation included with the redistribution,  *    if any, must include the following acknowledgment:  *       "This product includes software developed by the  *        Apache Software Foundation (http://www.apache.org/)."  *    Alternately, this acknowledgment may appear in the software itself,  *    if and wherever such third-party acknowledgments normally appear.  *  * 4. The names "Apache" and "Apache Software Foundation" and  *    "Apache Lucene" must not be used to endorse or promote products  *    derived from this software without prior written permission. For  *    written permission, please contact apache@apache.org.  *  * 5. Products derived from this software may not be called "Apache",  *    "Apache Lucene", nor may "Apache" appear in their name, without  *    prior written permission of the Apache Software Foundation.  *  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR  * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF  * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  * ====================================================================  *  * This software consists of voluntary contributions made by many  * individuals on behalf of the Apache Software Foundation.  For more  * information on the Apache Software Foundation, please see  *<http://www.apache.org/>.  */
+end_comment
+
 begin_package
 DECL|package|de.lanlab.larm.fetcher
 package|package
@@ -61,7 +65,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * this special kind of task queue reorders the incoming tasks so that every subsequent  * task is for a different host.  * This is done by a "HashedCircularLinkedList" which allows random adding while  * a differnet thread iterates through the collection circularly.  *  * @author    Clemens Marschner  * @created   23. November 2001  */
+comment|/**  * this special kind of task queue reorders the incoming tasks so that every subsequent  * task is for a different host.  * This is done by a "HashedCircularLinkedList" which allows random adding while  * a differnet thread iterates through the collection circularly.  *  * @author    Clemens Marschner  * @created   23. November 2001  * @version $Id$  */
 end_comment
 
 begin_class
@@ -370,6 +374,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
@@ -393,6 +399,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
@@ -416,6 +424,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
@@ -439,6 +449,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
@@ -462,6 +474,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
@@ -485,6 +499,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
@@ -508,6 +524,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
@@ -731,6 +749,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
@@ -754,6 +774,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
@@ -777,6 +799,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
@@ -895,6 +919,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
@@ -918,6 +944,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
@@ -941,6 +969,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
@@ -1081,6 +1111,8 @@ argument_list|,
 literal|null
 argument_list|,
 literal|false
+argument_list|,
+literal|null
 argument_list|)
 argument_list|)
 argument_list|)
--- sandbox/contributions/webcrawler-LARM/src/de/lanlab/larm/fetcher/URLMessage.java
+++ sandbox/contributions/webcrawler-LARM/src/de/lanlab/larm/fetcher/URLMessage.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/* ====================================================================  * The Apache Software License, Version 1.1  *  * Copyright (c) 2001 The Apache Software Foundation.  All rights  * reserved.  *  * Redistribution and use in source and binary forms, with or without  * modification, are permitted provided that the following conditions  * are met:  *  * 1. Redistributions of source code must retain the above copyright  *    notice, this list of conditions and the following disclaimer.  *  * 2. Redistributions in binary form must reproduce the above copyright  *    notice, this list of conditions and the following disclaimer in  *    the documentation and/or other materials provided with the  *    distribution.  *  * 3. The end-user documentation included with the redistribution,  *    if any, must include the following acknowledgment:  *       "This product includes software developed by the  *        Apache Software Foundation (http://www.apache.org/)."  *    Alternately, this acknowledgment may appear in the software itself,  *    if and wherever such third-party acknowledgments normally appear.  *  * 4. The names "Apache" and "Apache Software Foundation" and  *    "Apache Lucene" must not be used to endorse or promote products  *    derived from this software without prior written permission. For  *    written permission, please contact apache@apache.org.  *  * 5. Products derived from this software may not be called "Apache",  *    "Apache Lucene", nor may "Apache" appear in their name, without  *    prior written permission of the Apache Software Foundation.  *  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR  * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF  * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF  * SUCH DAMAGE.  * ====================================================================  *  * This software consists of voluntary contributions made by many  * individuals on behalf of the Apache Software Foundation.  For more  * information on the Apache Software Foundation, please see  *<http://www.apache.org/>.  */
+end_comment
+
 begin_package
 DECL|package|de.lanlab.larm.fetcher
 package|package
@@ -47,7 +51,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * represents a URL which is passed around in the messageHandler  */
+comment|/**  * represents a URL which is passed around in the messageHandler  * @version $Id$  */
 end_comment
 
 begin_class
@@ -85,6 +89,11 @@ DECL|field|isFrame
 name|boolean
 name|isFrame
 decl_stmt|;
+DECL|field|anchor
+specifier|protected
+name|String
+name|anchor
+decl_stmt|;
 DECL|method|URLMessage
 specifier|public
 name|URLMessage
@@ -97,6 +106,9 @@ name|referer
 parameter_list|,
 name|boolean
 name|isFrame
+parameter_list|,
+name|String
+name|anchor
 parameter_list|)
 block|{
 comment|//super();
@@ -151,6 +163,18 @@ operator|.
 name|isFrame
 operator|=
 name|isFrame
+expr_stmt|;
+name|this
+operator|.
+name|anchor
+operator|=
+name|anchor
+operator|!=
+literal|null
+condition|?
+name|anchor
+else|:
+literal|""
 expr_stmt|;
 comment|//System.out.println("" + refererString + " -> " + urlString);
 block|}
@@ -208,6 +232,16 @@ return|return
 name|refererString
 return|;
 block|}
+DECL|method|getAnchor
+specifier|public
+name|String
+name|getAnchor
+parameter_list|()
+block|{
+return|return
+name|anchor
+return|;
+block|}
 DECL|method|hashCode
 specifier|public
 name|int
@@ -255,6 +289,13 @@ operator|.
 name|writeBoolean
 argument_list|(
 name|isFrame
+argument_list|)
+expr_stmt|;
+name|out
+operator|.
+name|writeUTF
+argument_list|(
+name|anchor
 argument_list|)
 expr_stmt|;
 block|}
@@ -316,6 +357,13 @@ operator|.
 name|readBoolean
 argument_list|()
 expr_stmt|;
+name|anchor
+operator|=
+name|in
+operator|.
+name|readUTF
+argument_list|()
+expr_stmt|;
 block|}
 DECL|method|getInfo
 specifier|public
@@ -347,6 +395,10 @@ literal|"1"
 else|:
 literal|"0"
 operator|)
+operator|+
+literal|"\t"
+operator|+
+name|anchor
 return|;
 block|}
 block|}
