5e5143bb56013a7d3db8a728ba26486c9c30fc4c
Otis Gospodnetic <otis@apache.org>
2002-09-20 02:21:31 +0000

    - Reverted back to the previous revision (1.1).
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@149858 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 474d9de9858f7c20bcdc2334c4e0b7788b54f385

--- src/java/org/apache/lucene/index/FieldsReader.java
+++ src/java/org/apache/lucene/index/FieldsReader.java
@@ -102,10 +102,6 @@ name|Field
 import|;
 end_import
 
-begin_comment
-comment|/**  * FIXME: Describe class<code>FieldsReader</code> here.  *  * @version $Id$  */
-end_comment
-
 begin_class
 DECL|class|FieldsReader
 specifier|final
@@ -173,7 +169,6 @@ operator|+
 literal|".fdx"
 argument_list|)
 expr_stmt|;
-comment|// TODO: document the magic number 8
 name|size
 operator|=
 operator|(
@@ -227,7 +222,6 @@ parameter_list|)
 throws|throws
 name|IOException
 block|{
-comment|// TODO: document the magic number 8L
 name|indexStream
 operator|.
 name|seek
