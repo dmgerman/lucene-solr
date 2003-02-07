154e631fcfa742426f31c31dab797c225405237f
Doug Cutting <cutting@apache.org>
2003-02-07 18:45:16 +0000

    Fixed a bug with prohibited clauses.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@149945 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7248b18b1d187f4ebdd3fc2cec0fbc17f75c3e32

--- src/java/org/apache/lucene/search/BooleanQuery.java
+++ src/java/org/apache/lucene/search/BooleanQuery.java
@@ -474,7 +474,7 @@ name|clauses
 operator|.
 name|elementAt
 argument_list|(
-literal|0
+name|i
 argument_list|)
 decl_stmt|;
 name|Weight
@@ -607,7 +607,7 @@ name|clauses
 operator|.
 name|elementAt
 argument_list|(
-literal|0
+name|i
 argument_list|)
 decl_stmt|;
 name|Weight
