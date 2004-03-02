0c20c9f1cc66e63a256876ad29eaa70ea14901c3
Otis Gospodnetic <otis@apache.org>
2004-03-02 14:54:55 +0000

    - Applied patch for bug 26702, attachment 10637
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150220 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 17488d5e0844cacc31732897d0cf48d3663b6de9

--- src/java/org/apache/lucene/search/FieldDoc.java
+++ src/java/org/apache/lucene/search/FieldDoc.java
@@ -17,7 +17,7 @@ comment|/**  * Copyright 2004 The Apache Software Foundation  *  * Licensed unde
 end_comment
 
 begin_comment
-comment|/**  * Expert: A ScoreDoc which also contains information about  * how to sort the referenced document.  *  *<p>Created: Feb 11, 2004 1:23:38 PM   *   * @author  Tim Jones (Nacimiento Software)  * @since   lucene 1.4  * @version $Id$  * @see TopFieldDocs  */
+comment|/**  * Expert: A ScoreDoc which also contains information about  * how to sort the referenced document.  In addition to the  * document number and score, this object contains an array  * of values for the document from the field(s) used to sort.  * For example, if the sort criteria was to sort by fields  * "a", "b" then "c", the<code>fields</code> object array  * will have three elements, corresponding respectively to  * the term values for the document in fields "a", "b" and "c".  * The class of each element in the array will be either  * Integer, Float or String depending on the type of values  * in the terms of each field.  *  *<p>Created: Feb 11, 2004 1:23:38 PM  *  * @author  Tim Jones (Nacimiento Software)  * @since   lucene 1.4  * @version $Id$  * @see ScoreDoc  * @see TopFieldDocs  */
 end_comment
 
 begin_class
@@ -28,14 +28,14 @@ name|FieldDoc
 extends|extends
 name|ScoreDoc
 block|{
-comment|/** The values which are used to sort the referenced document. 	 * The order of these will match the original sort criteria given by an 	 * Sort object. 	 * @see Sort 	 * @see Searchable#search(Query,Filter,int,Sort) 	 */
+comment|/** Expert: The values which are used to sort the referenced document. 	 * The order of these will match the original sort criteria given by a 	 * Sort object.  Each Object will be either an Integer, Float or String, 	 * depending on the type of values in the terms of the original field. 	 * @see Sort 	 * @see Searchable#search(Query,Filter,int,Sort) 	 */
 DECL|field|fields
 specifier|public
 name|Object
 index|[]
 name|fields
 decl_stmt|;
-comment|/** Creates one of these objects with empty sort information. */
+comment|/** Expert: Creates one of these objects with empty sort information. */
 DECL|method|FieldDoc
 specifier|public
 name|FieldDoc
@@ -55,7 +55,7 @@ name|score
 argument_list|)
 expr_stmt|;
 block|}
-comment|/** Creates one of these objects with the given sort information. */
+comment|/** Expert: Creates one of these objects with the given sort information. */
 DECL|method|FieldDoc
 specifier|public
 name|FieldDoc
