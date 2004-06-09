65ef3bc09cabc128c499dee9591a521515e60fa1
Doug Cutting <cutting@apache.org>
2004-06-09 11:28:46 +0000

    Fixed a bug where obsolete segment files were not deleted on Win32.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150362 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5ca4cb27e26448af68123db2e293e15e758c4759

--- src/java/org/apache/lucene/index/IndexWriter.java
+++ src/java/org/apache/lucene/index/IndexWriter.java
@@ -1779,6 +1779,12 @@ name|directory
 argument_list|)
 argument_list|)
 expr_stmt|;
+comment|// close readers before we attempt to delete now-obsolete segments
+name|merger
+operator|.
+name|closeReaders
+argument_list|()
+expr_stmt|;
 synchronized|synchronized
 init|(
 name|directory
@@ -1833,11 +1839,6 @@ name|run
 argument_list|()
 expr_stmt|;
 block|}
-name|merger
-operator|.
-name|closeReaders
-argument_list|()
-expr_stmt|;
 block|}
 comment|/* Some operating systems (e.g. Windows) don't permit a file to be deleted      while it is opened for read (e.g. by another process or thread).  So we      assume that when a delete fails it is because the file is open in another      process, and queue the file for subsequent deletion. */
 DECL|method|deleteSegments
