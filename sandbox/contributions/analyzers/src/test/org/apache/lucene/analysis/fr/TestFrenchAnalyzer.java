a654bfdddb4fbd9c0da5d45ff978df704e39e99f
Daniel Naber <dnaber@apache.org>
2004-08-17 22:03:07 +0000

    convert to utf-8
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@151002 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1b5eb6c40c83f8592d8f39a10a98ea755c21205c

--- sandbox/contributions/analyzers/src/test/org/apache/lucene/analysis/fr/TestFrenchAnalyzer.java
+++ sandbox/contributions/analyzers/src/test/org/apache/lucene/analysis/fr/TestFrenchAnalyzer.java
@@ -91,7 +91,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Test case for FrenchAnalyzer.  *  * @author    Jean-François Halleux  * @version   $version$  */
+comment|/**  * Test case for FrenchAnalyzer.  *  * @author    Jean-FranÃ§ois Halleux  * @version   $version$  */
 end_comment
 
 begin_class
@@ -393,7 +393,7 @@ name|assertAnalyzesTo
 argument_list|(
 name|fa
 argument_list|,
-literal|"Jean-François"
+literal|"Jean-FranÃ§ois"
 argument_list|,
 operator|new
 name|String
@@ -401,7 +401,7 @@ index|[]
 block|{
 literal|"jean"
 block|,
-literal|"françois"
+literal|"franÃ§ois"
 block|}
 argument_list|)
 expr_stmt|;
@@ -410,7 +410,7 @@ name|assertAnalyzesTo
 argument_list|(
 name|fa
 argument_list|,
-literal|"le la chien les aux chat du des à cheval"
+literal|"le la chien les aux chat du des Ã  cheval"
 argument_list|,
 operator|new
 name|String
@@ -429,7 +429,7 @@ name|assertAnalyzesTo
 argument_list|(
 name|fa
 argument_list|,
-literal|"lances chismes habitable chiste éléments captifs"
+literal|"lances chismes habitable chiste Ã©lÃ©ments captifs"
 argument_list|,
 operator|new
 name|String
@@ -443,7 +443,7 @@ literal|"habit"
 block|,
 literal|"chist"
 block|,
-literal|"élément"
+literal|"Ã©lÃ©ment"
 block|,
 literal|"captif"
 block|}
@@ -474,7 +474,7 @@ name|assertAnalyzesTo
 argument_list|(
 name|fa
 argument_list|,
-literal|"C3PO aujourd'hui oeuf ïâöûàä anticonstitutionnellement Java++ "
+literal|"C3PO aujourd'hui oeuf Ã¯Ã¢Ã¶Ã»Ã Ã¤ anticonstitutionnellement Java++ "
 argument_list|,
 operator|new
 name|String
@@ -486,7 +486,7 @@ literal|"aujourd'hui"
 block|,
 literal|"oeuf"
 block|,
-literal|"ïâöûàä"
+literal|"Ã¯Ã¢Ã¶Ã»Ã Ã¤"
 block|,
 literal|"anticonstitutionnel"
 block|,
