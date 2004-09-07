927fa7a97f6e9c570eeda9520b2295495c248502
Daniel Naber <dnaber@apache.org>
2004-09-07 18:06:16 +0000

    Remove all "final" declarations from FSDirectory. Doug agreed and nobody objected, so I commit it. Submitted by: Mike Hearn
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150496 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3305cee1b6ccb1740a98dba31104e87a50f0a326

--- src/java/org/apache/lucene/store/FSDirectory.java
+++ src/java/org/apache/lucene/store/FSDirectory.java
@@ -117,7 +117,6 @@ end_comment
 begin_class
 DECL|class|FSDirectory
 specifier|public
-specifier|final
 class|class
 name|FSDirectory
 extends|extends
@@ -620,7 +619,6 @@ block|}
 comment|/** Returns an array of strings, one for each file in the directory. */
 DECL|method|list
 specifier|public
-specifier|final
 name|String
 index|[]
 name|list
@@ -636,7 +634,6 @@ block|}
 comment|/** Returns true iff a file with the given name exists. */
 DECL|method|fileExists
 specifier|public
-specifier|final
 name|boolean
 name|fileExists
 parameter_list|(
@@ -665,7 +662,6 @@ block|}
 comment|/** Returns the time the named file was last modified. */
 DECL|method|fileModified
 specifier|public
-specifier|final
 name|long
 name|fileModified
 parameter_list|(
@@ -695,7 +691,6 @@ comment|/** Returns the time the named file was last modified. */
 DECL|method|fileModified
 specifier|public
 specifier|static
-specifier|final
 name|long
 name|fileModified
 parameter_list|(
@@ -759,7 +754,6 @@ block|}
 comment|/** Returns the length in bytes of a file in the directory. */
 DECL|method|fileLength
 specifier|public
-specifier|final
 name|long
 name|fileLength
 parameter_list|(
@@ -788,7 +782,6 @@ block|}
 comment|/** Removes an existing file in the directory. */
 DECL|method|deleteFile
 specifier|public
-specifier|final
 name|void
 name|deleteFile
 parameter_list|(
@@ -830,7 +823,6 @@ block|}
 comment|/** Renames an existing file in the directory. */
 DECL|method|renameFile
 specifier|public
-specifier|final
 specifier|synchronized
 name|void
 name|renameFile
@@ -1096,7 +1088,6 @@ block|}
 comment|/** Creates a new, empty file in the directory with the given name.       Returns a stream writing this file. */
 DECL|method|createFile
 specifier|public
-specifier|final
 name|OutputStream
 name|createFile
 parameter_list|(
@@ -1123,7 +1114,6 @@ block|}
 comment|/** Returns a stream reading an existing file. */
 DECL|method|openFile
 specifier|public
-specifier|final
 name|InputStream
 name|openFile
 parameter_list|(
@@ -1193,7 +1183,6 @@ decl_stmt|;
 comment|/** Constructs a {@link Lock} with the specified name.  Locks are implemented    * with {@link File#createNewFile() }.    *    *<p>In JDK 1.1 or if system property<I>disableLuceneLocks</I> is the    * string "true", locks are disabled.  Assigning this property any other    * string will<B>not</B> prevent creation of lock files.  This is useful for    * using Lucene on read-only medium, such as CD-ROM.    *    * @param name the name of the lock file    * @return an instance of<code>Lock</code> holding the lock    */
 DECL|method|makeLock
 specifier|public
-specifier|final
 name|Lock
 name|makeLock
 parameter_list|(
@@ -1475,7 +1464,6 @@ block|}
 comment|/** Closes the store to future operations. */
 DECL|method|close
 specifier|public
-specifier|final
 specifier|synchronized
 name|void
 name|close
@@ -1532,7 +1520,6 @@ end_class
 
 begin_class
 DECL|class|FSInputStream
-specifier|final
 class|class
 name|FSInputStream
 extends|extends
@@ -1636,7 +1623,6 @@ block|}
 comment|/** InputStream methods */
 DECL|method|readInternal
 specifier|protected
-specifier|final
 name|void
 name|readInternal
 parameter_list|(
@@ -1748,7 +1734,6 @@ block|}
 block|}
 DECL|method|close
 specifier|public
-specifier|final
 name|void
 name|close
 parameter_list|()
@@ -1769,7 +1754,6 @@ block|}
 comment|/** Random-access methods */
 DECL|method|seekInternal
 specifier|protected
-specifier|final
 name|void
 name|seekInternal
 parameter_list|(
@@ -1779,7 +1763,6 @@ parameter_list|)
 block|{   }
 DECL|method|finalize
 specifier|protected
-specifier|final
 name|void
 name|finalize
 parameter_list|()
@@ -1841,7 +1824,6 @@ end_class
 
 begin_class
 DECL|class|FSOutputStream
-specifier|final
 class|class
 name|FSOutputStream
 extends|extends
@@ -1877,7 +1859,6 @@ block|}
 comment|/** output methods: */
 DECL|method|flushBuffer
 specifier|public
-specifier|final
 name|void
 name|flushBuffer
 parameter_list|(
@@ -1905,7 +1886,6 @@ expr_stmt|;
 block|}
 DECL|method|close
 specifier|public
-specifier|final
 name|void
 name|close
 parameter_list|()
@@ -1926,7 +1906,6 @@ block|}
 comment|/** Random-access methods */
 DECL|method|seek
 specifier|public
-specifier|final
 name|void
 name|seek
 parameter_list|(
@@ -1953,7 +1932,6 @@ expr_stmt|;
 block|}
 DECL|method|length
 specifier|public
-specifier|final
 name|long
 name|length
 parameter_list|()
@@ -1969,7 +1947,6 @@ return|;
 block|}
 DECL|method|finalize
 specifier|protected
-specifier|final
 name|void
 name|finalize
 parameter_list|()
