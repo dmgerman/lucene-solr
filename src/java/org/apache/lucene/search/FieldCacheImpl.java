f1c1bd9a1c3b726ebd027d172eb599a483c9d30d
Doug Cutting <cutting@apache.org>
2004-09-23 16:42:44 +0000

    Add an import so that GCJ can compile this.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@150532 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a69db22983ddc60bb374d0f1b0c463e4af468f87

--- src/java/org/apache/lucene/search/FieldCacheImpl.java
+++ src/java/org/apache/lucene/search/FieldCacheImpl.java
@@ -74,6 +74,22 @@ end_import
 
 begin_import
 import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|search
+operator|.
+name|FieldCache
+operator|.
+name|StringIndex
+import|;
+end_import
+
+begin_import
+import|import
 name|java
 operator|.
 name|io
