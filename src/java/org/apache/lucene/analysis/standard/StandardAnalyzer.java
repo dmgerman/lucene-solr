219a926918a7ad83b7e95c29894927a40d515b45
Daniel Naber <dnaber@apache.org>
2005-06-22 19:59:03 +0000

    small javadoc improvements
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@192989 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ca5b0a3ca038c44273aae63b54abc09d1a6f558f

--- src/java/org/apache/lucene/analysis/standard/StandardAnalyzer.java
+++ src/java/org/apache/lucene/analysis/standard/StandardAnalyzer.java
@@ -134,7 +134,7 @@ name|stopWords
 argument_list|)
 expr_stmt|;
 block|}
-comment|/** Builds an analyzer with the stop words from the given file. */
+comment|/** Builds an analyzer with the stop words from the given file.    * @see WordlistLoader#getWordSet(File)    */
 DECL|method|StandardAnalyzer
 specifier|public
 name|StandardAnalyzer
@@ -155,6 +155,7 @@ name|stopwords
 argument_list|)
 expr_stmt|;
 block|}
+comment|/** Builds an analyzer with the stop words from the given reader.    * @see WordlistLoader#getWordSet(Reader)    */
 DECL|method|StandardAnalyzer
 specifier|public
 name|StandardAnalyzer
