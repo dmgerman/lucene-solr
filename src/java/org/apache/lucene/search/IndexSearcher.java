d5ef735805772a283ef8d285266bc8628c7d98ff
Daniel Naber <dnaber@apache.org>
2005-10-05 20:14:56 +0000

    small javadoc improvements
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@295101 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 10fb3c64a59e592c73b2f6d3a91b85ebbd7f3e69

--- src/java/org/apache/lucene/search/IndexSearcher.java
+++ src/java/org/apache/lucene/search/IndexSearcher.java
@@ -93,7 +93,7 @@ import|;
 end_import
 
 begin_comment
-comment|/** Implements search over a single IndexReader.  *  *<p>Applications usually need only call the inherited {@link #search(Query)}  * or {@link #search(Query,Filter)} methods. For performance reasons it is   * recommended to open only one IndexSearcher and use it for all of your searches.  */
+comment|/** Implements search over a single IndexReader.  *  *<p>Applications usually need only call the inherited {@link #search(Query)}  * or {@link #search(Query,Filter)} methods. For performance reasons it is   * recommended to open only one IndexSearcher and use it for all of your searches.  *   *<p>Note that you can only access Hits from an IndexSearcher as long as it is  * not yet closed, otherwise an IOException will be thrown.   */
 end_comment
 
 begin_class
