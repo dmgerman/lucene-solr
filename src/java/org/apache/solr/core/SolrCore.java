917844fe4acecde29fb2dcd475497e1881e71c61
Yonik Seeley <yonik@apache.org>
2006-02-13 20:04:09 +0000

    remove unused try/finally
    git-svn-id: https://svn.apache.org/repos/asf/incubator/solr/trunk@377478 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ba5aa8eba0a0cad184f826bc70b1dc209481a008

--- src/java/org/apache/solr/core/SolrCore.java
+++ src/java/org/apache/solr/core/SolrCore.java
@@ -1184,8 +1184,6 @@ operator|=
 name|this
 expr_stmt|;
 comment|// set singleton
-try|try
-block|{
 if|if
 condition|(
 name|dataDir
@@ -1309,9 +1307,6 @@ name|e
 argument_list|)
 throw|;
 block|}
-block|}
-finally|finally
-block|{         }
 block|}
 block|}
 DECL|method|close
