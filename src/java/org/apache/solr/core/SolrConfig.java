490ff041309406c40be13260382d2dc7ee82403b
Yonik Seeley <yonik@apache.org>
2006-03-16 14:56:45 +0000

    move default configuration dir from ./conf to ./solrconf
    git-svn-id: https://svn.apache.org/repos/asf/incubator/solr/trunk@386349 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0f9bcba8ad2d67b7666a7152018eb1acd9109b1f

--- src/java/org/apache/solr/core/SolrConfig.java
+++ src/java/org/apache/solr/core/SolrConfig.java
@@ -44,7 +44,7 @@ name|config
 decl_stmt|;
 static|static
 block|{
-name|Exception
+name|RuntimeException
 name|e
 init|=
 literal|null
@@ -73,7 +73,7 @@ expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
-name|Exception
+name|RuntimeException
 name|ee
 parameter_list|)
 block|{
@@ -103,15 +103,12 @@ parameter_list|(
 name|Exception
 name|eee
 parameter_list|)
-block|{}
-block|}
-if|if
-condition|(
-name|is
-operator|!=
-literal|null
-condition|)
 block|{
+throw|throw
+name|e
+throw|;
+block|}
+block|}
 try|try
 block|{
 name|config
@@ -142,6 +139,8 @@ throw|throw
 operator|new
 name|RuntimeException
 argument_list|(
+literal|"Error in solrconfig.xml"
+argument_list|,
 name|ee
 argument_list|)
 throw|;
@@ -155,19 +154,6 @@ argument_list|(
 literal|"Loaded Config solrconfig.xml"
 argument_list|)
 expr_stmt|;
-block|}
-else|else
-block|{
-throw|throw
-operator|new
-name|RuntimeException
-argument_list|(
-literal|"Can't find Solr config file ./conf/solrconfig.xml"
-argument_list|,
-name|e
-argument_list|)
-throw|;
-block|}
 block|}
 block|}
 end_class
