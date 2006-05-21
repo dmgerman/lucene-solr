c81cc5b76cd228ffded9936ead4cf33ad9428a09
Yonik Seeley <yonik@apache.org>
2006-05-21 21:55:12 +0000

    enable multiple solr webapps with JNDI config
    git-svn-id: https://svn.apache.org/repos/asf/incubator/solr/trunk@408501 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1a66aa3853b58a39b17bafa4239c6b6978fbbf62

--- src/webapp/src/org/apache/solr/servlet/SolrUpdateServlet.java
+++ src/webapp/src/org/apache/solr/servlet/SolrUpdateServlet.java
@@ -222,6 +222,13 @@ operator|.
 name|getSolrCore
 argument_list|()
 expr_stmt|;
+name|log
+operator|.
+name|info
+argument_list|(
+literal|"SolrUpdateServlet.init() done"
+argument_list|)
+expr_stmt|;
 block|}
 DECL|method|doPost
 specifier|public
