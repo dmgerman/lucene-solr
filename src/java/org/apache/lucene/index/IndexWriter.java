3ac5a7e557f7ddf224c3e3126061f8bb681e8510
Yonik Seeley <yonik@apache.org>
2006-08-18 13:33:16 +0000

    move singleDocSegmentsCount maintinence to mergeSegments
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@432580 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: abb1bfc29021a82d20c0f0feacb3badbbe6d0c2a

--- src/java/org/apache/lucene/index/IndexWriter.java
+++ src/java/org/apache/lucene/index/IndexWriter.java
@@ -2069,7 +2069,6 @@ name|mergeDocs
 operator|>=
 name|targetMergeDocs
 condition|)
-block|{
 comment|// found a merge to do
 name|mergeSegments
 argument_list|(
@@ -2078,15 +2077,8 @@ operator|+
 literal|1
 argument_list|)
 expr_stmt|;
-name|singleDocSegmentsCount
-operator|=
-literal|0
-expr_stmt|;
-block|}
 else|else
-block|{
 break|break;
-block|}
 name|targetMergeDocs
 operator|*=
 name|mergeFactor
@@ -2273,6 +2265,31 @@ name|reader
 argument_list|)
 expr_stmt|;
 comment|// queue segment for deletion
+block|}
+comment|// update 1-doc segments counter accordin to range of merged segments
+if|if
+condition|(
+name|singleDocSegmentsCount
+operator|>
+literal|0
+condition|)
+block|{
+name|singleDocSegmentsCount
+operator|=
+name|Math
+operator|.
+name|min
+argument_list|(
+name|singleDocSegmentsCount
+argument_list|,
+name|segmentInfos
+operator|.
+name|size
+argument_list|()
+operator|-
+name|end
+argument_list|)
+expr_stmt|;
 block|}
 name|int
 name|mergedDocCount
