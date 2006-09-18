f0a9cb01a5ed2a54345169e77c24607372764987
Yonik Seeley <yonik@apache.org>
2006-09-18 21:26:02 +0000

    retain zero size segments resulting from a merge: LUCENE-672
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@447570 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b1322d5acd9a4e5dcc10565f71a756795bcbc026

--- src/java/org/apache/lucene/index/IndexWriter.java
+++ src/java/org/apache/lucene/index/IndexWriter.java
@@ -2040,8 +2040,6 @@ operator|>
 literal|0
 condition|)
 block|{
-if|if
-condition|(
 name|mergeSegments
 argument_list|(
 name|ramSegmentInfos
@@ -2053,14 +2051,10 @@ operator|.
 name|size
 argument_list|()
 argument_list|)
-operator|>
-literal|0
-condition|)
-block|{
+expr_stmt|;
 name|maybeMergeSegments
 argument_list|()
 expr_stmt|;
-block|}
 block|}
 block|}
 comment|/** Incremental segment merger.  */
@@ -2076,7 +2070,8 @@ block|{
 name|long
 name|lowerBound
 init|=
-literal|0
+operator|-
+literal|1
 decl_stmt|;
 name|long
 name|upperBound
@@ -2238,13 +2233,7 @@ operator|=
 literal|true
 expr_stmt|;
 block|}
-elseif|else
-if|if
-condition|(
-name|docCount
-operator|>
-literal|0
-condition|)
+else|else
 block|{
 comment|// if the merged segment does not exceed upperBound, consider
 comment|// this segment for further merges on this same level
@@ -2322,11 +2311,6 @@ name|this
 argument_list|,
 name|mergedName
 argument_list|)
-decl_stmt|;
-name|boolean
-name|fromRAM
-init|=
-literal|false
 decl_stmt|;
 specifier|final
 name|Vector
@@ -2436,28 +2420,6 @@ name|reader
 argument_list|)
 expr_stmt|;
 comment|// queue segment for deletion
-if|if
-condition|(
-operator|!
-name|fromRAM
-operator|&&
-operator|(
-name|reader
-operator|.
-name|directory
-argument_list|()
-operator|==
-name|this
-operator|.
-name|ramDirectory
-operator|)
-condition|)
-block|{
-name|fromRAM
-operator|=
-literal|true
-expr_stmt|;
-block|}
 block|}
 name|int
 name|mergedDocCount
@@ -2505,7 +2467,9 @@ argument_list|)
 decl_stmt|;
 if|if
 condition|(
-name|fromRAM
+name|sourceSegments
+operator|==
+name|ramSegmentInfos
 condition|)
 block|{
 name|sourceSegments
@@ -2513,12 +2477,6 @@ operator|.
 name|removeAllElements
 argument_list|()
 expr_stmt|;
-if|if
-condition|(
-name|mergedDocCount
-operator|>
-literal|0
-condition|)
 name|segmentInfos
 operator|.
 name|addElement
@@ -2553,12 +2511,6 @@ argument_list|(
 name|i
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|mergedDocCount
-operator|>
-literal|0
-condition|)
 name|segmentInfos
 operator|.
 name|set
@@ -2566,14 +2518,6 @@ argument_list|(
 name|minSegment
 argument_list|,
 name|newSegment
-argument_list|)
-expr_stmt|;
-else|else
-name|sourceSegments
-operator|.
-name|remove
-argument_list|(
-name|minSegment
 argument_list|)
 expr_stmt|;
 block|}
