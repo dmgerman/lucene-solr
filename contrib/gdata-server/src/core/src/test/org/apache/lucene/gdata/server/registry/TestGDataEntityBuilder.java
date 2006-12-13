f98aaf3486a67436a91e54568ac05a82fe5b2c38
Simon Willnauer <simonw@apache.org>
2006-12-13 19:36:49 +0000

    fixed some path errors due to the new structure
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@486811 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ad3ccde3a067c7ebf3d051d249d628c629eea3f4

--- contrib/gdata-server/src/core/src/test/org/apache/lucene/gdata/server/registry/TestGDataEntityBuilder.java
+++ contrib/gdata-server/src/core/src/test/org/apache/lucene/gdata/server/registry/TestGDataEntityBuilder.java
@@ -237,7 +237,7 @@ init|=
 operator|new
 name|File
 argument_list|(
-literal|"src/test/org/apache/lucene/gdata/server/registry/TestEntityBuilderIncomingFeed.xml"
+literal|"src/core/src/test/org/apache/lucene/gdata/server/registry/TestEntityBuilderIncomingFeed.xml"
 argument_list|)
 decl_stmt|;
 DECL|field|incomingEntry
@@ -249,7 +249,7 @@ init|=
 operator|new
 name|File
 argument_list|(
-literal|"src/test/org/apache/lucene/gdata/server/registry/TestEntityBuilderIncomingEntry.xml"
+literal|"src/core/src/test/org/apache/lucene/gdata/server/registry/TestEntityBuilderIncomingEntry.xml"
 argument_list|)
 decl_stmt|;
 DECL|field|feedTitleFromXML
