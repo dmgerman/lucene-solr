84196ea7e6f56dc30f312df2b414792ab0deda22
Bertrand Delacretaz <bdelacretaz@apache.org>
2007-01-15 10:59:46 +0000

    SolrConfig says 'system property solr.solr.home not set' in the log, when using default Solr home
    git-svn-id: https://svn.apache.org/repos/asf/incubator/solr/trunk@496274 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9737c076ae6911207bb5fe077a182e4737b4ebe3

--- src/java/org/apache/solr/core/Config.java
+++ src/java/org/apache/solr/core/Config.java
@@ -1405,7 +1405,11 @@ literal|"Solr home defaulted to '"
 operator|+
 name|instanceDir
 operator|+
-literal|"'"
+literal|"' (system property "
+operator|+
+name|prop
+operator|+
+literal|" not set)"
 argument_list|)
 expr_stmt|;
 block|}
