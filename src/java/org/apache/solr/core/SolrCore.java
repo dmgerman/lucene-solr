d9f48a924b004cc8bc381a56e6b6a8f502bfc93b
Yonik Seeley <yonik@apache.org>
2007-02-18 03:33:13 +0000

    make solr more easily embeddable: SOLR-149
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@508859 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 28d75b04bc6abfa2b450d78380fa27ba588545a2

--- src/java/org/apache/solr/core/SolrCore.java
+++ src/java/org/apache/solr/core/SolrCore.java
@@ -2582,6 +2582,18 @@ name|info
 argument_list|(
 name|req
 operator|.
+name|getContext
+argument_list|()
+operator|.
+name|get
+argument_list|(
+literal|"path"
+argument_list|)
+operator|+
+literal|" "
+operator|+
+name|req
+operator|.
 name|getParamString
 argument_list|()
 operator|+
