98a889d4ed235ba2cb2b7ce020e4b3a0eb17bd0d
Grant Ingersoll <gsingers@apache.org>
2007-03-05 14:28:01 +0000

    https://issues.apache.org/jira/browse/LUCENE-822
    Applied Mark Miller's patch.  Added Unit tests to TestRemoteSearchable and TestMultiSearcher.  To get RemoteSearchable to work, had to make FieldSelector and FieldSelectorResult serializable.
    
    All tests pass.
    
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@514675 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 83efa50f5ed6614873b5c4508428921ee1fb7621

--- src/java/org/apache/lucene/document/FieldSelectorResult.java
+++ src/java/org/apache/lucene/document/FieldSelectorResult.java
@@ -12,6 +12,16 @@ name|document
 package|;
 end_package
 
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|Serializable
+import|;
+end_import
+
 begin_comment
 comment|/**  * Copyright 2004 The Apache Software Foundation  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
@@ -30,10 +40,13 @@ specifier|public
 specifier|final
 class|class
 name|FieldSelectorResult
+implements|implements
+name|Serializable
 block|{
 comment|/**      * Load this {@link Field} every time the {@link Document} is loaded, reading in the data as it is encounterd.      *  {@link Document#getField(String)} and {@link Document#getFieldable(String)} should not return null.      *<p/>      * {@link Document#add(Fieldable)} should be called by the Reader.       */
 DECL|field|LOAD
 specifier|public
+specifier|transient
 specifier|static
 specifier|final
 name|FieldSelectorResult
@@ -48,6 +61,7 @@ decl_stmt|;
 comment|/**      * Lazily load this {@link Field}.  This means the {@link Field} is valid, but it may not actually contain its data until      * invoked.  {@link Document#getField(String)} SHOULD NOT BE USED.  {@link Document#getFieldable(String)} is safe to use and should      * return a valid instance of a {@link Fieldable}.      *<p/>      * {@link Document#add(Fieldable)} should be called by the Reader.      */
 DECL|field|LAZY_LOAD
 specifier|public
+specifier|transient
 specifier|static
 specifier|final
 name|FieldSelectorResult
@@ -62,6 +76,7 @@ decl_stmt|;
 comment|/**      * Do not load the {@link Field}.  {@link Document#getField(String)} and {@link Document#getFieldable(String)} should return null.      * {@link Document#add(Fieldable)} is not called.      *<p/>      * {@link Document#add(Fieldable)} should not be called by the Reader.      */
 DECL|field|NO_LOAD
 specifier|public
+specifier|transient
 specifier|static
 specifier|final
 name|FieldSelectorResult
@@ -76,6 +91,7 @@ decl_stmt|;
 comment|/**      * Load this field as in the {@link #LOAD} case, but immediately return from {@link Field} loading for the {@link Document}.  Thus, the      * Document may not have its complete set of Fields.  {@link Document#getField(String)} and {@link Document#getFieldable(String)} should      * both be valid for this {@link Field}      *<p/>      * {@link Document#add(Fieldable)} should be called by the Reader.      */
 DECL|field|LOAD_AND_BREAK
 specifier|public
+specifier|transient
 specifier|static
 specifier|final
 name|FieldSelectorResult
@@ -90,6 +106,7 @@ decl_stmt|;
 comment|/**      * Behaves much like {@link #LOAD} but does not uncompress any compressed data.  This is used for internal purposes.      * {@link Document#getField(String)} and {@link Document#getFieldable(String)} should not return null.      *<p/>      * {@link Document#add(Fieldable)} should be called by the Reader.      */
 DECL|field|LOAD_FOR_MERGE
 specifier|public
+specifier|transient
 specifier|static
 specifier|final
 name|FieldSelectorResult
@@ -104,6 +121,7 @@ decl_stmt|;
 comment|/** Expert:  Load the size of this {@link Field} rather than its value.        * Size is measured as number of bytes required to store the field == bytes for a binary or any compressed value, and 2*chars for a String value.       * The size is stored as a binary value, represented as an int in a byte[], with the higher order byte first in [0]       */
 DECL|field|SIZE
 specifier|public
+specifier|transient
 specifier|static
 specifier|final
 name|FieldSelectorResult
@@ -118,6 +136,7 @@ decl_stmt|;
 comment|/** Expert: Like {@link #SIZE} but immediately break from the field loading loop, i.e. stop loading further fields, after the size is loaded */
 DECL|field|SIZE_AND_BREAK
 specifier|public
+specifier|transient
 specifier|static
 specifier|final
 name|FieldSelectorResult
