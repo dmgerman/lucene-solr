d0ea70a1f5a5ad714ba3d5a3c8699205226bd628
Yonik Seeley <yonik@apache.org>
2007-04-11 18:16:01 +0000

    allow fieldType as well as fieldtype in schema
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@527594 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 834c7c60d34261231f5b7fcfa2e3b8c35201df7e

--- src/java/org/apache/solr/schema/IndexSchema.java
+++ src/java/org/apache/solr/schema/IndexSchema.java
@@ -1257,7 +1257,7 @@ expr_stmt|;
 name|String
 name|expression
 init|=
-literal|"/schema/types/fieldtype"
+literal|"/schema/types/fieldtype | /schema/types/fieldType"
 decl_stmt|;
 name|NodeList
 name|nodes
