0fc09dff4a81c3e7bbfaaa7ec6d5e4baa1ac96df
Yonik Seeley <yonik@apache.org>
2007-10-22 13:43:07 +0000

    SOLR-334: pluggable query parsers
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@587090 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: fc96a325ed2176b6cd0eae9087726387347e470a

--- src/java/org/apache/solr/schema/IndexSchema.java
+++ src/java/org/apache/solr/schema/IndexSchema.java
@@ -1378,6 +1378,10 @@ name|FieldType
 argument_list|>
 argument_list|(
 literal|"[schema.xml] fieldType"
+argument_list|,
+literal|true
+argument_list|,
+literal|true
 argument_list|)
 block|{
 annotation|@
