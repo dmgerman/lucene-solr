21d6fb1cd35042baf67d15f13f1157a174d01feb
Ryan McKinley <ryan@apache.org>
2008-01-23 17:29:43 +0000

    removed unused imports
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@614585 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e71870c4f62f44b94bbe6586e9be2ca84fc97c19

--- client/java/solrj/test/org/apache/solr/client/solrj/embedded/LargeVolumeJettyTest.java
+++ client/java/solrj/test/org/apache/solr/client/solrj/embedded/LargeVolumeJettyTest.java
@@ -70,34 +70,6 @@ name|CommonsHttpSolrServer
 import|;
 end_import
 
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|solr
-operator|.
-name|core
-operator|.
-name|SolrCore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|solr
-operator|.
-name|schema
-operator|.
-name|SchemaField
-import|;
-end_import
-
 begin_comment
 comment|/**  * @version $Id$  * @since solr 1.3  */
 end_comment
