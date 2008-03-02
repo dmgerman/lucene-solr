516c0da3db667f4ac8fb78f9f940095b80fbd8dd
Michael McCandless <mikemccand@apache.org>
2008-03-02 23:34:40 +0000

    LUCENE-1199: do defensive check of indexReader != null before trying to close it
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@632879 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 33bee3c60ce485ae6be4a3ce6038fe09f105f6a1

--- src/java/org/apache/lucene/index/IndexModifier.java
+++ src/java/org/apache/lucene/index/IndexModifier.java
@@ -1251,7 +1251,13 @@ operator|=
 literal|null
 expr_stmt|;
 block|}
-else|else
+elseif|else
+if|if
+condition|(
+name|indexReader
+operator|!=
+literal|null
+condition|)
 block|{
 name|indexReader
 operator|.
