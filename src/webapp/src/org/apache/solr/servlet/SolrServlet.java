4a8889c6bf282037958658ef3e7caf239a155ded
Chris M. Hostetter <hossman@apache.org>
2008-06-12 18:27:50 +0000

    SOLR-597: SolrServlet no longer "caches" SolrCore.  This was causing problems in Resin, and could potentially cause problems for customized usages of SolrServlet.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@667190 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f690d9facfec3c82fcb518c387c5a4db7e562496

--- src/webapp/src/org/apache/solr/servlet/SolrServlet.java
+++ src/webapp/src/org/apache/solr/servlet/SolrServlet.java
@@ -195,10 +195,6 @@ name|getName
 argument_list|()
 argument_list|)
 decl_stmt|;
-DECL|field|core
-name|SolrCore
-name|core
-decl_stmt|;
 DECL|method|init
 specifier|public
 name|void
@@ -214,13 +210,8 @@ argument_list|(
 literal|"SolrServlet.init()"
 argument_list|)
 expr_stmt|;
-name|core
-operator|=
-name|SolrCore
-operator|.
-name|getSolrCore
-argument_list|()
-expr_stmt|;
+comment|// we deliberately do not initialize a SolrCore because of SOLR-597
+comment|// https://issues.apache.org/jira/browse/SOLR-597
 name|log
 operator|.
 name|info
@@ -269,6 +260,15 @@ name|ServletException
 throws|,
 name|IOException
 block|{
+specifier|final
+name|SolrCore
+name|core
+init|=
+name|SolrCore
+operator|.
+name|getSolrCore
+argument_list|()
+decl_stmt|;
 name|SolrServletRequest
 name|solrReq
 init|=
