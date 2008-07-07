065faf0c19e223bbdd91232e8de29d6c662de4a6
Yonik Seeley <yonik@apache.org>
2008-07-07 16:37:30 +0000

    fix distributed search null pointer exception when no docs are matched and debugQuery=true
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@674549 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e7b1fff87a8d677a4238bbd7a5a00c9802f57d4e

--- src/java/org/apache/solr/handler/component/DebugComponent.java
+++ src/java/org/apache/solr/handler/component/DebugComponent.java
@@ -449,7 +449,7 @@ name|isDebug
 argument_list|()
 condition|)
 return|return;
-comment|// Turn on debug to get explain only only when retrieving fields
+comment|// Turn on debug to get explain only when retrieving fields
 if|if
 condition|(
 operator|(
@@ -768,6 +768,20 @@ argument_list|)
 argument_list|)
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|info
+operator|==
+literal|null
+condition|)
+block|{
+name|info
+operator|=
+operator|new
+name|NamedList
+argument_list|()
+expr_stmt|;
+block|}
 name|int
 name|idx
 init|=
