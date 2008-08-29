c8dd097a4726083c1041b2861ce09097efd6dc52
Yonik Seeley <yonik@apache.org>
2008-08-29 15:39:52 +0000

    javadoc fix
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@690306 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 09114186ba1c9dab89b4a2d848dcc7f7c81dff96

--- src/java/org/apache/lucene/util/OpenBitSet.java
+++ src/java/org/apache/lucene/util/OpenBitSet.java
@@ -378,7 +378,7 @@ operator|!=
 literal|0
 return|;
 block|}
-comment|/** Returns true or false for the specified bit index   * The index should be less than the OpenBitSet size   */
+comment|/** Returns true or false for the specified bit index   */
 DECL|method|get
 specifier|public
 name|boolean
@@ -443,7 +443,7 @@ operator|!=
 literal|0
 return|;
 block|}
-comment|/** Returns true or false for the specified bit index.  Allows specifying    * an index outside the current size. */
+comment|/** Returns true or false for the specified bit index.    * The index should be less than the OpenBitSet size.    */
 DECL|method|fastGet
 specifier|public
 name|boolean
