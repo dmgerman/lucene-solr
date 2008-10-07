84fbc937d7ca2605d84d6ff581c82b89fa4f525f
Ryan McKinley <ryan@apache.org>
2008-10-07 16:13:02 +0000

    make sure the directory exists before listing files  (this could only happen if instanceDir is invalid)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@702528 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9465b6bc2993e53ce66d80a0a357981f131581b9

--- src/java/org/apache/solr/core/SolrCore.java
+++ src/java/org/apache/solr/core/SolrCore.java
@@ -6684,6 +6684,19 @@ name|getConfigDir
 argument_list|()
 argument_list|)
 decl_stmt|;
+if|if
+condition|(
+name|configdir
+operator|.
+name|exists
+argument_list|()
+operator|&&
+name|configdir
+operator|.
+name|isDirectory
+argument_list|()
+condition|)
+block|{
 for|for
 control|(
 name|String
@@ -6705,6 +6718,7 @@ name|toUpperCase
 argument_list|()
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 comment|// except the "gettable" list
 name|StringTokenizer
