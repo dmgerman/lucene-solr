50b0d6f7e3e6bb1061f0e5835f1e89142fa29868
Shalin Shekhar Mangar <shalin@apache.org>
2008-10-31 05:32:03 +0000

    SOLR-832 -- Rows parameter is not honored in non-debug mode and can abort a running import in debug mode.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@709352 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 96b0ebc980a9e9ba411cd220ed1ec2d8cf46e12d

--- contrib/dataimporthandler/src/main/java/org/apache/solr/handler/dataimport/DataImporter.java
+++ contrib/dataimporthandler/src/main/java/org/apache/solr/handler/dataimport/DataImporter.java
@@ -2632,7 +2632,9 @@ specifier|public
 name|int
 name|rows
 init|=
-literal|10
+name|Integer
+operator|.
+name|MAX_VALUE
 decl_stmt|;
 DECL|field|clean
 specifier|public
@@ -2721,6 +2723,10 @@ block|{
 name|debug
 operator|=
 literal|true
+expr_stmt|;
+name|rows
+operator|=
+literal|10
 expr_stmt|;
 comment|// Set default values suitable for debug mode
 name|commit
