17931ae27401850fe925143d98fbdb2773286e70
Ryan McKinley <ryan@apache.org>
2008-11-20 19:52:11 +0000

    SOLR-793: fix possible deadlock
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@719347 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 075cdc58900bd61f583baac91b2ada0b2b48a1f0

--- src/java/org/apache/solr/update/DirectUpdateHandler2.java
+++ src/java/org/apache/solr/update/DirectUpdateHandler2.java
@@ -1723,6 +1723,21 @@ name|long
 name|commitMaxTime
 parameter_list|)
 block|{
+name|_scheduleCommitWithin
+argument_list|(
+name|commitMaxTime
+argument_list|)
+expr_stmt|;
+block|}
+DECL|method|_scheduleCommitWithin
+specifier|private
+name|void
+name|_scheduleCommitWithin
+parameter_list|(
+name|long
+name|commitMaxTime
+parameter_list|)
+block|{
 comment|// Check if there is a commit already scheduled for longer then this time
 if|if
 condition|(
@@ -1813,7 +1828,7 @@ name|docsUpperBound
 operator|)
 condition|)
 block|{
-name|scheduleCommitWithin
+name|_scheduleCommitWithin
 argument_list|(
 name|DOC_COMMIT_DELAY_MS
 argument_list|)
@@ -1840,7 +1855,7 @@ operator|>
 literal|0
 condition|)
 block|{
-name|scheduleCommitWithin
+name|_scheduleCommitWithin
 argument_list|(
 name|ctime
 argument_list|)
