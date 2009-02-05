956ce4f063cd9abbe4f2e5e8318c96bdb03b6e55
Shalin Shekhar Mangar <shalin@apache.org>
2009-02-05 19:38:52 +0000

    SOLR-986 -- Return invalid error codes in case of an exception or an error while waiting for external process
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@741257 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 23ab5e1869b5e5fa45f282335b7503404a202c39

--- src/java/org/apache/solr/core/RunExecutableListener.java
+++ src/java/org/apache/solr/core/RunExecutableListener.java
@@ -360,6 +360,7 @@ operator|=
 literal|false
 expr_stmt|;
 block|}
+comment|/**    * External executable listener.    *     * @param callback Unused (As of solr 1.4-dev)    * @return Error code indicating if the command has executed successfully.<br />    *  0 , indicates normal termination.<br />    *  non-zero , otherwise.    */
 DECL|method|exec
 specifier|protected
 name|int
@@ -449,6 +450,10 @@ argument_list|,
 name|e
 argument_list|)
 expr_stmt|;
+name|ret
+operator|=
+name|INVALID_PROCESS_RETURN_CODE
+expr_stmt|;
 block|}
 block|}
 if|if
@@ -492,6 +497,10 @@ argument_list|,
 name|e
 argument_list|)
 expr_stmt|;
+name|ret
+operator|=
+name|INVALID_PROCESS_RETURN_CODE
+expr_stmt|;
 block|}
 return|return
 name|ret
@@ -530,6 +539,16 @@ literal|"newSearcher"
 argument_list|)
 expr_stmt|;
 block|}
+comment|/** Non-zero value for an invalid return code **/
+DECL|field|INVALID_PROCESS_RETURN_CODE
+specifier|private
+specifier|static
+name|int
+name|INVALID_PROCESS_RETURN_CODE
+init|=
+operator|-
+literal|1
+decl_stmt|;
 block|}
 end_class
 
