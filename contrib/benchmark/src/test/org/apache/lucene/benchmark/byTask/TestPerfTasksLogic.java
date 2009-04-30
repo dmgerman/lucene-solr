5345cb8004057d663965666883f344de546c8aa6
Michael McCandless <mikemccand@apache.org>
2009-04-30 10:32:56 +0000

    try to prevent false failure of testTimedSearchTask
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@770151 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9ba59706b0fc7727283609dd8bedd774167fcd48

--- contrib/benchmark/src/test/org/apache/lucene/benchmark/byTask/TestPerfTasksLogic.java
+++ contrib/benchmark/src/test/org/apache/lucene/benchmark/byTask/TestPerfTasksLogic.java
@@ -595,7 +595,7 @@ name|elapsed
 init|=
 name|CountingSearchTestTask
 operator|.
-name|lastMillis
+name|prevLastMillis
 operator|-
 name|CountingSearchTestTask
 operator|.
@@ -610,8 +610,8 @@ operator|+
 literal|" msec"
 argument_list|,
 name|elapsed
-operator|<
-literal|2000
+operator|<=
+literal|1500
 argument_list|)
 expr_stmt|;
 block|}
