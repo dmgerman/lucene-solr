032ddbb629e2c84f3d8b45bb79c4a503dddef2d2
Yonik Seeley <yonik@apache.org>
2009-05-28 02:41:51 +0000

    revert previous accidental commit
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@779413 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3e7361e964cca05d678a204253b800fb2c7f4ae0

--- src/java/org/apache/solr/search/SolrIndexSearcher.java
+++ src/java/org/apache/solr/search/SolrIndexSearcher.java
@@ -3116,15 +3116,6 @@ parameter_list|)
 throws|throws
 name|IOException
 block|{
-name|query
-operator|=
-name|QueryUtils
-operator|.
-name|simplifyQuery
-argument_list|(
-name|query
-argument_list|)
-expr_stmt|;
 name|DocSetCollector
 name|collector
 init|=
