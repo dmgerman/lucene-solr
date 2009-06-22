958061f101ec8b42c9bea268c3ef4aef4e79ea2a
Michael McCandless <mikemccand@apache.org>
2009-06-22 22:18:56 +0000

    bulk fix svn:eol-style to native for text files
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@787424 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ec8088654dca92e0f219bfa27c54b16e2013d669

--- contrib/spatial/src/test/org/apache/lucene/spatial/tier/PolyShape.java
+++ contrib/spatial/src/test/org/apache/lucene/spatial/tier/PolyShape.java
@@ -1,15 +1,10 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-DECL|package|org.apache.lucene.spatial.tier
-DECL|class|PolyShape
-DECL|field|lat
-DECL|field|lng
-DECL|field|miles
-DECL|method|main
 comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
+DECL|package|org.apache.lucene.spatial.tier
 package|package
 name|org
 operator|.
@@ -28,10 +23,12 @@ comment|/**  *  */
 end_comment
 
 begin_class
+DECL|class|PolyShape
 specifier|public
 class|class
 name|PolyShape
 block|{
+DECL|field|lat
 specifier|private
 specifier|static
 name|double
@@ -39,6 +36,7 @@ name|lat
 init|=
 literal|38.969398
 decl_stmt|;
+DECL|field|lng
 specifier|private
 specifier|static
 name|double
@@ -47,6 +45,7 @@ init|=
 operator|-
 literal|77.386398
 decl_stmt|;
+DECL|field|miles
 specifier|private
 specifier|static
 name|int
@@ -55,6 +54,7 @@ init|=
 literal|1000
 decl_stmt|;
 comment|/**    * @param args    */
+DECL|method|main
 specifier|public
 specifier|static
 name|void
