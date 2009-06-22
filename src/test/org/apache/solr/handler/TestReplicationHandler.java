7966715e9ef61d010dc6de08a9bb4b20680c33f7
Noble Paul <noble@apache.org>
2009-06-22 12:05:54 +0000

    SOLR-1216
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@787212 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f2343ea9877c4a5b6bc8e3ea5d78c992577c148e

--- src/test/org/apache/solr/handler/TestReplicationHandler.java
+++ src/test/org/apache/solr/handler/TestReplicationHandler.java
@@ -1907,7 +1907,7 @@ operator|.
 name|getLocalPort
 argument_list|()
 operator|+
-literal|"/solr/replication?command=snappull&masterUrl="
+literal|"/solr/replication?command=fetchindex&masterUrl="
 decl_stmt|;
 name|masterUrl
 operator|+=
