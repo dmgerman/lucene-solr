20c128c904d6676307b00fe39ef076122f7700f8
Yonik Seeley <yonik@apache.org>
2009-07-10 19:40:33 +0000

    SOLR-1266: add WordDelimiterFilter.stemEnglishPossessive option
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@793090 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8bb8d6561c9bb562807e2a4a3ad631c30121a50c

--- src/java/org/apache/solr/analysis/WordDelimiterFilterFactory.java
+++ src/java/org/apache/solr/analysis/WordDelimiterFilterFactory.java
@@ -361,6 +361,12 @@ name|preserveOriginal
 init|=
 literal|0
 decl_stmt|;
+DECL|field|stemEnglishPossessive
+name|int
+name|stemEnglishPossessive
+init|=
+literal|0
+decl_stmt|;
 annotation|@
 name|Override
 DECL|method|init
@@ -456,6 +462,15 @@ argument_list|,
 literal|0
 argument_list|)
 expr_stmt|;
+name|stemEnglishPossessive
+operator|=
+name|getInt
+argument_list|(
+literal|"stemEnglishPossessive"
+argument_list|,
+literal|1
+argument_list|)
+expr_stmt|;
 block|}
 DECL|method|create
 specifier|public
@@ -487,6 +502,8 @@ argument_list|,
 name|preserveOriginal
 argument_list|,
 name|splitOnNumerics
+argument_list|,
+name|stemEnglishPossessive
 argument_list|,
 name|protectedWords
 argument_list|)
