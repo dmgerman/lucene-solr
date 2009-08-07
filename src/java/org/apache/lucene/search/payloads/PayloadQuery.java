10c86ad7e5bc9c3c112a57b478a2d265e4c003fa
Grant Ingersoll <gsingers@apache.org>
2009-08-07 20:34:58 +0000

    LUCENE-1790: small refactor of Payload queries, plus add in some new payload query functionality
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@802174 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e079d1cec34ffd1dbf2b04e1ab3a9c5820b9452b

---
+++ src/java/org/apache/lucene/search/payloads/PayloadQuery.java
@@ -0,0 +1,30 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+DECL|package|org.apache.lucene.search.payloads
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|search
+operator|.
+name|payloads
+package|;
+end_package
+
+begin_comment
+comment|/**  * Marker interface inidcating this Query is Payload aware  *  **/
+end_comment
+
+begin_interface
+DECL|interface|PayloadQuery
+specifier|public
+interface|interface
+name|PayloadQuery
+block|{ }
+end_interface
+
+end_unit
+
--- src/test/org/apache/lucene/search/payloads/TestBoostingNearQuery.java
+++ src/test/org/apache/lucene/search/payloads/TestBoostingNearQuery.java
@@ -1311,6 +1311,9 @@ specifier|public
 name|float
 name|scorePayload
 parameter_list|(
+name|int
+name|docId
+parameter_list|,
 name|String
 name|fieldName
 parameter_list|,
