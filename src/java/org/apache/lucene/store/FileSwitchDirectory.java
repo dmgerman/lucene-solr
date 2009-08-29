1b1f20e1938f8786b32ab61544d6992a4bf9896b
Michael McCandless <mikemccand@apache.org>
2009-08-29 20:52:03 +0000

    LUCENE-1872: improve javadocs
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@809202 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8107d186c7538842d710aa2c665e9d4a1613ee7c

--- src/java/org/apache/lucene/store/FileSwitchDirectory.java
+++ src/java/org/apache/lucene/store/FileSwitchDirectory.java
@@ -57,7 +57,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Files with the specified extensions are placed in the  * primary directory; others are placed in the secondary  * directory.  The provided Set must not change once passed  * to this class, and must allow multiple threads to call  * contains at once.  */
+comment|/**  * Expert: A Directory instance that switches files betweeen  * two other Directory instances.   *<p>Files with the specified extensions are placed in the  * primary directory; others are placed in the secondary  * directory.  The provided Set must not change once passed  * to this class, and must allow multiple threads to call  * contains at once.</p>  *  *<p><b>NOTE</b>: this API is new and experimental and is  * subject to suddenly change in the next release.  */
 end_comment
 
 begin_class
@@ -142,6 +142,7 @@ name|getLockFactory
 argument_list|()
 expr_stmt|;
 block|}
+comment|/** Return the primary directory */
 DECL|method|getPrimaryDir
 specifier|public
 name|Directory
@@ -152,6 +153,7 @@ return|return
 name|primaryDir
 return|;
 block|}
+comment|/** Return the secondary directory */
 DECL|method|getSecondaryDir
 specifier|public
 name|Directory
@@ -294,6 +296,7 @@ name|listAll
 argument_list|()
 return|;
 block|}
+comment|/** Utility method to return a file's extension. */
 DECL|method|getExtension
 specifier|public
 specifier|static
