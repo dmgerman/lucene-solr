978524f0a697132db5e39dee769d692210fe58ae
Chris M. Hostetter <hossman@apache.org>
2009-09-01 20:10:33 +0000

    LUCENE-1882: move SmartChineseAnalyzer to the 'correct' package ... this commit is based on a sequence of svn commands and a patch provided by Robert Muir in LUCENE-1862
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@810208 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e5cb7f668a919906a478ebcce78b336af50d747c

--- contrib/analyzers/smartcn/src/test/org/apache/lucene/analysis/cn/smart/TestSmartChineseAnalyzer.java
+++ contrib/analyzers/smartcn/src/test/org/apache/lucene/analysis/cn/smart/TestSmartChineseAnalyzer.java
@@ -4,7 +4,7 @@ comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or mor
 end_comment
 
 begin_package
-DECL|package|org.apache.lucene.analysis.cn
+DECL|package|org.apache.lucene.analysis.cn.smart
 package|package
 name|org
 operator|.
@@ -15,6 +15,8 @@ operator|.
 name|analysis
 operator|.
 name|cn
+operator|.
+name|smart
 package|;
 end_package
 
