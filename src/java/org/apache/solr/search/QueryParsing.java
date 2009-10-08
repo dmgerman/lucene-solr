94eabb9b80c787e8ebf6550cfba6406825837d0d
Yonik Seeley <yonik@apache.org>
2009-10-08 15:15:50 +0000

    SOLR-1478: use _docid_ to sort by the lucene docid
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@823199 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: aaa638d1880db943b3d2dd259ddd30cea3047683

--- src/java/org/apache/solr/search/QueryParsing.java
+++ src/java/org/apache/solr/search/QueryParsing.java
@@ -290,7 +290,6 @@ specifier|public
 class|class
 name|QueryParsing
 block|{
-comment|/** the SolrParam used to override the QueryParser "default operator" */
 DECL|field|OP
 specifier|public
 specifier|static
@@ -300,6 +299,7 @@ name|OP
 init|=
 literal|"q.op"
 decl_stmt|;
+comment|// the SolrParam used to override the QueryParser "default operator"
 DECL|field|V
 specifier|public
 specifier|static
@@ -357,6 +357,15 @@ name|char
 name|LOCALPARAM_END
 init|=
 literal|'}'
+decl_stmt|;
+DECL|field|DOCID
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|DOCID
+init|=
+literal|"_docid_"
 decl_stmt|;
 comment|/**     * Helper utility for parsing a query using the Lucene QueryParser syntax.     * @param qs query expression in standard Lucene syntax    * @param schema used for default operator (overridden by params) and passed to the query parser for field format analysis information    */
 DECL|method|parseQuery
@@ -1398,7 +1407,7 @@ block|}
 elseif|else
 if|if
 condition|(
-literal|"#"
+name|DOCID
 operator|.
 name|equals
 argument_list|(
