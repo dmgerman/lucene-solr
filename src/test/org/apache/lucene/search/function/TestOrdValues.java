6be32a6842ea08ae720545186d4f2fd0ff17e8ae
Uwe Schindler <uschindler@apache.org>
2009-11-11 09:12:18 +0000

    LUCENE-1973: Remove deprec MultiValueSource
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@834812 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 552ba37c66bcea4d08f74d3b6ac52b1d8ebd3b91

--- src/test/org/apache/lucene/search/function/TestOrdValues.java
+++ src/test/org/apache/lucene/search/function/TestOrdValues.java
@@ -220,13 +220,9 @@ block|{
 name|vs
 operator|=
 operator|new
-name|MultiValueSource
-argument_list|(
-operator|new
 name|OrdFieldSource
 argument_list|(
 name|field
-argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
@@ -235,13 +231,9 @@ block|{
 name|vs
 operator|=
 operator|new
-name|MultiValueSource
-argument_list|(
-operator|new
 name|ReverseOrdFieldSource
 argument_list|(
 name|field
-argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
