5a935ebb6fb3d90a094542da0cdedba49ba08188
Yonik Seeley <yonik@apache.org>
2009-11-24 01:42:40 +0000

    SOLR-1595: use utf8 in streaming solrj client
    git-svn-id: https://svn.apache.org/repos/asf/lucene/solr/trunk@883566 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 73fe358140b314f5027b7f1d215df186749613e9

--- src/test/org/apache/solr/client/solrj/SolrExampleTests.java
+++ src/test/org/apache/solr/client/solrj/SolrExampleTests.java
@@ -1317,7 +1317,7 @@ name|addField
 argument_list|(
 literal|"name"
 argument_list|,
-literal|"doc2"
+literal|"h\u1234llo"
 argument_list|,
 literal|1.0f
 argument_list|)
@@ -1408,8 +1408,6 @@ argument_list|(
 name|query
 argument_list|)
 decl_stmt|;
-name|Assert
-operator|.
 name|assertEquals
 argument_list|(
 literal|2
@@ -1457,8 +1455,6 @@ argument_list|(
 name|query
 argument_list|)
 expr_stmt|;
-name|Assert
-operator|.
 name|assertEquals
 argument_list|(
 literal|2
@@ -1472,15 +1468,34 @@ name|getNumFound
 argument_list|()
 argument_list|)
 expr_stmt|;
-name|System
+comment|// System.out.println( rsp.getResults() );
+comment|// query outside ascii range
+name|query
 operator|.
-name|out
-operator|.
-name|println
+name|setQuery
 argument_list|(
+literal|"name:h\u1234llo"
+argument_list|)
+expr_stmt|;
+name|rsp
+operator|=
+name|server
+operator|.
+name|query
+argument_list|(
+name|query
+argument_list|)
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|1
+argument_list|,
 name|rsp
 operator|.
 name|getResults
+argument_list|()
+operator|.
+name|getNumFound
 argument_list|()
 argument_list|)
 expr_stmt|;
