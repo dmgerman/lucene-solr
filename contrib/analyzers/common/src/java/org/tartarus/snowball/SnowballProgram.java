e8bb9e82e1b997c11be8ef693b62d8a418c5b9ef
Robert Muir <rmuir@apache.org>
2010-02-28 10:41:28 +0000

    LUCENE-2288: remove more unnecessary snowball object creation
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/trunk@917161 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0a1472386a48e014d334f2b53225d1667828a103

--- contrib/analyzers/common/src/java/org/tartarus/snowball/SnowballProgram.java
+++ contrib/analyzers/common/src/java/org/tartarus/snowball/SnowballProgram.java
@@ -55,7 +55,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * This is the rev 502 of the Snowball SVN trunk,  * but modified:  * made abstract and introduced abstract method stem to avoid expensive reflection in filter class.  * refactored StringBuffers to StringBuilder  * uses char[] as buffer instead of StringBuffer/StringBuilder  * eq_s,eq_s_b,insert,replace_s take CharSequence like eq_v and eq_v_b  */
+comment|/**  * This is the rev 502 of the Snowball SVN trunk,  * but modified:  * made abstract and introduced abstract method stem to avoid expensive reflection in filter class.  * refactored StringBuffers to StringBuilder  * uses char[] as buffer instead of StringBuffer/StringBuilder  * eq_s,eq_s_b,insert,replace_s take CharSequence like eq_v and eq_v_b  * reflection calls (Lovins, etc) use EMPTY_ARGS/EMPTY_PARAMS  */
 end_comment
 
 begin_class
@@ -65,6 +65,20 @@ specifier|abstract
 class|class
 name|SnowballProgram
 block|{
+DECL|field|EMPTY_ARGS
+specifier|private
+specifier|static
+specifier|final
+name|Object
+index|[]
+name|EMPTY_ARGS
+init|=
+operator|new
+name|Object
+index|[
+literal|0
+index|]
+decl_stmt|;
 DECL|method|SnowballProgram
 specifier|protected
 name|SnowballProgram
@@ -1417,11 +1431,7 @@ name|w
 operator|.
 name|methodobject
 argument_list|,
-operator|new
-name|Object
-index|[
-literal|0
-index|]
+name|EMPTY_ARGS
 argument_list|)
 decl_stmt|;
 name|res
@@ -1779,11 +1789,7 @@ name|w
 operator|.
 name|methodobject
 argument_list|,
-operator|new
-name|Object
-index|[
-literal|0
-index|]
+name|EMPTY_ARGS
 argument_list|)
 decl_stmt|;
 name|res
