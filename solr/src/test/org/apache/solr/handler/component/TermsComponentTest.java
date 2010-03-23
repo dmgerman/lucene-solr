747e77072917eb6a360694259bebcc13aefdcbb1
Yonik Seeley <yonik@apache.org>
2010-03-23 21:55:51 +0000

    tests: ignore expected exceptions
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@926807 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6ebd260d016bab3bfd777e7a402fad216f815ee3

--- solr/src/test/org/apache/solr/handler/component/TermsComponentTest.java
+++ solr/src/test/org/apache/solr/handler/component/TermsComponentTest.java
@@ -1166,6 +1166,11 @@ name|SimpleOrderedMap
 argument_list|()
 argument_list|)
 expr_stmt|;
+name|ignoreException
+argument_list|(
+literal|"No terms.fl parameter specified"
+argument_list|)
+expr_stmt|;
 name|handler
 operator|.
 name|handleRequest
@@ -1180,6 +1185,9 @@ argument_list|)
 argument_list|,
 name|rsp
 argument_list|)
+expr_stmt|;
+name|resetExceptionIgnores
+argument_list|()
 expr_stmt|;
 name|Exception
 name|exception
