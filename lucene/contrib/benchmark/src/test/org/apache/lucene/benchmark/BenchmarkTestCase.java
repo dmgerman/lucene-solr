2731ef299258b9da8b2f412cd6bfce06e9172691
Robert Muir <rmuir@apache.org>
2010-03-26 21:55:57 +0000

    LUCENE-1709: Parallelize Tests
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@928069 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 65127736f0f01cbbaff17582d471b40f01414726

--- lucene/contrib/benchmark/src/test/org/apache/lucene/benchmark/BenchmarkTestCase.java
+++ lucene/contrib/benchmark/src/test/org/apache/lucene/benchmark/BenchmarkTestCase.java
@@ -28,11 +28,15 @@ end_import
 
 begin_import
 import|import
-name|junit
+name|org
 operator|.
-name|framework
+name|apache
 operator|.
-name|TestCase
+name|lucene
+operator|.
+name|util
+operator|.
+name|LuceneTestCase
 import|;
 end_import
 
@@ -46,7 +50,7 @@ specifier|public
 class|class
 name|BenchmarkTestCase
 extends|extends
-name|TestCase
+name|LuceneTestCase
 block|{
 DECL|field|workDir
 specifier|private
