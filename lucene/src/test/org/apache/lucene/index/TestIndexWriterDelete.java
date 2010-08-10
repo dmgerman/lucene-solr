9927c7b86e3ff3114cdf601f291ec8751a88fc18
Michael McCandless <mikemccand@apache.org>
2010-08-10 19:45:48 +0000

    LUCENE-2593: fix certain rare cases where disk full could cause index corruption
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@984187 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 91b5291d5a9e21e8a1933f24c5150dda771edd72

--- lucene/src/test/org/apache/lucene/index/TestIndexWriterDelete.java
+++ lucene/src/test/org/apache/lucene/index/TestIndexWriterDelete.java
@@ -166,6 +166,20 @@ name|LuceneTestCase
 import|;
 end_import
 
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|util
+operator|.
+name|_TestUtil
+import|;
+end_import
+
 begin_class
 DECL|class|TestIndexWriterDelete
 specifier|public
@@ -3168,6 +3182,14 @@ if|if
 condition|(
 name|success
 condition|)
+block|{
+name|_TestUtil
+operator|.
+name|checkIndex
+argument_list|(
+name|dir
+argument_list|)
+expr_stmt|;
 name|TestIndexWriter
 operator|.
 name|assertNoUnreferencedFiles
@@ -3177,6 +3199,7 @@ argument_list|,
 literal|"after writer.close"
 argument_list|)
 expr_stmt|;
+block|}
 comment|// Finally, verify index is not corrupt, and, if
 comment|// we succeeded, we see all docs changed, and if
 comment|// we failed, we see either all docs or no docs
