54ca902a28fd61e0f9f1e08fa38ff4ef1c5ccd94
Robert Muir <rmuir@apache.org>
2010-08-24 20:27:47 +0000

    wire this test for ramdir too for now
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@988710 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ba6d2d37dcba7145a075ace856023958ad920a4e

--- lucene/src/test/org/apache/lucene/index/TestIndexWriter.java
+++ lucene/src/test/org/apache/lucene/index/TestIndexWriter.java
@@ -2791,7 +2791,7 @@ name|close
 argument_list|()
 expr_stmt|;
 block|}
-comment|/*      * Make sure IndexWriter cleans up on hitting a disk      * full exception in addDocument.      */
+comment|/*      * Make sure IndexWriter cleans up on hitting a disk      * full exception in addDocument.      * TODO: how to do this on windows with FSDirectory?      */
 DECL|method|testAddDocumentOnDiskFull
 specifier|public
 name|void
@@ -2865,9 +2865,12 @@ expr_stmt|;
 name|MockDirectoryWrapper
 name|dir
 init|=
-name|newDirectory
+operator|new
+name|MockDirectoryWrapper
 argument_list|(
-name|random
+operator|new
+name|RAMDirectory
+argument_list|()
 argument_list|)
 decl_stmt|;
 name|dir
