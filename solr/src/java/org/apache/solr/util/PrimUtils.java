462ef7c71d21fb3abfd98cd98c52f40feff2b66c
Yonik Seeley <yonik@apache.org>
2010-08-26 00:20:39 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@989410 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0b2ed2201949e9381259f30de1355ce9cf697af1

--- solr/src/java/org/apache/solr/util/PrimUtils.java
+++ solr/src/java/org/apache/solr/util/PrimUtils.java
@@ -93,7 +93,7 @@ literal|0
 return|;
 block|}
 block|}
-comment|/** Sort the integer array from "start" inclusive to "end" exclusive in ascending order,    *  using the provided comparator.    */
+comment|/** Sort the integer array from "start" inclusive to "end" exclusive in ascending order,    *  using the provided comparator.    * TODO: is this an unstable sort?    */
 DECL|method|sort
 specifier|public
 specifier|static
