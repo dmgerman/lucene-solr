a041df5bec41f8653233d14ee53a10252719e40f
Yonik Seeley <yonik@apache.org>
2010-08-26 00:30:42 +0000

    SOLR-2089: correct test class name
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@989413 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b9ab49e6957e0cf80259e7c3b1fa319e02acad89

--- solr/src/test/org/apache/solr/util/PrimUtilsTest.java
+++ solr/src/test/org/apache/solr/util/PrimUtilsTest.java
@@ -51,10 +51,10 @@ import|;
 end_import
 
 begin_class
-DECL|class|IntUtilsTest
+DECL|class|PrimUtilsTest
 specifier|public
 class|class
-name|IntUtilsTest
+name|PrimUtilsTest
 extends|extends
 name|LuceneTestCase
 block|{
