e2225633d53ca8a286f53cdd8de931c85663bfb2
Michael McCandless <mikemccand@apache.org>
2010-09-15 08:49:17 +0000

    don't close reader prematurely
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@997230 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 76a9dc048cdcba729098d066a4b280db38735b93

--- lucene/contrib/misc/src/test/org/apache/lucene/misc/TestHighFreqTerms.java
+++ lucene/contrib/misc/src/test/org/apache/lucene/misc/TestHighFreqTerms.java
@@ -1127,11 +1127,6 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-name|reader
-operator|.
-name|close
-argument_list|()
-expr_stmt|;
 block|}
 comment|/********************Tests for getTotalTermFreq**********************************/
 DECL|method|testGetTotalTermFreq
@@ -1175,11 +1170,6 @@ argument_list|,
 name|termtext
 argument_list|)
 decl_stmt|;
-name|reader
-operator|.
-name|close
-argument_list|()
-expr_stmt|;
 name|assertEquals
 argument_list|(
 literal|"highTf tf should be 200"
@@ -1231,11 +1221,6 @@ argument_list|,
 name|termtext
 argument_list|)
 decl_stmt|;
-name|reader
-operator|.
-name|close
-argument_list|()
-expr_stmt|;
 name|assertEquals
 argument_list|(
 literal|"totalTermFreq should be 0 for term not in index"
