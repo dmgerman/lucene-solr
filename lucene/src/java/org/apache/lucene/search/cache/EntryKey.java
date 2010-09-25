e8d30c094aa89fe98ba7a22e2bbe779df2e4a8a6
Ryan McKinley <ryan@apache.org>
2010-09-25 19:32:37 +0000

    LUCENE-2649: Objects in the FieldCache can optionally store valid Bits
    Apologies for 'CTR' rather then 'RTC' -- we can always revert if I jumped the gun!  
    
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1001303 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: aeab95d3afe41f1bbaa449a39c6f2962ccdf52a3

---
+++ lucene/src/java/org/apache/lucene/search/cache/EntryKey.java
@@ -0,0 +1,31 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+DECL|package|org.apache.lucene.search.cache
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|search
+operator|.
+name|cache
+package|;
+end_package
+
+begin_comment
+comment|/**  * A Simple marker class -- Perhaps it could/should just be an Object  */
+end_comment
+
+begin_class
+DECL|class|EntryKey
+specifier|public
+specifier|abstract
+class|class
+name|EntryKey
+block|{  }
+end_class
+
+end_unit
+
