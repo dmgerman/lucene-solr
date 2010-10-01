25f1e5b22cb0b554f479087b5a64a9615efbfc80
Robert Muir <rmuir@apache.org>
2010-10-01 21:40:33 +0000

    make tests deterministic
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1003658 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0e5d709bd78fdd7aa17ea2c5321916dcafe45257

--- lucene/src/java/org/apache/lucene/util/automaton/State.java
+++ lucene/src/java/org/apache/lucene/util/automaton/State.java
@@ -968,6 +968,18 @@ operator|-
 name|id
 return|;
 block|}
+annotation|@
+name|Override
+DECL|method|hashCode
+specifier|public
+name|int
+name|hashCode
+parameter_list|()
+block|{
+return|return
+name|id
+return|;
+block|}
 block|}
 end_class
 
