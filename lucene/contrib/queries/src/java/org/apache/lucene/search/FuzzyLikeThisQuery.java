87581b9d34d0b7c1df731abe90304c74eb631a43
Robert Muir <rmuir@apache.org>
2010-10-11 19:43:45 +0000

    LUCENE-2304: FuzzyLikeThisQuery should set MaxNonCompetitiveBoost for faster speed
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1021483 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 668ef95427ca3a2dae20b5bde3df927fc4026eda

--- lucene/contrib/queries/src/java/org/apache/lucene/search/FuzzyLikeThisQuery.java
+++ lucene/contrib/queries/src/java/org/apache/lucene/search/FuzzyLikeThisQuery.java
@@ -1150,6 +1150,24 @@ name|score
 expr_stmt|;
 comment|// maintain minScore
 block|}
+name|boostAtt
+operator|.
+name|setMaxNonCompetitiveBoost
+argument_list|(
+name|variantsQ
+operator|.
+name|size
+argument_list|()
+operator|>=
+name|MAX_VARIANTS_PER_TERM
+condition|?
+name|minScore
+else|:
+name|Float
+operator|.
+name|NEGATIVE_INFINITY
+argument_list|)
+expr_stmt|;
 block|}
 block|}
 if|if
