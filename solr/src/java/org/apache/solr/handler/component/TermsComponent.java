06e3c0d642e043f3bd786182aabd97d247544d29
Mark Robert Miller <markrmiller@apache.org>
2010-10-13 17:01:13 +0000

    SOLR-1873: SolrCloud - added shared/central config and core/shard managment via zookeeper, built-in load balancing, and infrastructure for future SolrCloud work.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1022188 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b55f338757d2bbd616bc5f77c3a05dcfd20b3022

--- solr/src/java/org/apache/solr/handler/component/TermsComponent.java
+++ solr/src/java/org/apache/solr/handler/component/TermsComponent.java
@@ -407,6 +407,12 @@ operator|!=
 literal|null
 condition|)
 block|{
+name|rb
+operator|.
+name|isDistrib
+operator|=
+literal|true
+expr_stmt|;
 if|if
 condition|(
 name|params
@@ -1782,18 +1788,6 @@ argument_list|(
 name|params
 argument_list|)
 expr_stmt|;
-comment|// don't pass through the shards param
-name|sreq
-operator|.
-name|params
-operator|.
-name|remove
-argument_list|(
-name|ShardParams
-operator|.
-name|SHARDS
-argument_list|)
-expr_stmt|;
 comment|// remove any limits for shards, we want them to return all possible
 comment|// responses
 comment|// we want this so we can calculate the correct counts
@@ -1849,40 +1843,6 @@ operator|.
 name|TERMS_SORT_INDEX
 argument_list|)
 expr_stmt|;
-comment|// TODO: is there a better way to handle this?
-name|String
-name|qt
-init|=
-name|params
-operator|.
-name|get
-argument_list|(
-name|CommonParams
-operator|.
-name|QT
-argument_list|)
-decl_stmt|;
-if|if
-condition|(
-name|qt
-operator|!=
-literal|null
-condition|)
-block|{
-name|sreq
-operator|.
-name|params
-operator|.
-name|add
-argument_list|(
-name|CommonParams
-operator|.
-name|QT
-argument_list|,
-name|qt
-argument_list|)
-expr_stmt|;
-block|}
 return|return
 name|sreq
 return|;
