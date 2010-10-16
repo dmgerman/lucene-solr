6efa9444b04cc0ff0ffb7a14abecf46cf7140180
Robert Muir <rmuir@apache.org>
2010-10-16 12:46:22 +0000

    revert r1023266: maybe openjdk doesnt have scripts this needs... perhaps use Assume later in case you have the support
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1023272 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f0e36d867b4a169104555ccdcf354e9d7ce53bd0

--- solr/contrib/dataimporthandler/src/test/java/org/apache/solr/handler/dataimport/TestScriptTransformer.java
+++ solr/contrib/dataimporthandler/src/test/java/org/apache/solr/handler/dataimport/TestScriptTransformer.java
@@ -149,7 +149,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  *<p>  * Test for ScriptTransformer  *</p>  *<p/>  *  * @version $Id$  * @since solr 1.3  */
+comment|/**  *<p>  * Test for ScriptTransformer  *</p>  *<p/>  * All tests in this have been ignored because script support is only available  * in Java 1.6+  *  * @version $Id$  * @since solr 1.3  */
 end_comment
 
 begin_class
@@ -162,6 +162,8 @@ name|AbstractDataImportHandlerTestCase
 block|{
 annotation|@
 name|Test
+annotation|@
+name|Ignore
 DECL|method|testBasic
 specifier|public
 name|void
@@ -365,6 +367,8 @@ return|;
 block|}
 annotation|@
 name|Test
+annotation|@
+name|Ignore
 DECL|method|testOneparam
 specifier|public
 name|void
@@ -460,6 +464,8 @@ expr_stmt|;
 block|}
 annotation|@
 name|Test
+annotation|@
+name|Ignore
 DECL|method|testReadScriptTag
 specifier|public
 name|void
@@ -544,6 +550,8 @@ expr_stmt|;
 block|}
 annotation|@
 name|Test
+annotation|@
+name|Ignore
 DECL|method|testCheckScript
 specifier|public
 name|void
