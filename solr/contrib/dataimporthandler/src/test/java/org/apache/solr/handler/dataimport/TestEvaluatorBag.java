6166c75eef7b855b22015143545cff926f5e6bb1
Robert Muir <rmuir@apache.org>
2010-10-17 13:05:22 +0000

    add another for now, since it appears to have TZ problems :(
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1023493 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 39e1e216b2b947484563125ca15d77fbb66d36b3

--- solr/contrib/dataimporthandler/src/test/java/org/apache/solr/handler/dataimport/TestEvaluatorBag.java
+++ solr/contrib/dataimporthandler/src/test/java/org/apache/solr/handler/dataimport/TestEvaluatorBag.java
@@ -34,6 +34,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -557,6 +567,11 @@ block|}
 comment|/**    * Test method for {@link EvaluatorBag#getDateFormatEvaluator()}.    */
 annotation|@
 name|Test
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Known Locale/TZ problems: see https://issues.apache.org/jira/browse/SOLR-1916"
+argument_list|)
 DECL|method|testGetDateFormatEvaluator
 specifier|public
 name|void
