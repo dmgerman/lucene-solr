4ab6015665369e6bdd748a53b238bc762bc85418
Uwe Schindler <uschindler@apache.org>
2010-11-19 18:22:04 +0000

    LUCENE-2770: Make SegmentMerger always work on atomic subreaders, even when IndexWriter.addIndexes(IndexReader...) is used with DirectoryReaders or other MultiReaders. This saves lots of memory during merge of norms
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1036970 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 24178a6eb86ee9193bb58416391f24ca1c1d5774

--- lucene/src/test/org/apache/lucene/index/TestSegmentMerger.java
+++ lucene/src/test/org/apache/lucene/index/TestSegmentMerger.java
@@ -423,11 +423,6 @@ operator|.
 name|merge
 argument_list|()
 decl_stmt|;
-name|merger
-operator|.
-name|closeReaders
-argument_list|()
-expr_stmt|;
 name|assertTrue
 argument_list|(
 name|docsMerged
