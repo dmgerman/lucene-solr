c8db1cb56f998c14de31553cc23b31e7986e2ddd
Robert Muir <rmuir@apache.org>
2010-12-18 21:20:46 +0000

    LUCENE-2819: set this before interuppting also, to prevent false fails
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1050718 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: fb7f94b419850b66a0c01edc565f762c74148fa8

--- lucene/src/test/org/apache/lucene/util/LuceneTestCase.java
+++ lucene/src/test/org/apache/lucene/util/LuceneTestCase.java
@@ -3123,6 +3123,13 @@ argument_list|(
 literal|null
 argument_list|)
 expr_stmt|;
+name|Thread
+operator|.
+name|setDefaultUncaughtExceptionHandler
+argument_list|(
+literal|null
+argument_list|)
+expr_stmt|;
 name|t
 operator|.
 name|interrupt
