b2a79cd888c610741d1bb0cf83d299f51f769f05
Michael McCandless <mikemccand@apache.org>
2010-12-19 10:17:50 +0000

    LUCENE-1737: set up FieldInfos correctly the when we open a pre-4.0 index
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1050790 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 89d6a37d4811bee8beb7b260f619e918d6b43a53

--- lucene/src/java/org/apache/lucene/index/IndexWriter.java
+++ lucene/src/java/org/apache/lucene/index/IndexWriter.java
@@ -2514,7 +2514,7 @@ argument_list|()
 operator|>
 name|DefaultSegmentInfosWriter
 operator|.
-name|FORMAT_4_0
+name|FORMAT_HAS_VECTORS
 condition|)
 block|{
 comment|// Pre-4.0 index.  In this case we sweep all
