9730143d7fd79e2da7321423e3bc6fc8c1f1ec01
Simon Willnauer <simonw@apache.org>
2011-01-24 13:51:06 +0000

    LUCENE-2882: Cut over SpanQuery#getSpans to AtomicReaderContext
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1062775 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ce53019db596ef94db813e00e10b00a208f0e889

--- lucene/src/test/org/apache/lucene/search/spans/JustCompileSearchSpans.java
+++ lucene/src/test/org/apache/lucene/search/spans/JustCompileSearchSpans.java
@@ -49,6 +49,8 @@ operator|.
 name|index
 operator|.
 name|IndexReader
+operator|.
+name|AtomicReaderContext
 import|;
 end_import
 
@@ -264,8 +266,8 @@ specifier|public
 name|Spans
 name|getSpans
 parameter_list|(
-name|IndexReader
-name|reader
+name|AtomicReaderContext
+name|context
 parameter_list|)
 throws|throws
 name|IOException
