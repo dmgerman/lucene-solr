30892adfec02f59257f1fcea9074de7eee04739c
Michael McCandless <mikemccand@apache.org>
2011-02-28 17:38:33 +0000

    LUCENE-2918: fix thread safety bug w/ assert statement
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1075443 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0e8612512489d4101655950618f7442e9ce66d50

--- lucene/src/java/org/apache/lucene/index/IndexWriter.java
+++ lucene/src/java/org/apache/lucene/index/IndexWriter.java
@@ -10343,7 +10343,7 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|reader
+name|clone
 operator|.
 name|numDocs
 argument_list|()
@@ -10358,7 +10358,6 @@ argument_list|(
 name|clone
 argument_list|)
 expr_stmt|;
-block|}
 name|totDocCount
 operator|+=
 name|clone
@@ -10366,6 +10365,7 @@ operator|.
 name|numDocs
 argument_list|()
 expr_stmt|;
+block|}
 name|segUpto
 operator|++
 expr_stmt|;
