7988bef3dcf1dbd84f8f12f1272d2ed2804e0bc0
Robert Muir <rmuir@apache.org>
2011-03-11 20:46:07 +0000

    ignore didnt work either, giving up and reverting
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1080753 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3ba04fee2cafc0e5058d9107fba1b398fb7cc967

--- solr/src/test/org/apache/solr/cloud/ZkSolrClientTest.java
+++ solr/src/test/org/apache/solr/cloud/ZkSolrClientTest.java
@@ -130,16 +130,6 @@ name|Watcher
 import|;
 end_import
 
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
-name|Ignore
-import|;
-end_import
-
 begin_class
 DECL|class|ZkSolrClientTest
 specifier|public
@@ -679,11 +669,6 @@ expr_stmt|;
 block|}
 block|}
 block|}
-annotation|@
-name|Ignore
-argument_list|(
-literal|"test case crashes modern jres"
-argument_list|)
 DECL|method|testWatchChildren
 specifier|public
 name|void
