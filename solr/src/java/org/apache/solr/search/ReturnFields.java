30da329ed5f3268ddfd5764de059198c14d6b703
Chris M. Hostetter <hossman@apache.org>
2011-04-12 17:58:20 +0000

    rip out excessive logging that was originally ment to be nocommit anyway (see r1085618)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1091522 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 961a548a2a5f91af30aacb134be7625a7c824458

--- solr/src/java/org/apache/solr/search/ReturnFields.java
+++ solr/src/java/org/apache/solr/search/ReturnFields.java
@@ -585,25 +585,6 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-name|SolrCore
-operator|.
-name|log
-operator|.
-name|info
-argument_list|(
-literal|"fields="
-operator|+
-name|fields
-operator|+
-literal|"\t globs="
-operator|+
-name|globs
-operator|+
-literal|"\t transformer="
-operator|+
-name|transformer
-argument_list|)
-expr_stmt|;
 block|}
 DECL|method|ReturnFields
 specifier|public
@@ -622,25 +603,6 @@ argument_list|(
 name|fl
 argument_list|,
 name|req
-argument_list|)
-expr_stmt|;
-name|SolrCore
-operator|.
-name|log
-operator|.
-name|info
-argument_list|(
-literal|"fields="
-operator|+
-name|fields
-operator|+
-literal|"\t globs="
-operator|+
-name|globs
-operator|+
-literal|"\t transformer="
-operator|+
-name|transformer
 argument_list|)
 expr_stmt|;
 block|}
