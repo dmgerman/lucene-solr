06cc9ff9bc7b8089cd8f9448c6114fbafe3d4a41
Simon Willnauer <simonw@apache.org>
2011-04-27 15:11:23 +0000

    added some debug information
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/realtime_search@1097138 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f89d7b4ee05f1960b17de38b568be37af3be2e70

--- lucene/src/java/org/apache/lucene/index/DocumentsWriterDeleteQueue.java
+++ lucene/src/java/org/apache/lucene/index/DocumentsWriterDeleteQueue.java
@@ -124,6 +124,10 @@ operator|new
 name|ReentrantLock
 argument_list|()
 decl_stmt|;
+DECL|field|generation
+name|long
+name|generation
+decl_stmt|;
 DECL|method|DocumentsWriterDeleteQueue
 name|DocumentsWriterDeleteQueue
 parameter_list|()
@@ -1166,6 +1170,22 @@ name|bytesUsed
 operator|.
 name|get
 argument_list|()
+return|;
+block|}
+annotation|@
+name|Override
+DECL|method|toString
+specifier|public
+name|String
+name|toString
+parameter_list|()
+block|{
+return|return
+literal|"DWDQ: [ generation: "
+operator|+
+name|generation
+operator|+
+literal|" ]"
 return|;
 block|}
 block|}
