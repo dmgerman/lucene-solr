c24b28d811232802b5a256d71f7b64348fa8de5b
Michael McCandless <mikemccand@apache.org>
2011-05-02 20:39:26 +0000

    LUCENE-3029: MultiPhraseQuery scores should not depend on docID
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1098782 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: fd0701bf4e0d4612cb44aad77c79e00f79f0fd69

--- lucene/src/java/org/apache/lucene/search/PhraseQueue.java
+++ lucene/src/java/org/apache/lucene/search/PhraseQueue.java
@@ -91,6 +91,29 @@ name|position
 condition|)
 comment|// same doc and pp.position, so decide by actual term positions.
 comment|// rely on: pp.position == tp.position - offset.
+if|if
+condition|(
+name|pp1
+operator|.
+name|offset
+operator|==
+name|pp2
+operator|.
+name|offset
+condition|)
+block|{
+return|return
+name|pp1
+operator|.
+name|ord
+operator|<
+name|pp2
+operator|.
+name|ord
+return|;
+block|}
+else|else
+block|{
 return|return
 name|pp1
 operator|.
@@ -100,7 +123,9 @@ name|pp2
 operator|.
 name|offset
 return|;
+block|}
 else|else
+block|{
 return|return
 name|pp1
 operator|.
@@ -110,7 +135,9 @@ name|pp2
 operator|.
 name|position
 return|;
+block|}
 else|else
+block|{
 return|return
 name|pp1
 operator|.
@@ -120,6 +147,7 @@ name|pp2
 operator|.
 name|doc
 return|;
+block|}
 block|}
 block|}
 end_class
