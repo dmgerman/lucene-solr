b08a931bb5eb307279789b0fdffe9ecbdd12009d
Dawid Weiss <dweiss@apache.org>
2011-06-14 12:03:56 +0000

    LUCENE-3202: Add DataInput/DataOutput subclasses that delegate to an InputStream/OutputStream.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1135506 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7614bedc9d90dc31b11b6d3830b3de53d43741b3

--- lucene/src/java/org/apache/lucene/store/OutputStreamDataOutput.java
+++ lucene/src/java/org/apache/lucene/store/OutputStreamDataOutput.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_package
-DECL|package|org.apache.lucene.search.suggest.fst
+DECL|package|org.apache.lucene.store
 package|package
 name|org
 operator|.
@@ -8,23 +8,13 @@ name|apache
 operator|.
 name|lucene
 operator|.
-name|search
-operator|.
-name|suggest
-operator|.
-name|fst
+name|store
 package|;
 end_package
 
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
-name|IOException
-import|;
-end_import
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
 
 begin_import
 import|import
@@ -32,21 +22,7 @@ name|java
 operator|.
 name|io
 operator|.
-name|OutputStream
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|lucene
-operator|.
-name|store
-operator|.
-name|DataOutput
+name|*
 import|;
 end_import
 
@@ -61,6 +37,8 @@ class|class
 name|OutputStreamDataOutput
 extends|extends
 name|DataOutput
+implements|implements
+name|Closeable
 block|{
 DECL|field|os
 specifier|private
@@ -134,6 +112,20 @@ name|offset
 argument_list|,
 name|length
 argument_list|)
+expr_stmt|;
+block|}
+DECL|method|close
+specifier|public
+name|void
+name|close
+parameter_list|()
+throws|throws
+name|IOException
+block|{
+name|os
+operator|.
+name|close
+argument_list|()
 expr_stmt|;
 block|}
 block|}
