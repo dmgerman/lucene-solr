e1fce12c93168f59305d8e6b7ff750f4af085e6b
Robert Muir <rmuir@apache.org>
2011-06-22 08:11:44 +0000

    LUCENE-3226: revert
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1138331 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8d61139abb4afeee7fdce78f3579c4375840e957

--- lucene/src/java/org/apache/lucene/index/SegmentInfos.java
+++ lucene/src/java/org/apache/lucene/index/SegmentInfos.java
@@ -1126,14 +1126,14 @@ comment|// either we are on 4.0 or we don't have a lastGlobalFieldMapVersion i.e
 assert|assert
 name|DefaultSegmentInfosWriter
 operator|.
-name|FORMAT_FLEX
+name|FORMAT_4_0
 operator|<=
 name|format
 operator|||
 operator|(
 name|DefaultSegmentInfosWriter
 operator|.
-name|FORMAT_FLEX
+name|FORMAT_4_0
 operator|>
 name|format
 operator|&&
@@ -3728,7 +3728,7 @@ name|format
 operator|>
 name|DefaultSegmentInfosWriter
 operator|.
-name|FORMAT_FLEX
+name|FORMAT_4_0
 condition|)
 block|{
 assert|assert
