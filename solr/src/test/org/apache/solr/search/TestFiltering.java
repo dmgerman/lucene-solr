6694951f0eee4e9d7677903360d959591968fe8f
Yonik Seeley <yonik@apache.org>
2011-06-28 18:53:39 +0000

    SOLR-2429: don't need to check deletions when iterating over filter
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1140786 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: abdb8e0e2f4e3974ee4a1176234e3727c8c95928

--- solr/src/test/org/apache/solr/search/TestFiltering.java
+++ solr/src/test/org/apache/solr/search/TestFiltering.java
@@ -1478,6 +1478,16 @@ name|commit
 argument_list|()
 argument_list|)
 expr_stmt|;
+name|int
+name|totalMatches
+init|=
+literal|0
+decl_stmt|;
+name|int
+name|nonZeros
+init|=
+literal|0
+decl_stmt|;
 for|for
 control|(
 name|int
@@ -1821,6 +1831,21 @@ operator|.
 name|cardinality
 argument_list|()
 decl_stmt|;
+name|totalMatches
+operator|+=
+name|expected
+expr_stmt|;
+if|if
+condition|(
+name|expected
+operator|>
+literal|0
+condition|)
+block|{
+name|nonZeros
+operator|++
+expr_stmt|;
+block|}
 if|if
 condition|(
 name|iiter
@@ -1928,6 +1953,9 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
+comment|// After making substantial changes to this test, make sure that we still get a
+comment|// decent number of queries that match some documents
+comment|// System.out.println("totalMatches=" + totalMatches + " nonZeroQueries="+nonZeros);
 block|}
 block|}
 block|}
