d4c26dfe66aaac3bb0419d68eb947e67dbf9e227
Christopher John Male <chrism@apache.org>
2011-07-06 00:28:34 +0000

    LUCENE-3274: Collapsed common module into lucene core util, removed module
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1143234 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 36330b50b439d79b3ef53049a5e2e7c5836d7e97

--- lucene/src/java/org/apache/lucene/util/mutable/MutableValueDate.java
+++ lucene/src/java/org/apache/lucene/util/mutable/MutableValueDate.java
@@ -4,7 +4,7 @@ comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or mor
 end_comment
 
 begin_package
-DECL|package|org.apache.lucene.common.mutable
+DECL|package|org.apache.lucene.util.mutable
 package|package
 name|org
 operator|.
@@ -12,7 +12,7 @@ name|apache
 operator|.
 name|lucene
 operator|.
-name|common
+name|util
 operator|.
 name|mutable
 package|;
