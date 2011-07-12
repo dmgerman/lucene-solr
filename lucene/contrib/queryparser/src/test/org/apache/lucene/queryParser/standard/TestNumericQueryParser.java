bf2cb5328df8c6ac3fc0cc033c24601f7c462be4
Robert Muir <rmuir@apache.org>
2011-07-12 05:08:35 +0000

    remove accidental line from debugging
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1145458 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f34671ca466cb80f21bc516a4c3af58931669c25

--- lucene/contrib/queryparser/src/test/org/apache/lucene/queryParser/standard/TestNumericQueryParser.java
+++ lucene/contrib/queryparser/src/test/org/apache/lucene/queryParser/standard/TestNumericQueryParser.java
@@ -599,13 +599,6 @@ argument_list|(
 name|random
 argument_list|)
 expr_stmt|;
-name|LOCALE
-operator|=
-name|Locale
-operator|.
-name|getDefault
-argument_list|()
-expr_stmt|;
 name|TIMEZONE
 operator|=
 name|randomTimeZone
