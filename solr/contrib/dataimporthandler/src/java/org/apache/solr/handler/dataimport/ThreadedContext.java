72c9f5405bd0eadd3ba0a0b930b3f17924d55c62
Shalin Shekhar Mangar <shalin@apache.org>
2011-07-15 08:41:26 +0000

    SOLR-2186 -- DataImportHandler's multi-threaded option throws NPE
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1147023 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 80d436e29f477396416aca1d84acd0b01c172c11

--- solr/contrib/dataimporthandler/src/java/org/apache/solr/handler/dataimport/ThreadedContext.java
+++ solr/contrib/dataimporthandler/src/java/org/apache/solr/handler/dataimport/ThreadedContext.java
@@ -65,7 +65,7 @@ name|entity
 argument_list|,
 literal|null
 argument_list|,
-comment|//to be fethed realtime
+comment|//to be fetched realtime
 literal|null
 argument_list|,
 literal|null
@@ -223,11 +223,25 @@ name|checkLimited
 argument_list|()
 expr_stmt|;
 return|return
-name|super
+name|entity
+operator|==
+literal|null
+condition|?
+literal|null
+else|:
+name|getVariableResolver
+argument_list|()
 operator|.
-name|getResolvedEntityAttribute
+name|replaceTokens
+argument_list|(
+name|entity
+operator|.
+name|allAttributes
+operator|.
+name|get
 argument_list|(
 name|name
+argument_list|)
 argument_list|)
 return|;
 block|}
