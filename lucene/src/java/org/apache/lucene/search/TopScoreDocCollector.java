0cac1ebb20dbe953715f7b8e2644309fd1044eb2
Michael McCandless <mikemccand@apache.org>
2011-07-21 16:17:00 +0000

    small optimization: don't add docBase unless score may be competitive
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1149256 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7fb6c2ecfe001fa2e60b5239c26ab27d1dc32046

--- lucene/src/java/org/apache/lucene/search/TopScoreDocCollector.java
+++ lucene/src/java/org/apache/lucene/search/TopScoreDocCollector.java
@@ -228,10 +228,6 @@ assert|;
 name|totalHits
 operator|++
 expr_stmt|;
-name|doc
-operator|+=
-name|docBase
-expr_stmt|;
 if|if
 condition|(
 name|score
@@ -239,8 +235,17 @@ operator|<
 name|pqTop
 operator|.
 name|score
-operator|||
-operator|(
+condition|)
+block|{
+comment|// Doesn't compete w/ bottom entry in queue
+return|return;
+block|}
+name|doc
+operator|+=
+name|docBase
+expr_stmt|;
+if|if
+condition|(
 name|score
 operator|==
 name|pqTop
@@ -252,9 +257,9 @@ operator|>
 name|pqTop
 operator|.
 name|doc
-operator|)
 condition|)
 block|{
+comment|// Break tie in score by doc ID:
 return|return;
 block|}
 name|pqTop
