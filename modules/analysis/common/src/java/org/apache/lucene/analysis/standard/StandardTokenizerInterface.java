809809968293c712053dae3878297379be6cb2e6
Robert Muir <rmuir@apache.org>
2011-08-04 20:49:47 +0000

    LUCENE-3358: StandardTokenizer wrongly discarded combining marks attached to Han/Hiragana
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1154005 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2dda5bc35f8c49a1443a4f65616e4a4e390def9b

--- modules/analysis/common/src/java/org/apache/lucene/analysis/standard/StandardTokenizerInterface.java
+++ modules/analysis/common/src/java/org/apache/lucene/analysis/standard/StandardTokenizerInterface.java
@@ -54,8 +54,13 @@ name|IOException
 import|;
 end_import
 
+begin_comment
+comment|/** @lucene.internal */
+end_comment
+
 begin_interface
 DECL|interface|StandardTokenizerInterface
+specifier|public
 interface|interface
 name|StandardTokenizerInterface
 block|{
@@ -72,6 +77,7 @@ literal|1
 decl_stmt|;
 comment|/**    * Copies the matched text into the CharTermAttribute    */
 DECL|method|getText
+specifier|public
 name|void
 name|getText
 parameter_list|(
@@ -81,12 +87,14 @@ parameter_list|)
 function_decl|;
 comment|/**    * Returns the current position.    */
 DECL|method|yychar
+specifier|public
 name|int
 name|yychar
 parameter_list|()
 function_decl|;
 comment|/**    * Resets the scanner to read from a new input stream.    * Does not close the old reader.    *    * All internal variables are reset, the old input stream     *<b>cannot</b> be reused (internal buffer is discarded and lost).    * Lexical state is set to<tt>ZZ_INITIAL</tt>.    *    * @param reader   the new input stream     */
 DECL|method|yyreset
+specifier|public
 name|void
 name|yyreset
 parameter_list|(
@@ -96,12 +104,14 @@ parameter_list|)
 function_decl|;
 comment|/**    * Returns the length of the matched text region.    */
 DECL|method|yylength
+specifier|public
 name|int
 name|yylength
 parameter_list|()
 function_decl|;
 comment|/**    * Resumes scanning until the next regular expression is matched,    * the end of input is encountered or an I/O-Error occurs.    *    * @return      the next token, {@link #YYEOF} on end of stream    * @exception   IOException  if any I/O-Error occurs    */
 DECL|method|getNextToken
+specifier|public
 name|int
 name|getNextToken
 parameter_list|()
