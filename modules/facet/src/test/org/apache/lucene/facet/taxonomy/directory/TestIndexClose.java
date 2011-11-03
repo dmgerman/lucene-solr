b9f93a3a051c0be789a8812d22956f9aafc96c83
Shai Erera <shaie@apache.org>
2011-11-03 07:41:40 +0000

    LUCENE-3556: make DirectoryTaxonomyWriter's indexWriter private
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1196982 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 23de4c514bfec96d8a2fc987441c1e7dab46588c

--- modules/facet/src/test/org/apache/lucene/facet/taxonomy/directory/TestIndexClose.java
+++ modules/facet/src/test/org/apache/lucene/facet/taxonomy/directory/TestIndexClose.java
@@ -805,7 +805,7 @@ annotation|@
 name|Override
 DECL|method|openIndexWriter
 specifier|protected
-name|void
+name|IndexWriter
 name|openIndexWriter
 parameter_list|(
 name|Directory
@@ -817,8 +817,7 @@ parameter_list|)
 throws|throws
 name|IOException
 block|{
-name|indexWriter
-operator|=
+return|return
 operator|new
 name|InstrumentedIndexWriter
 argument_list|(
@@ -846,7 +845,7 @@ argument_list|(
 name|openMode
 argument_list|)
 argument_list|)
-expr_stmt|;
+return|;
 block|}
 block|}
 DECL|class|InstrumentedTaxonomyReader
