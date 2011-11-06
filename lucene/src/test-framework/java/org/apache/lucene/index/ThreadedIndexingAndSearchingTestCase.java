d936d4f963d1f3dcfd3037d50db280e13a41d642
Michael McCandless <mikemccand@apache.org>
2011-11-06 13:52:58 +0000

    add a few asserts/checks, improve NRT tests
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1198371 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 64f8a1ffaf93a78f5c856fb8aaa77739ad684f32

--- lucene/src/test-framework/java/org/apache/lucene/index/ThreadedIndexingAndSearchingTestCase.java
+++ lucene/src/test-framework/java/org/apache/lucene/index/ThreadedIndexingAndSearchingTestCase.java
@@ -3339,6 +3339,14 @@ name|s
 argument_list|)
 expr_stmt|;
 block|}
+name|assertFalse
+argument_list|(
+name|failed
+operator|.
+name|get
+argument_list|()
+argument_list|)
+expr_stmt|;
 name|boolean
 name|doFail
 init|=
