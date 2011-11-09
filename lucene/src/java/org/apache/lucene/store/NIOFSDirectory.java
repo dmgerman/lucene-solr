53a598808b866dfbf6c946449e705cd118975fe6
Michael McCandless <mikemccand@apache.org>
2011-11-09 23:57:24 +0000

    LUCENE-3539: include resource in a few more cases
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1200051 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c0c36d00d638e1df132fa2356c6dddc1f9b046a1

--- lucene/src/java/org/apache/lucene/store/NIOFSDirectory.java
+++ lucene/src/java/org/apache/lucene/store/NIOFSDirectory.java
@@ -32,6 +32,16 @@ name|java
 operator|.
 name|io
 operator|.
+name|EOFException
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
 name|IOException
 import|;
 end_import
@@ -726,11 +736,13 @@ condition|)
 block|{
 throw|throw
 operator|new
-name|IOException
+name|EOFException
 argument_list|(
-literal|"read past EOF: "
+literal|"read past EOF (resource: "
 operator|+
 name|this
+operator|+
+literal|")"
 argument_list|)
 throw|;
 block|}
