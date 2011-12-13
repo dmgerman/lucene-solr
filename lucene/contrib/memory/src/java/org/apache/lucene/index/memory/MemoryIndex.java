57f67862528e83dfc1ecbcd06afa22533b2ed30c
Robert Muir <rmuir@apache.org>
2011-12-13 20:20:43 +0000

    LUCENE-3644: fix problems with IR's readerFinishedListener
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1213910 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1a4c0ff254dfe41c38c5948fe754bf2b238eece1

--- lucene/contrib/memory/src/java/org/apache/lucene/index/memory/MemoryIndex.java
+++ lucene/contrib/memory/src/java/org/apache/lucene/index/memory/MemoryIndex.java
@@ -94,16 +94,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|HashSet
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|Iterator
 import|;
 end_import
@@ -3256,20 +3246,6 @@ name|super
 argument_list|()
 expr_stmt|;
 comment|// avoid as much superclass baggage as possible
-name|readerFinishedListeners
-operator|=
-name|Collections
-operator|.
-name|synchronizedSet
-argument_list|(
-operator|new
-name|HashSet
-argument_list|<
-name|ReaderFinishedListener
-argument_list|>
-argument_list|()
-argument_list|)
-expr_stmt|;
 block|}
 DECL|method|getInfo
 specifier|private
