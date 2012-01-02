c80258feaa23f08faae2ddc1d862805e3693d7f3
Robert Muir <rmuir@apache.org>
2012-01-02 16:16:34 +0000

    LUCENE-3667: fix broken test optimization (when n > 1000)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1226455 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a4fc20d91ece6b62bafda798f2773ad9bf1f5eb7

--- lucene/src/test/org/apache/lucene/index/TestIndexWriter.java
+++ lucene/src/test/org/apache/lucene/index/TestIndexWriter.java
@@ -3396,9 +3396,8 @@ argument_list|(
 name|reader
 argument_list|)
 decl_stmt|;
-name|ScoreDoc
-index|[]
-name|hits
+name|int
+name|totalHits
 init|=
 name|searcher
 operator|.
@@ -3418,10 +3417,10 @@ argument_list|)
 argument_list|,
 literal|null
 argument_list|,
-literal|1000
+literal|1
 argument_list|)
 operator|.
-name|scoreDocs
+name|totalHits
 decl_stmt|;
 name|assertEquals
 argument_list|(
@@ -3429,9 +3428,7 @@ name|n
 operator|*
 literal|100
 argument_list|,
-name|hits
-operator|.
-name|length
+name|totalHits
 argument_list|)
 expr_stmt|;
 name|reader
