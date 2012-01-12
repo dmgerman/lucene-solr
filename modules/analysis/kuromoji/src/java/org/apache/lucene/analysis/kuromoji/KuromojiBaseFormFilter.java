bbb5ef70b6402b16ca4a050affe7979ea67a2f63
Robert Muir <rmuir@apache.org>
2012-01-12 20:36:58 +0000

    LUCENE-3305: optimization, don't retrieve the base form twice in this filter
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1230769 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 05a65507af1356df29bd7606e531397d4f79f67f

--- modules/analysis/kuromoji/src/java/org/apache/lucene/analysis/kuromoji/KuromojiBaseFormFilter.java
+++ modules/analysis/kuromoji/src/java/org/apache/lucene/analysis/kuromoji/KuromojiBaseFormFilter.java
@@ -237,10 +237,7 @@ argument_list|()
 operator|.
 name|append
 argument_list|(
-name|basicFormAtt
-operator|.
-name|getBaseForm
-argument_list|()
+name|baseForm
 argument_list|)
 expr_stmt|;
 block|}
