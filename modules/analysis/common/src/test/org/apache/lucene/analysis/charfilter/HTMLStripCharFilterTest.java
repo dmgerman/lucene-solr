39302208d06f5f20d6dbd9b47b0c1844cc7ade6c
Robert Muir <rmuir@apache.org>
2012-01-13 19:46:30 +0000

    LUCENE-3690: since this filter handles all kinds of bad partial and wierd input, this should be fine to enable
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1231272 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8b8c2b4dee5a792048f08c0fd3d58abb87b0ad12

--- modules/analysis/common/src/test/org/apache/lucene/analysis/charfilter/HTMLStripCharFilterTest.java
+++ modules/analysis/common/src/test/org/apache/lucene/analysis/charfilter/HTMLStripCharFilterTest.java
@@ -1761,11 +1761,6 @@ literal|"X<&zz>X&#< X><&l>&g< X"
 argument_list|)
 expr_stmt|;
 block|}
-annotation|@
-name|Ignore
-argument_list|(
-literal|"broken offsets: see LUCENE-2208"
-argument_list|)
 DECL|method|testRandom
 specifier|public
 name|void
