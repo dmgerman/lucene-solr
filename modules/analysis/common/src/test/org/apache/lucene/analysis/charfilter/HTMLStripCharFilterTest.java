be9ea3ad54f05aa4614c50a61086575ff0f8b205
Michael McCandless <mikemccand@apache.org>
2012-01-14 15:20:58 +0000

    woops
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1231513 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5ca66287ea9f539c8f5b81547305ab55436b62b9

--- modules/analysis/common/src/test/org/apache/lucene/analysis/charfilter/HTMLStripCharFilterTest.java
+++ modules/analysis/common/src/test/org/apache/lucene/analysis/charfilter/HTMLStripCharFilterTest.java
@@ -178,13 +178,7 @@ name|Ignore
 import|;
 end_import
 
-begin_comment
-comment|// nocommit
-end_comment
-
 begin_class
-annotation|@
-name|Ignore
 DECL|class|HTMLStripCharFilterTest
 specifier|public
 class|class
