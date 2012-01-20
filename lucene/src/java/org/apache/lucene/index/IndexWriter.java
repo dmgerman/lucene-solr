745582abf24aebae9ddf30ad4d1c4159e071a9ed
Robert Muir <rmuir@apache.org>
2012-01-20 14:47:42 +0000

    LUCENE-3661: nuke obselete nocommit, thanks Mike
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene3661@1233935 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f6b18248a9f556f90f2400db109f44a53febd9b5

--- lucene/src/java/org/apache/lucene/index/IndexWriter.java
+++ lucene/src/java/org/apache/lucene/index/IndexWriter.java
@@ -1774,7 +1774,6 @@ literal|null
 assert|;
 block|}
 block|}
-comment|// nocommit: if this is read-only live docs, why doesn't it return Bits?!
 DECL|method|getReadOnlyLiveDocs
 specifier|public
 specifier|synchronized
