d9b472e3f37c317797c00781bd6a891daff3cb0e
Mark Robert Miller <markrmiller@apache.org>
2012-01-27 03:49:51 +0000

    ensure that a newly opened searcher has a non null newIndexDir to work from
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1236509 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ccee2bca8f4b74cf661019dc705c4f08827d0436

--- solr/core/src/java/org/apache/solr/core/SolrCore.java
+++ solr/core/src/java/org/apache/solr/core/SolrCore.java
@@ -4915,7 +4915,8 @@ block|{
 name|String
 name|newIndexDir
 init|=
-literal|null
+name|getNewIndexDir
+argument_list|()
 decl_stmt|;
 name|File
 name|indexDirFile
@@ -4944,11 +4945,6 @@ argument_list|()
 argument_list|)
 operator|.
 name|getCanonicalFile
-argument_list|()
-expr_stmt|;
-name|newIndexDir
-operator|=
-name|getNewIndexDir
 argument_list|()
 expr_stmt|;
 name|newIndexDirFile
