3585c49eff85827ec379526f9856cfb9c5cdd9e8
Michael McCandless <mikemccand@apache.org>
2012-01-28 17:59:45 +0000

    LUCENE-3661: simplify del count tracking during merge
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene3661@1237114 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1b8d8b435088dcd53544f27913af21e1e457745f

--- lucene/src/java/org/apache/lucene/index/MergeState.java
+++ lucene/src/java/org/apache/lucene/index/MergeState.java
@@ -28,16 +28,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|util
-operator|.
-name|Map
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
@@ -192,17 +182,6 @@ index|[]
 name|docBase
 decl_stmt|;
 comment|// New docID base per reader
-DECL|field|segmentDocCounts
-specifier|public
-name|Map
-argument_list|<
-name|SegmentInfo
-argument_list|,
-name|Integer
-argument_list|>
-name|segmentDocCounts
-decl_stmt|;
-comment|// Non-deleted docCount per reader
 DECL|field|mergedDocCount
 specifier|public
 name|int
