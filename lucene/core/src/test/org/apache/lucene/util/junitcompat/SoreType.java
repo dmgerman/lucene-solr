6ecc99dfa0233092f33513746c135d0621617489
Dawid Weiss <dweiss@apache.org>
2012-02-14 10:27:16 +0000

    LUCENE-3762: Upgrade JUnit to 4.10, refactor state-machine of detecting setUp/tearDown call chaining.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1243863 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 213ad3290a174b888dfe709b4ec52ed18382f4e5

---
+++ lucene/core/src/test/org/apache/lucene/util/junitcompat/SoreType.java
@@ -0,0 +1,35 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+DECL|package|org.apache.lucene.util.junitcompat
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|util
+operator|.
+name|junitcompat
+package|;
+end_package
+
+begin_enum
+DECL|enum|SoreType
+specifier|public
+enum|enum
+name|SoreType
+block|{
+DECL|enum constant|ASSUMPTION
+name|ASSUMPTION
+block|,
+DECL|enum constant|FAILURE
+name|FAILURE
+block|,
+DECL|enum constant|ERROR
+name|ERROR
+block|}
+end_enum
+
+end_unit
+
