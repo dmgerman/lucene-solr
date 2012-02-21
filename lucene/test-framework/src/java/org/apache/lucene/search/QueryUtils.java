9ae50eaf93ccc9c1c123cf64eed7c31e357d2317
Robert Muir <rmuir@apache.org>
2012-02-21 22:29:03 +0000

    pass wrap=false in queryutils temporarily, since we are causing insanity this way if the reader is already atomic
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1292063 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2e2d3dac916041e3c24a6aa8c9d928d281885cde

--- lucene/test-framework/src/java/org/apache/lucene/search/QueryUtils.java
+++ lucene/test-framework/src/java/org/apache/lucene/search/QueryUtils.java
@@ -953,6 +953,9 @@ index|]
 argument_list|)
 block|}
 decl_stmt|;
+comment|// TODO: fix me,
+comment|//   wrapping causes insanity when we have an already-atomic reader?!
+comment|// IndexSearcher out = LuceneTestCase.newSearcher(new MultiReader(readers));
 name|IndexSearcher
 name|out
 init|=
@@ -965,6 +968,8 @@ name|MultiReader
 argument_list|(
 name|readers
 argument_list|)
+argument_list|,
+literal|false
 argument_list|)
 decl_stmt|;
 name|out
