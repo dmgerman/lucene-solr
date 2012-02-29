34964e7a238e9509aa7b787044f7af5c93c7ff14
Michael McCandless <mikemccand@apache.org>
2012-02-29 09:19:25 +0000

    LUCENE-3824: disable randomStringSort for Lucene3x codec (it doesn't support doc values)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1295027 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a4916d13677b92b52ff3d9e4e843e6cf94f60dae

--- lucene/core/src/test/org/apache/lucene/search/TestSort.java
+++ lucene/core/src/test/org/apache/lucene/search/TestSort.java
@@ -10950,6 +10950,14 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+name|assumeTrue
+argument_list|(
+literal|"cannot work with Lucene3x codec"
+argument_list|,
+name|defaultCodecSupportsDocValues
+argument_list|()
+argument_list|)
+expr_stmt|;
 specifier|final
 name|int
 name|NUM_DOCS
