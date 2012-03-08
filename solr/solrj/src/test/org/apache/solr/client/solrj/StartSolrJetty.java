a6e52a96cd0afead76d997621840e5ec47cf11ec
Ryan McKinley <ryan@apache.org>
2012-03-08 19:26:49 +0000

    StartSolrJetty now emulates java -jar start.jar
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1298528 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2488497c69e4465838bb947e13b7b1336e7682d0

--- solr/solrj/src/test/org/apache/solr/client/solrj/StartSolrJetty.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/StartSolrJetty.java
@@ -143,7 +143,7 @@ name|connector
 operator|.
 name|setPort
 argument_list|(
-literal|8080
+literal|8983
 argument_list|)
 expr_stmt|;
 name|server
@@ -176,7 +176,7 @@ name|bb
 operator|.
 name|setContextPath
 argument_list|(
-literal|"/"
+literal|"/solr"
 argument_list|)
 expr_stmt|;
 name|bb
