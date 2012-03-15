b63e0e925236b12d34979e132dbc81ae9fe2408e
Martijn van Groningen <mvg@apache.org>
2012-03-15 22:09:05 +0000

    Rename docvalues to values
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1301238 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9a15b3f4496e67747c63977be578b9f1dd15dc3c

--- modules/grouping/src/java/org/apache/lucene/search/grouping/function/FunctionAllGroupHeadsCollector.java
+++ modules/grouping/src/java/org/apache/lucene/search/grouping/function/FunctionAllGroupHeadsCollector.java
@@ -549,7 +549,7 @@ operator|=
 name|context
 expr_stmt|;
 name|FunctionValues
-name|docValues
+name|values
 init|=
 name|groupBy
 operator|.
@@ -562,7 +562,7 @@ argument_list|)
 decl_stmt|;
 name|filler
 operator|=
-name|docValues
+name|values
 operator|.
 name|getValueFiller
 argument_list|()
