fde8a1128b2fb9633e3d850305072529cd2f7d9e
Robert Muir <rmuir@apache.org>
2012-03-26 13:28:01 +0000

    add test for noCFSRatio setter (thanks Tomas for the idea)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1305346 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: aad3a816abf2d7b00069f7ef3ecde6c87bd5c54d

--- solr/core/src/test/org/apache/solr/core/TestMergePolicyConfig.java
+++ solr/core/src/test/org/apache/solr/core/TestMergePolicyConfig.java
@@ -201,6 +201,20 @@ argument_list|,
 literal|0.001
 argument_list|)
 expr_stmt|;
+comment|// make sure we overrode noCFSRatio (useless because we disabled useCompoundFile,
+comment|// but just to make sure it works)
+name|assertEquals
+argument_list|(
+literal|1.0D
+argument_list|,
+name|tieredMP
+operator|.
+name|getNoCFSRatio
+argument_list|()
+argument_list|,
+literal|0.001
+argument_list|)
+expr_stmt|;
 block|}
 block|}
 end_class
