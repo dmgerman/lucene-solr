8b55cc380332295294f9ee4d3f3399089e75e743
Uwe Schindler <uschindler@apache.org>
2012-04-01 13:52:16 +0000

    LUCENE-3738: Add random test for vints and vlongs, cleanup code and make more clear for longs
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1308117 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5ab0204216e73df587f1e8ef6240c8a6143e9a55

--- lucene/core/src/java/org/apache/lucene/store/DataOutput.java
+++ lucene/core/src/java/org/apache/lucene/store/DataOutput.java
@@ -258,7 +258,7 @@ argument_list|(
 operator|(
 name|i
 operator|&
-literal|0x7f
+literal|0x7F
 operator|)
 operator||
 literal|0x80
@@ -336,10 +336,10 @@ operator|(
 name|i
 operator|&
 operator|~
-literal|0x7F
+literal|0x7FL
 operator|)
 operator|!=
-literal|0
+literal|0L
 condition|)
 block|{
 name|writeByte
@@ -351,10 +351,10 @@ argument_list|(
 operator|(
 name|i
 operator|&
-literal|0x7f
+literal|0x7FL
 operator|)
 operator||
-literal|0x80
+literal|0x80L
 argument_list|)
 argument_list|)
 expr_stmt|;
