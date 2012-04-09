a49cab19d44f330957aedf8519554069b4d18e8b
Michael McCandless <mikemccand@apache.org>
2012-04-09 20:04:55 +0000

    LUCENE-3969: check that startOffset <= endOffset; comment out sops
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene3969@1311406 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9e98ec186cb042546bb98172327585f60b17ce2f

--- lucene/test-framework/src/java/org/apache/lucene/analysis/ValidatingTokenFilter.java
+++ lucene/test-framework/src/java/org/apache/lucene/analysis/ValidatingTokenFilter.java
@@ -392,6 +392,37 @@ operator|.
 name|endOffset
 argument_list|()
 decl_stmt|;
+if|if
+condition|(
+name|endOffset
+operator|<
+name|startOffset
+condition|)
+block|{
+throw|throw
+operator|new
+name|IllegalStateException
+argument_list|(
+name|name
+operator|+
+literal|": startOffset="
+operator|+
+name|startOffset
+operator|+
+literal|" is> endOffset="
+operator|+
+name|endOffset
+operator|+
+literal|" pos="
+operator|+
+name|pos
+operator|+
+literal|"; token="
+operator|+
+name|termAtt
+argument_list|)
+throw|;
+block|}
 specifier|final
 name|int
 name|posLen
@@ -428,41 +459,13 @@ argument_list|,
 name|startOffset
 argument_list|)
 expr_stmt|;
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|(
-literal|"  + s "
-operator|+
-name|pos
-operator|+
-literal|" -> "
-operator|+
-name|startOffset
-argument_list|)
-expr_stmt|;
+comment|//System.out.println("  + s " + pos + " -> " + startOffset);
 block|}
 else|else
 block|{
 comment|// We've seen a token leaving from this position
 comment|// before; verify the startOffset is the same:
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|(
-literal|"  + vs "
-operator|+
-name|pos
-operator|+
-literal|" -> "
-operator|+
-name|startOffset
-argument_list|)
-expr_stmt|;
+comment|//System.out.println("  + vs " + pos + " -> " + startOffset);
 specifier|final
 name|int
 name|oldStartOffset
@@ -487,7 +490,7 @@ name|IllegalStateException
 argument_list|(
 name|name
 operator|+
-literal|": inconsistent startOffset as pos="
+literal|": inconsistent startOffset at pos="
 operator|+
 name|pos
 operator|+
@@ -535,41 +538,13 @@ argument_list|,
 name|endOffset
 argument_list|)
 expr_stmt|;
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|(
-literal|"  + e "
-operator|+
-name|endPos
-operator|+
-literal|" -> "
-operator|+
-name|endOffset
-argument_list|)
-expr_stmt|;
+comment|//System.out.println("  + e " + endPos + " -> " + endOffset);
 block|}
 else|else
 block|{
 comment|// We've seen a token arriving to this position
 comment|// before; verify the endOffset is the same:
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|(
-literal|"  + ve "
-operator|+
-name|endPos
-operator|+
-literal|" -> "
-operator|+
-name|endOffset
-argument_list|)
-expr_stmt|;
+comment|//System.out.println("  + ve " + endPos + " -> " + endOffset);
 specifier|final
 name|int
 name|oldEndOffset
@@ -594,7 +569,7 @@ name|IllegalStateException
 argument_list|(
 name|name
 operator|+
-literal|": inconsistent endOffset as pos="
+literal|": inconsistent endOffset at pos="
 operator|+
 name|endPos
 operator|+
