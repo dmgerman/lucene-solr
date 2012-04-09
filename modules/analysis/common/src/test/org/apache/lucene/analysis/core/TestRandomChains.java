1c28be5cbcaaf7fd4783bc1728dc237e3d47f6b2
Uwe Schindler <uschindler@apache.org>
2012-04-09 15:15:11 +0000

    LUCENE-3969: Minor cleanups and code consistency
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene3969@1311278 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 214ab39f68c7e6fbd92048af5e15a5cabc2ab5dc

--- modules/analysis/common/src/test/org/apache/lucene/analysis/core/TestRandomChains.java
+++ modules/analysis/common/src/test/org/apache/lucene/analysis/core/TestRandomChains.java
@@ -2310,13 +2310,17 @@ name|Exception
 name|ex
 parameter_list|)
 block|{
-throw|throw
-operator|new
-name|RuntimeException
+name|Rethrow
+operator|.
+name|rethrow
 argument_list|(
 name|ex
 argument_list|)
-throw|;
+expr_stmt|;
+return|return
+literal|null
+return|;
+comment|// unreachable code
 block|}
 block|}
 block|}
@@ -2938,16 +2942,20 @@ block|}
 catch|catch
 parameter_list|(
 name|Exception
-name|e
+name|ex
 parameter_list|)
 block|{
-throw|throw
-operator|new
-name|RuntimeException
+name|Rethrow
+operator|.
+name|rethrow
 argument_list|(
-name|e
+name|ex
 argument_list|)
-throw|;
+expr_stmt|;
+return|return
+literal|null
+return|;
+comment|// unreachable code
 block|}
 block|}
 specifier|private
@@ -3176,6 +3184,7 @@ operator|.
 name|class
 argument_list|)
 expr_stmt|;
+comment|// TODO: fix this one, thats broken:
 name|allowedTokenFilterArgs
 operator|.
 name|add
@@ -3238,13 +3247,13 @@ name|SuppressWarnings
 argument_list|(
 literal|"unchecked"
 argument_list|)
-DECL|method|createRandomArg
+DECL|method|newRandomArg
 specifier|static
 parameter_list|<
 name|T
 parameter_list|>
 name|T
-name|createRandomArg
+name|newRandomArg
 parameter_list|(
 name|Random
 name|random
@@ -3406,9 +3415,10 @@ index|[
 name|i
 index|]
 operator|=
-literal|null
+operator|new
+name|AttributeSource
+argument_list|()
 expr_stmt|;
-comment|// this always gives IAE: fine
 block|}
 else|else
 block|{
@@ -3417,7 +3427,7 @@ index|[
 name|i
 index|]
 operator|=
-name|createRandomArg
+name|newRandomArg
 argument_list|(
 name|random
 argument_list|,
@@ -3537,7 +3547,7 @@ index|[
 name|i
 index|]
 operator|=
-name|createRandomArg
+name|newRandomArg
 argument_list|(
 name|random
 argument_list|,
@@ -3637,7 +3647,7 @@ operator|.
 name|class
 condition|)
 block|{
-comment|// CommonGramsQueryFilter takes this one explicitly
+comment|// TODO: fix this one, thats broken: CommonGramsQueryFilter takes this one explicitly
 name|args
 index|[
 name|i
@@ -3650,7 +3660,7 @@ name|TEST_VERSION_CURRENT
 argument_list|,
 name|stream
 argument_list|,
-name|createRandomArg
+name|newRandomArg
 argument_list|(
 name|random
 argument_list|,
@@ -3668,7 +3678,7 @@ index|[
 name|i
 index|]
 operator|=
-name|createRandomArg
+name|newRandomArg
 argument_list|(
 name|random
 argument_list|,
