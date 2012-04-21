5e8575bceaeba1b93cf6e4a2f30ded0c2ff2d036
Robert Muir <rmuir@apache.org>
2012-04-21 07:51:46 +0000

    LUCENE-3977: remove javadoc duplication in the website/binary release
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1328605 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: dad1d2808543e8c27a365d24e65f48c8f93ce24a

--- lucene/join/src/java/org/apache/lucene/search/join/JoinUtil.java
+++ lucene/join/src/java/org/apache/lucene/search/join/JoinUtil.java
@@ -57,7 +57,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Utility for query time joining using {@link TermsQuery} and {@link TermsCollector}.  *  * @lucene.experimental  */
+comment|/**  * Utility for query time joining using TermsQuery and TermsCollector.  *  * @lucene.experimental  */
 end_comment
 
 begin_class
