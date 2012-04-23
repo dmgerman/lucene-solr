15d8f767c9e02e6f4f2662a3379ab1ed9c0c75c9
Robert Muir <rmuir@apache.org>
2012-04-23 02:20:59 +0000

    don't create broken javadocs links in subclasses, TermCollectingRewrite is already pkg-private
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1329027 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 137beb2463565fbce77c8c9b67572fe3c177e9fe

--- lucene/core/src/java/org/apache/lucene/search/TermCollectingRewrite.java
+++ lucene/core/src/java/org/apache/lucene/search/TermCollectingRewrite.java
@@ -262,7 +262,6 @@ throws|throws
 name|IOException
 function_decl|;
 DECL|method|collectTerms
-specifier|protected
 specifier|final
 name|void
 name|collectTerms
@@ -480,7 +479,6 @@ block|}
 block|}
 block|}
 DECL|class|TermCollector
-specifier|protected
 specifier|static
 specifier|abstract
 class|class
