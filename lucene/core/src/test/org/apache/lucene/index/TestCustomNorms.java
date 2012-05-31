44830433250e433a720f7bc5d98b3556b6beba60
Robert Muir <rmuir@apache.org>
2012-05-31 17:32:11 +0000

    LUCENE-4096: impossible to checkindex if you use norms other than byte[]
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1344826 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 168e73bca7817c08590203fd57fbb9ebfe513575

--- lucene/core/src/test/org/apache/lucene/index/TestCustomNorms.java
+++ lucene/core/src/test/org/apache/lucene/index/TestCustomNorms.java
@@ -228,36 +228,11 @@ name|LuceneTestCase
 import|;
 end_import
 
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|lucene
-operator|.
-name|util
-operator|.
-name|LuceneTestCase
-operator|.
-name|SuppressCodecs
-import|;
-end_import
-
 begin_comment
 comment|/**  *   */
 end_comment
 
-begin_comment
-comment|// TODO: what is the problem with SimpleText
-end_comment
-
 begin_class
-annotation|@
-name|SuppressCodecs
-argument_list|(
-literal|"SimpleText"
-argument_list|)
 DECL|class|TestCustomNorms
 specifier|public
 class|class
@@ -293,15 +268,6 @@ init|=
 name|newDirectory
 argument_list|()
 decl_stmt|;
-comment|// TODO: what is the checkindex problem?
-name|dir
-operator|.
-name|setCheckIndexOnClose
-argument_list|(
-literal|false
-argument_list|)
-expr_stmt|;
-comment|// can't set sim to checkindex yet
 name|IndexWriterConfig
 name|config
 init|=
@@ -621,14 +587,6 @@ init|=
 name|newDirectory
 argument_list|()
 decl_stmt|;
-name|dir
-operator|.
-name|setCheckIndexOnClose
-argument_list|(
-literal|false
-argument_list|)
-expr_stmt|;
-comment|// can't set sim to checkindex yet
 name|IndexWriterConfig
 name|config
 init|=
