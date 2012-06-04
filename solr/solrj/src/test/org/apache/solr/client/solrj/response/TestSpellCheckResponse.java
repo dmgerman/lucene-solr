18b9eaa2dd4ecedaee3144e106e907ec3434a10e
James Dyer <jdyer@apache.org>
2012-06-04 19:48:26 +0000

    SOLR-2993:  fix (2) for fail on TestSpellCheckResponse:  ConjunctionSolrSpellChecker was not preserving original Token Freqs.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1346116 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 050f6c7a3c4e12ede3aa89dff06215b4904aac43

--- solr/solrj/src/test/org/apache/solr/client/solrj/response/TestSpellCheckResponse.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/response/TestSpellCheckResponse.java
@@ -314,7 +314,7 @@ init|=
 operator|new
 name|SolrQuery
 argument_list|(
-literal|"*:*"
+literal|"name:samsang"
 argument_list|)
 decl_stmt|;
 name|query
@@ -335,17 +335,6 @@ argument_list|(
 literal|"spellcheck"
 argument_list|,
 literal|true
-argument_list|)
-expr_stmt|;
-name|query
-operator|.
-name|set
-argument_list|(
-name|SpellingParams
-operator|.
-name|SPELLCHECK_Q
-argument_list|,
-literal|"samsang"
 argument_list|)
 expr_stmt|;
 name|query
