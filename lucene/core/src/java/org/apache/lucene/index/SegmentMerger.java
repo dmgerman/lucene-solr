cc7064cf3e7737c21b4d66e734bb4a8eb83dbe34
Robert Muir <rmuir@apache.org>
2012-06-11 15:32:34 +0000

    LUCENE-4086: remove 3.x cruft
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1348907 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b7a1c626dca592cf252be70765c504d77f10b121

--- lucene/core/src/java/org/apache/lucene/index/SegmentMerger.java
+++ lucene/core/src/java/org/apache/lucene/index/SegmentMerger.java
@@ -1877,17 +1877,11 @@ name|segmentWriteState
 argument_list|)
 argument_list|)
 decl_stmt|;
-comment|// TODO: remove this check when 3.x indexes are no longer supported
-comment|// (3.x indexes don't have docvalues)
-if|if
-condition|(
+assert|assert
 name|docsConsumer
-operator|==
+operator|!=
 literal|null
-condition|)
-block|{
-return|return;
-block|}
+assert|;
 name|boolean
 name|success
 init|=
@@ -1963,17 +1957,11 @@ name|segmentWriteState
 argument_list|)
 argument_list|)
 decl_stmt|;
-comment|// TODO: remove this check when 3.x indexes are no longer supported
-comment|// (3.x indexes don't have docvalues)
-if|if
-condition|(
+assert|assert
 name|docsConsumer
-operator|==
+operator|!=
 literal|null
-condition|)
-block|{
-return|return;
-block|}
+assert|;
 name|boolean
 name|success
 init|=
