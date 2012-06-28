994525cacb7303311de073fb7b07229b71b10ac1
Jan Høydahl <janhoy@apache.org>
2012-06-28 08:56:18 +0000

    Fixing Javadoc warnings to make javadocs-all succeed
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1354864 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e709e713d2cdddbcaa833721d4c97b69ed5e1609

--- lucene/spatial/src/java/org/apache/lucene/spatial/bbox/BBoxSimilarityValueSource.java
+++ lucene/spatial/src/java/org/apache/lucene/spatial/bbox/BBoxSimilarityValueSource.java
@@ -194,7 +194,6 @@ specifier|final
 name|BBoxSimilarity
 name|similarity
 decl_stmt|;
-comment|/**    * Constructor.    *    * @param queryEnvelope the query envelope    * @param queryPower the query power (scoring algorithm)    * @param targetPower the target power (scoring algorithm)    */
 DECL|method|BBoxSimilarityValueSource
 specifier|public
 name|BBoxSimilarityValueSource
@@ -236,7 +235,7 @@ operator|+
 literal|")"
 return|;
 block|}
-comment|/**    * Returns the DocValues used by the function query.    *    * @param reader the index reader    * @return the values    */
+comment|/**    * Returns the DocValues used by the function query.    *    * @param readerContext the AtomicReaderContext which holds an AtomicReader    * @return the values    */
 annotation|@
 name|Override
 DECL|method|getValues
