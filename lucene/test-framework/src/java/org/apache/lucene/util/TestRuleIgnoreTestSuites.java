f959a0e45fc8d3eae590b4142e56bb3105640b4d
Dawid Weiss <dweiss@apache.org>
2012-06-29 08:07:56 +0000

    LUCENE-4160: Bring back the functional equivalent of tests.iters.min. Added two properties: -Dtests.maxfailures=M and -Dtests.failfast=[true/false/yes/no/on/off] which control skipping tests after the first M failures have been hit or after the first failure (failfast is an alias for tests.maxfailures=1).
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1355258 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 63eefa822f830cf24abb4c756b8018ea3e488ff1

--- lucene/test-framework/src/java/org/apache/lucene/util/TestRuleIgnoreTestSuites.java
+++ lucene/test-framework/src/java/org/apache/lucene/util/TestRuleIgnoreTestSuites.java
@@ -18,16 +18,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Assume
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|rules
 operator|.
 name|TestRule
