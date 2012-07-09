c52c571bd641f5db7073a4e567955f911846620e
Robert Muir <rmuir@apache.org>
2012-07-09 14:06:17 +0000

    LUCENE-4199: fix more default locale/charset violations
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene4199@1359181 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 46cb8b97807d08ee76883f4818cec0ca25c71e88

--- solr/solrj/src/java/org/apache/solr/common/util/ContentStreamBase.java
+++ solr/solrj/src/java/org/apache/solr/common/util/ContentStreamBase.java
@@ -54,16 +54,6 @@ name|java
 operator|.
 name|io
 operator|.
-name|FileReader
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
 name|IOException
 import|;
 end_import
@@ -135,6 +125,20 @@ operator|.
 name|util
 operator|.
 name|Locale
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|util
+operator|.
+name|IOUtils
 import|;
 end_import
 
@@ -469,7 +473,7 @@ name|file
 argument_list|)
 return|;
 block|}
-comment|/**      * If an charset is defined (by the contentType) use that, otherwise       * use a file reader      */
+comment|/**      * If an charset is defined (by the contentType) use that, otherwise       * use a UTF-8 reader      */
 annotation|@
 name|Override
 DECL|method|getReader
@@ -494,9 +498,14 @@ operator|==
 literal|null
 condition|?
 operator|new
-name|FileReader
+name|InputStreamReader
 argument_list|(
-name|file
+name|getStream
+argument_list|()
+argument_list|,
+name|IOUtils
+operator|.
+name|CHARSET_UTF_8
 argument_list|)
 else|:
 operator|new
