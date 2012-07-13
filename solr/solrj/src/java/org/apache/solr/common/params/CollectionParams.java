ab140d5459216f4d076edd4d2f0ec983ea6251e5
Mark Robert Miller <markrmiller@apache.org>
2012-07-13 00:35:54 +0000

    Add reload command to collections api - also improve tests.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1361017 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e7d1278836bf73754e80d5c8fa191450bca68a5b

--- solr/solrj/src/java/org/apache/solr/common/params/CollectionParams.java
+++ solr/solrj/src/java/org/apache/solr/common/params/CollectionParams.java
@@ -60,9 +60,12 @@ name|CollectionAction
 block|{
 DECL|enum constant|CREATE
 DECL|enum constant|DELETE
+DECL|enum constant|RELOAD
 name|CREATE
 block|,
 name|DELETE
+block|,
+name|RELOAD
 block|;
 DECL|method|get
 specifier|public
