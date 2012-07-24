5b8a654bfaa48f977b25b4f725e60356cd19c824
Uwe Schindler <uschindler@apache.org>
2012-07-24 15:49:46 +0000

    LUCENE-2510: Some cleanup in the new service class iterator + better messages
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene2510@1365143 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: baa71b943fa6658c9c516ab38e240a4923d20bde

--- lucene/core/src/java/org/apache/lucene/util/SPIClassIterator.java
+++ lucene/core/src/java/org/apache/lucene/util/SPIClassIterator.java
@@ -112,6 +112,16 @@ name|java
 operator|.
 name|util
 operator|.
+name|Locale
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
 name|NoSuchElementException
 import|;
 end_import
@@ -332,7 +342,14 @@ throw|throw
 operator|new
 name|ServiceConfigurationError
 argument_list|(
-literal|"Error loading SPI classes."
+literal|"Error loading SPI profiles for type "
+operator|+
+name|clazz
+operator|.
+name|getName
+argument_list|()
+operator|+
+literal|" from classpath"
 argument_list|,
 name|ioe
 argument_list|)
@@ -401,8 +418,6 @@ argument_list|>
 argument_list|()
 expr_stmt|;
 block|}
-try|try
-block|{
 specifier|final
 name|URL
 name|url
@@ -412,6 +427,8 @@ operator|.
 name|nextElement
 argument_list|()
 decl_stmt|;
+try|try
+block|{
 specifier|final
 name|InputStream
 name|in
@@ -564,7 +581,9 @@ throw|throw
 operator|new
 name|ServiceConfigurationError
 argument_list|(
-literal|"Error loading SPI classes."
+literal|"Error loading SPI class list from URL: "
+operator|+
+name|url
 argument_list|,
 name|ioe
 argument_list|)
@@ -659,7 +678,7 @@ argument_list|()
 decl_stmt|;
 try|try
 block|{
-comment|// don't initialize the class:
+comment|// don't initialize the class (pass false as 2nd parameter):
 return|return
 name|Class
 operator|.
@@ -688,9 +707,32 @@ throw|throw
 operator|new
 name|ServiceConfigurationError
 argument_list|(
-literal|"SPI class not found: "
+name|String
+operator|.
+name|format
+argument_list|(
+name|Locale
+operator|.
+name|ROOT
+argument_list|,
+literal|"A SPI class of type %s with classname %s does not exist, "
 operator|+
+literal|"please fix the file '%s%s' in your classpath."
+argument_list|,
+name|clazz
+operator|.
+name|getName
+argument_list|()
+argument_list|,
 name|c
+argument_list|,
+name|META_INF_SERVICES
+argument_list|,
+name|clazz
+operator|.
+name|getName
+argument_list|()
+argument_list|)
 argument_list|)
 throw|;
 block|}
