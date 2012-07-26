f64366cab2ef76a355603cf78905c27b044697a3
Mark Robert Miller <markrmiller@apache.org>
2012-07-26 23:53:33 +0000

    fix javadoc
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1366236 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a5f1159a6f6475721b47142b7376d2756e661f35

--- lucene/analysis/common/src/java/org/apache/lucene/analysis/compound/HyphenationCompoundWordTokenFilter.java
+++ lucene/analysis/common/src/java/org/apache/lucene/analysis/compound/HyphenationCompoundWordTokenFilter.java
@@ -328,7 +328,7 @@ name|DEFAULT_MAX_SUBWORD_SIZE
 argument_list|)
 expr_stmt|;
 block|}
-comment|/**    * Create a hyphenator tree    *     * @param hyphenationFilename the filename of the XML grammar to load    * @return An object representing the hyphenation patterns    * @throws Exception    */
+comment|/**    * Create a hyphenator tree    *     * @param hyphenationFilename the filename of the XML grammar to load    * @return An object representing the hyphenation patterns    * @throws IOException    */
 DECL|method|getHyphenationTree
 specifier|public
 specifier|static
@@ -352,7 +352,7 @@ argument_list|)
 argument_list|)
 return|;
 block|}
-comment|/**    * Create a hyphenator tree    *     * @param hyphenationFile the file of the XML grammar to load    * @return An object representing the hyphenation patterns    * @throws Exception    */
+comment|/**    * Create a hyphenator tree    *     * @param hyphenationFile the file of the XML grammar to load    * @return An object representing the hyphenation patterns    * @throws IOException    */
 DECL|method|getHyphenationTree
 specifier|public
 specifier|static
@@ -382,7 +382,7 @@ argument_list|)
 argument_list|)
 return|;
 block|}
-comment|/**    * Create a hyphenator tree    *     * @param hyphenationSource the InputSource pointing to the XML grammar    * @return An object representing the hyphenation patterns    * @throws Exception    */
+comment|/**    * Create a hyphenator tree    *     * @param hyphenationSource the InputSource pointing to the XML grammar    * @return An object representing the hyphenation patterns    * @throws IOException    */
 DECL|method|getHyphenationTree
 specifier|public
 specifier|static
