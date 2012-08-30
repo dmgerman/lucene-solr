76b5d88b151552ba3c74416a7f19f7249a764a4c
Mark Robert Miller <markrmiller@apache.org>
2012-08-30 13:18:29 +0000

    use sendToLeaders rather than replicas != null to determine if we add repicas to end of the list
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1378922 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e31ddf3554665bf0a7e37b0e98724281ac5808b8

--- solr/solrj/src/java/org/apache/solr/client/solrj/impl/CloudSolrServer.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/impl/CloudSolrServer.java
@@ -1341,9 +1341,7 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|replicas
-operator|!=
-literal|null
+name|sendToLeaders
 condition|)
 block|{
 name|ArrayList
@@ -1380,6 +1378,8 @@ argument_list|,
 name|rand
 argument_list|)
 expr_stmt|;
+comment|//  System.out.println("leaders:" + theUrlList);
+comment|//  System.out.println("replicas:" + theReplicas);
 name|theUrlList
 operator|.
 name|addAll
@@ -1388,7 +1388,7 @@ name|theReplicas
 argument_list|)
 expr_stmt|;
 block|}
-comment|//System.out.println("########################## MAKING REQUEST TO " + theUrlList);
+comment|// System.out.println("########################## MAKING REQUEST TO " + theUrlList);
 name|LBHttpSolrServer
 operator|.
 name|Req
