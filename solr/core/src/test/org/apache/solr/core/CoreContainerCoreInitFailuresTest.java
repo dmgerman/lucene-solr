8c3af2b98d60015a4629df15b591473b25df46cb
Chris M. Hostetter <hossman@apache.org>
2012-09-07 21:58:30 +0000

    SOLR-3699: Fixed some Directory leaks when there were errors during SolrCore or SolrIndexWriter initialization
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1382187 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ee0bf2549b711abac052d29291031ba0fa8726ba

--- solr/core/src/test/org/apache/solr/core/CoreContainerCoreInitFailuresTest.java
+++ solr/core/src/test/org/apache/solr/core/CoreContainerCoreInitFailuresTest.java
@@ -425,6 +425,16 @@ argument_list|)
 decl_stmt|;
 try|try
 block|{
+name|ignoreException
+argument_list|(
+name|Pattern
+operator|.
+name|quote
+argument_list|(
+literal|"bogus_path"
+argument_list|)
+argument_list|)
+expr_stmt|;
 name|cc
 operator|.
 name|create
@@ -572,18 +582,6 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
-comment|// TODO: even if we close all solr cores in the container, there is still a leaked dir?
-comment|// maybe from one that didnt load right?
-comment|// TODO: make SolrCore closeable since its has close()
-name|System
-operator|.
-name|setProperty
-argument_list|(
-literal|"solr.directoryFactory"
-argument_list|,
-literal|"org.apache.solr.core.SimpleFSDirectoryFactory"
-argument_list|)
-expr_stmt|;
 comment|// reused state
 name|Map
 argument_list|<
@@ -1014,6 +1012,16 @@ argument_list|)
 decl_stmt|;
 try|try
 block|{
+name|ignoreException
+argument_list|(
+name|Pattern
+operator|.
+name|quote
+argument_list|(
+literal|"bogus_path"
+argument_list|)
+argument_list|)
+expr_stmt|;
 name|cc
 operator|.
 name|create
@@ -1323,6 +1331,16 @@ argument_list|)
 expr_stmt|;
 try|try
 block|{
+name|ignoreException
+argument_list|(
+name|Pattern
+operator|.
+name|quote
+argument_list|(
+literal|"SAX"
+argument_list|)
+argument_list|)
+expr_stmt|;
 name|cc
 operator|.
 name|reload
