8c34f7d20b1ac24d4090fe92c16585e8ad529834
James Dyer <jdyer@apache.org>
2012-11-08 15:51:48 +0000

    SOLR-3856: @Ignore Delta test while investigating Linux failures
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1407140 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f57cdb8fc30f6171c4459d81d51426f8efce7af8

--- solr/contrib/dataimporthandler/src/test/org/apache/solr/handler/dataimport/TestSqlEntityProcessorDelta.java
+++ solr/contrib/dataimporthandler/src/test/org/apache/solr/handler/dataimport/TestSqlEntityProcessorDelta.java
@@ -44,6 +44,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -57,6 +67,11 @@ comment|/**  * Test with various combinations of parameters, child entites, tran
 end_comment
 
 begin_class
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Investigate failures on Policeman Jenkins Linux"
+argument_list|)
 DECL|class|TestSqlEntityProcessorDelta
 specifier|public
 class|class
