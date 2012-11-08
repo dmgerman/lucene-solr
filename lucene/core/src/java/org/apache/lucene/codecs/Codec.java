14f01d3076ea3e70b6102f9b678091c57730c645
Michael McCandless <mikemccand@apache.org>
2012-11-08 18:49:14 +0000

    LUCENE-4547: very rough prototype buffering
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene4547@1407212 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0dec2d10174a262283f7c32987a45295a43bc0eb

--- lucene/core/src/java/org/apache/lucene/codecs/Codec.java
+++ lucene/core/src/java/org/apache/lucene/codecs/Codec.java
@@ -167,6 +167,18 @@ name|DocValuesFormat
 name|docValuesFormat
 parameter_list|()
 function_decl|;
+comment|/** Encodes/decodes streaming docvalues */
+DECL|method|simpleDocValuesFormat
+specifier|public
+name|SimpleDocValuesFormat
+name|simpleDocValuesFormat
+parameter_list|()
+block|{
+comment|// nocommit make this abstract
+return|return
+literal|null
+return|;
+block|}
 comment|/** Encodes/decodes stored fields */
 DECL|method|storedFieldsFormat
 specifier|public
