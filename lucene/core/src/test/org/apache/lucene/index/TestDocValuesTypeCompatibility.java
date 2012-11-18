56185a2fb29afc281b52be2206e5471a6e9ae546
Michael McCandless <mikemccand@apache.org>
2012-11-18 22:53:12 +0000

    fix more fails
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene4547@1411014 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 216e72ce47d90552498cc508592f6cd0c9a5729c

--- lucene/core/src/test/org/apache/lucene/index/TestDocValuesTypeCompatibility.java
+++ lucene/core/src/test/org/apache/lucene/index/TestDocValuesTypeCompatibility.java
@@ -266,6 +266,16 @@ name|LuceneTestCase
 import|;
 end_import
 
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|Ignore
+import|;
+end_import
+
 begin_comment
 comment|/**  * Tests compatibility of {@link DocValues.Type} during indexing  */
 end_comment
@@ -1181,6 +1191,10 @@ argument_list|()
 expr_stmt|;
 block|}
 block|}
+comment|// nocommit remove this test?  simple dv doesn't let you
+comment|// change b/w sorted& binary?
+annotation|@
+name|Ignore
 DECL|method|testAddCompatibleByteTypes
 specifier|public
 name|void
