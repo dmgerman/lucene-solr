9ecc98ba9b4edb1302febd7899d02f4543cbf6a6
James Dyer <jdyer@apache.org>
2012-11-26 15:36:46 +0000

    SOLR-1916: reinstate @Ignore'ed test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1413684 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 012c260973ab975ed8f829dcfc69a2d01b8e9974

--- solr/contrib/dataimporthandler/src/test/org/apache/solr/handler/dataimport/TestDocBuilder2.java
+++ solr/contrib/dataimporthandler/src/test/org/apache/solr/handler/dataimport/TestDocBuilder2.java
@@ -1484,7 +1484,7 @@ name|Test
 annotation|@
 name|Ignore
 argument_list|(
-literal|"Known Locale/TZ problems: see https://issues.apache.org/jira/browse/SOLR-1916"
+literal|"Fix Me. See SOLR-4103."
 argument_list|)
 DECL|method|testFileListEntityProcessor_lastIndexTime
 specifier|public
