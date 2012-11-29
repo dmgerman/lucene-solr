8738991ccc07a581f5f18b9ba7ff61132c669d9c
Robert Muir <rmuir@apache.org>
2012-11-29 15:28:02 +0000

    add per-field docvalues format. lucene41 (and maybe simpletext) are currently broken because they dont respect segment suffixes
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene4547@1415208 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c4a28360fc830ea65055f695bab8e53ed4eee6f2

--- lucene/codecs/src/java/org/apache/lucene/codecs/simpletext/SimpleTextSimpleDocValuesFormat.java
+++ lucene/codecs/src/java/org/apache/lucene/codecs/simpletext/SimpleTextSimpleDocValuesFormat.java
@@ -599,6 +599,17 @@ argument_list|(
 literal|"  ordpattern "
 argument_list|)
 decl_stmt|;
+DECL|method|SimpleTextSimpleDocValuesFormat
+specifier|public
+name|SimpleTextSimpleDocValuesFormat
+parameter_list|()
+block|{
+name|super
+argument_list|(
+literal|"SimpleText"
+argument_list|)
+expr_stmt|;
+block|}
 annotation|@
 name|Override
 DECL|method|fieldsConsumer
