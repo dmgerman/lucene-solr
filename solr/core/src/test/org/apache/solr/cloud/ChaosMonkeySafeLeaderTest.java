b4fe4c18b1c8402caf57995f36b360ea1b1a0b6f
Mark Robert Miller <markrmiller@apache.org>
2013-01-01 23:39:19 +0000

    tests: allow control of chaosmonkey nothing is safe test by sys prop
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1427591 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0268e3807bea446ec7041dab534d1108b27b54f0

--- solr/core/src/test/org/apache/solr/cloud/ChaosMonkeySafeLeaderTest.java
+++ solr/core/src/test/org/apache/solr/cloud/ChaosMonkeySafeLeaderTest.java
@@ -58,6 +58,22 @@ name|org
 operator|.
 name|apache
 operator|.
+name|lucene
+operator|.
+name|util
+operator|.
+name|LuceneTestCase
+operator|.
+name|Slow
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
 name|solr
 operator|.
 name|client
@@ -165,6 +181,8 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|Slow
 annotation|@
 name|BadApple
 DECL|class|ChaosMonkeySafeLeaderTest
