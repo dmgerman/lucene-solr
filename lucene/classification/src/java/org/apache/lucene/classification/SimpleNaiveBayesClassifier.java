c69c4824a9dcc6b00a481250a62837150677d3b1
Tommaso Teofili <tommaso@apache.org>
2013-01-03 15:48:13 +0000

    [LUCENE-4651] - added generics to Classifier and ClassificationResult
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1428411 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 325c3678ea1fc60e973230d68be67264151ce6ba

--- lucene/classification/src/java/org/apache/lucene/classification/SimpleNaiveBayesClassifier.java
+++ lucene/classification/src/java/org/apache/lucene/classification/SimpleNaiveBayesClassifier.java
@@ -265,6 +265,9 @@ class|class
 name|SimpleNaiveBayesClassifier
 implements|implements
 name|Classifier
+argument_list|<
+name|BytesRef
+argument_list|>
 block|{
 DECL|field|atomicReader
 specifier|private
@@ -491,6 +494,9 @@ name|Override
 DECL|method|assignClass
 specifier|public
 name|ClassificationResult
+argument_list|<
+name|BytesRef
+argument_list|>
 name|assignClass
 parameter_list|(
 name|String
@@ -519,10 +525,12 @@ name|max
 init|=
 literal|0d
 decl_stmt|;
-name|String
+name|BytesRef
 name|foundClass
 init|=
-literal|null
+operator|new
+name|BytesRef
+argument_list|()
 decl_stmt|;
 name|Terms
 name|terms
@@ -603,7 +611,7 @@ name|foundClass
 operator|=
 name|next
 operator|.
-name|utf8ToString
+name|clone
 argument_list|()
 expr_stmt|;
 block|}
@@ -611,6 +619,9 @@ block|}
 return|return
 operator|new
 name|ClassificationResult
+argument_list|<
+name|BytesRef
+argument_list|>
 argument_list|(
 name|foundClass
 argument_list|,
