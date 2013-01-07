83418a074fcdf4d52e1a8ac7259a5fb9681309f4
Robert Muir <rmuir@apache.org>
2013-01-07 15:15:45 +0000

    more progress on lucene41 impl
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene4547@1429834 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e327c650c0e78586d63c2904b970833f428fa67b

--- lucene/core/src/test/org/apache/lucene/TestDemoDocValue.java
+++ lucene/core/src/test/org/apache/lucene/TestDemoDocValue.java
@@ -4054,6 +4054,14 @@ argument_list|(
 literal|"dv"
 argument_list|)
 decl_stmt|;
+name|assertFalse
+argument_list|(
+name|dv
+operator|.
+name|isFixedLength
+argument_list|()
+argument_list|)
+expr_stmt|;
 name|BytesRef
 name|scratch
 init|=
