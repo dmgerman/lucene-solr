dd32254bebab57284f3b609b5f44cda327335487
Shai Erera <shaie@apache.org>
2013-01-16 13:25:13 +0000

    LUCENE-4620: inline encoding/decoding
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1433926 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 800d2f322bdd1bbeecf21002b4a98b175174cee0

--- lucene/facet/src/test/org/apache/lucene/util/encoding/EncodingTest.java
+++ lucene/facet/src/test/org/apache/lucene/util/encoding/EncodingTest.java
@@ -412,6 +412,22 @@ literal|100
 argument_list|)
 decl_stmt|;
 comment|// some initial capacity - decoders should grow the int[]
+for|for
+control|(
+name|int
+name|i
+init|=
+literal|0
+init|;
+name|i
+operator|<
+literal|2
+condition|;
+name|i
+operator|++
+control|)
+block|{
+comment|// run 2 iterations to catch encoders/decoders which don't reset properly
 name|encoding
 argument_list|(
 name|encoder
@@ -443,6 +459,7 @@ name|values
 argument_list|)
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 DECL|method|encoding
 specifier|private
