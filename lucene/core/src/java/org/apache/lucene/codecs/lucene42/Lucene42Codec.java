a3594812e40984523e7aaf00de0a1364ee9eea6c
Robert Muir <rmuir@apache.org>
2013-01-22 15:08:25 +0000

    4.2 fieldinfos
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene4547@1436986 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 44cdb10870f04b2465efba070063c1739ccf8867

--- lucene/core/src/java/org/apache/lucene/codecs/lucene42/Lucene42Codec.java
+++ lucene/core/src/java/org/apache/lucene/codecs/lucene42/Lucene42Codec.java
@@ -170,22 +170,6 @@ name|codecs
 operator|.
 name|lucene40
 operator|.
-name|Lucene40FieldInfosFormat
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|lucene
-operator|.
-name|codecs
-operator|.
-name|lucene40
-operator|.
 name|Lucene40LiveDocsFormat
 import|;
 end_import
@@ -271,7 +255,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Implements the Lucene 4.2 index format, with configurable per-field postings formats.  *<p>  * If you want to reuse functionality of this codec in another codec, extend  * {@link FilterCodec}.  *  * @see org.apache.lucene.codecs.lucene42 package documentation for file format details.  * @lucene.experimental  */
+comment|/**  * Implements the Lucene 4.2 index format, with configurable per-field postings  * and docvalues formats.  *<p>  * If you want to reuse functionality of this codec in another codec, extend  * {@link FilterCodec}.  *  * @see org.apache.lucene.codecs.lucene42 package documentation for file format details.  * @lucene.experimental  */
 end_comment
 
 begin_comment
@@ -321,7 +305,7 @@ name|FieldInfosFormat
 name|fieldInfosFormat
 init|=
 operator|new
-name|Lucene40FieldInfosFormat
+name|Lucene42FieldInfosFormat
 argument_list|()
 decl_stmt|;
 DECL|field|infosFormat
