f97fd1bc67cb38e84ae019e39a8949113deda3a9
Michael McCandless <mikemccand@apache.org>
2013-01-27 12:13:09 +0000

    remove Serializable
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1439066 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 93962e17f3e345a40a18238c9b0f39696241d226

--- lucene/facet/src/java/org/apache/lucene/facet/index/params/CategoryListParams.java
+++ lucene/facet/src/java/org/apache/lucene/facet/index/params/CategoryListParams.java
@@ -28,16 +28,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|io
-operator|.
-name|Serializable
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
@@ -177,8 +167,6 @@ DECL|class|CategoryListParams
 specifier|public
 class|class
 name|CategoryListParams
-implements|implements
-name|Serializable
 block|{
 comment|/** OrdinalPolicy defines which ordinals are encoded for every document. */
 DECL|enum|OrdinalPolicy
