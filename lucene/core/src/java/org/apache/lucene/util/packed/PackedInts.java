352b403bafbe0d11758f249309702df0f5ef859e
Robert Muir <rmuir@apache.org>
2013-01-31 04:22:41 +0000

    clear nocommit
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene4547@1440843 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8e053920cc853f4f122289c8c48370f430e6b490

--- lucene/core/src/java/org/apache/lucene/util/packed/PackedInts.java
+++ lucene/core/src/java/org/apache/lucene/util/packed/PackedInts.java
@@ -4055,17 +4055,6 @@ operator|=
 name|version
 expr_stmt|;
 block|}
-comment|// nocommit
-DECL|method|getValueCount
-specifier|public
-name|int
-name|getValueCount
-parameter_list|()
-block|{
-return|return
-name|valueCount
-return|;
-block|}
 block|}
 block|}
 end_class
