73c77fc5cbeab2e66bced7a0bba6b5255f50ffd8
Tommaso Teofili <tommaso@apache.org>
2013-02-06 09:39:08 +0000

    LUCENE-4756 - release AE and CAS on #close
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1442876 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f1d80996faca79cea3e64ff6ab7f5481b7564aad

--- lucene/analysis/uima/src/java/org/apache/lucene/analysis/uima/ae/AEProviderFactory.java
+++ lucene/analysis/uima/src/java/org/apache/lucene/analysis/uima/ae/AEProviderFactory.java
@@ -53,8 +53,13 @@ block|{
 DECL|field|instance
 specifier|private
 specifier|static
+specifier|final
 name|AEProviderFactory
 name|instance
+init|=
+operator|new
+name|AEProviderFactory
+argument_list|()
 decl_stmt|;
 DECL|field|providerCache
 specifier|private
@@ -90,25 +95,11 @@ name|AEProviderFactory
 name|getInstance
 parameter_list|()
 block|{
-if|if
-condition|(
-name|instance
-operator|==
-literal|null
-condition|)
-block|{
-name|instance
-operator|=
-operator|new
-name|AEProviderFactory
-argument_list|()
-expr_stmt|;
-block|}
 return|return
 name|instance
 return|;
 block|}
-comment|/**    * @param keyPrefix a prefix of the key used to cache the AEProvider    * @param aePath the AnalysisEngine descriptor path    * @param runtimeParameters map of runtime parameters to configure inside the AnalysisEngine    * @return AEProvider    */
+comment|/**    * @param keyPrefix         a prefix of the key used to cache the AEProvider    * @param aePath            the AnalysisEngine descriptor path    * @param runtimeParameters map of runtime parameters to configure inside the AnalysisEngine    * @return AEProvider    */
 DECL|method|getAEProvider
 specifier|public
 specifier|synchronized
@@ -248,7 +239,7 @@ literal|null
 argument_list|)
 return|;
 block|}
-comment|/**    * @param aePath the AnalysisEngine descriptor path    * @param runtimeParameters map of runtime parameters to configure inside the AnalysisEngine    * @return AEProvider    */
+comment|/**    * @param aePath            the AnalysisEngine descriptor path    * @param runtimeParameters map of runtime parameters to configure inside the AnalysisEngine    * @return AEProvider    */
 DECL|method|getAEProvider
 specifier|public
 specifier|synchronized
