0b643ad1aad288c23a5a1a9aec3f3ae35ae269b9
Shai Erera <shaie@apache.org>
2013-02-10 12:55:40 +0000

    LUCENE-4762: improve facet examples
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1444538 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 71f295a69d3591eaed1089f4295a3828d99e0475

--- lucene/demo/src/java/org/apache/lucene/demo/facet/FacetExamples.java
+++ lucene/demo/src/java/org/apache/lucene/demo/facet/FacetExamples.java
@@ -33,80 +33,29 @@ comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more
 end_comment
 
 begin_comment
-comment|/**  * Simple utility functions for the faceting examples  * @lucene.experimental  */
+comment|/**  * Hold various constants used by facet examples.  *   * @lucene.experimental  */
 end_comment
 
-begin_class
-DECL|class|ExampleUtils
+begin_interface
+DECL|interface|FacetExamples
 specifier|public
-class|class
-name|ExampleUtils
+interface|interface
+name|FacetExamples
 block|{
-comment|/** No instance */
-DECL|method|ExampleUtils
-specifier|private
-name|ExampleUtils
-parameter_list|()
-block|{}
-comment|/**     * True if the system property<code>tests.verbose</code> has been set.    * If true, it causes {@link #log(Object)} to print messages to the console.    */
-DECL|field|VERBOSE
-specifier|public
-specifier|static
-specifier|final
-name|boolean
-name|VERBOSE
-init|=
-name|Boolean
-operator|.
-name|getBoolean
-argument_list|(
-literal|"tests.verbose"
-argument_list|)
-decl_stmt|;
 comment|/** The Lucene {@link Version} used by the example code. */
-DECL|field|EXAMPLE_VER
+DECL|field|EXAMPLES_VER
 specifier|public
 specifier|static
 specifier|final
 name|Version
-name|EXAMPLE_VER
+name|EXAMPLES_VER
 init|=
 name|Version
 operator|.
-name|LUCENE_40
+name|LUCENE_50
 decl_stmt|;
-comment|/**    * Logs the String representation of<code>msg</code> to the console,    * if {@link #VERBOSE} is true. Otherwise, does nothing.    * @see #VERBOSE    */
-DECL|method|log
-specifier|public
-specifier|static
-name|void
-name|log
-parameter_list|(
-name|Object
-name|msg
-parameter_list|)
-block|{
-if|if
-condition|(
-name|VERBOSE
-condition|)
-block|{
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|(
-name|msg
-operator|.
-name|toString
-argument_list|()
-argument_list|)
-expr_stmt|;
 block|}
-block|}
-block|}
-end_class
+end_interface
 
 end_unit
 
