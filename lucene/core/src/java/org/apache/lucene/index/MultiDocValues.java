a4af6af78d3b21f8cc27768e6d68ebeb6bb2489d
Robert Muir <rmuir@apache.org>
2013-02-15 16:09:53 +0000

    fix documentation-lint
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene4765@1446665 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3572f7a351348132ea3d643c90bdb701808e1eb1

--- lucene/core/src/java/org/apache/lucene/index/MultiDocValues.java
+++ lucene/core/src/java/org/apache/lucene/index/MultiDocValues.java
@@ -1439,6 +1439,7 @@ block|}
 block|}
 comment|/** maps per-segment ordinals to/from global ordinal space */
 comment|// TODO: use more efficient packed ints structures?
+comment|// TODO: pull this out? its pretty generic (maps between N ord()-enabled TermsEnums)
 DECL|class|OrdinalMap
 specifier|public
 specifier|static
@@ -1470,6 +1471,7 @@ name|AppendingLongBuffer
 name|ordDeltas
 index|[]
 decl_stmt|;
+comment|/**       * Creates an ordinal map that allows mapping ords to/from a merged      * space from<code>subs</code>.      * @param owner a cache key      * @param subs TermsEnums that support {@link TermsEnum#ord()}. They need      *             not be dense (e.g. can be FilteredTermsEnums}.      * @throws IOException if an I/O error occurred.      */
 DECL|method|OrdinalMap
 specifier|public
 name|OrdinalMap
@@ -1771,6 +1773,7 @@ operator|++
 expr_stmt|;
 block|}
 block|}
+comment|/**       * Given a segment number and segment ordinal, returns      * the corresponding global ordinal.      */
 DECL|method|getGlobalOrd
 specifier|public
 name|long
@@ -1797,6 +1800,7 @@ name|segmentOrd
 argument_list|)
 return|;
 block|}
+comment|/**      * Given a segment number and global ordinal, returns      * the corresponding segment ordinal.      */
 DECL|method|getSegmentOrd
 specifier|public
 name|long
@@ -1820,6 +1824,7 @@ name|globalOrd
 argument_list|)
 return|;
 block|}
+comment|/**       * Given a global ordinal, returns the index of the first      * sub that contains this term.      */
 DECL|method|getSegmentNumber
 specifier|public
 name|int
@@ -1841,6 +1846,7 @@ name|globalOrd
 argument_list|)
 return|;
 block|}
+comment|/**      * Returns the total number of unique terms in global ord space.      */
 DECL|method|getValueCount
 specifier|public
 name|long
