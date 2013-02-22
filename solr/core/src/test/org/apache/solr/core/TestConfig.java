0000693fd74c0c16dfa4873845b9c99fa0b26908
Erik Hatcher <ehatcher@apache.org>
2013-02-22 20:52:17 +0000

    SOLR-2856: Fix schema (scheam) typo in test configurations
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1449211 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 13fc322cf06dc90e498dcf7de051d8d2ce3390da

--- solr/core/src/test/org/apache/solr/core/TestConfig.java
+++ solr/core/src/test/org/apache/solr/core/TestConfig.java
@@ -632,7 +632,7 @@ literal|null
 argument_list|)
 expr_stmt|;
 comment|//System.out.println( handler.getHiddenFiles() );
-comment|// should not contain:<gettableFiles>solrconfig.xml scheam.xml admin-extra.html</gettableFiles>
+comment|// should not contain:<gettableFiles>solrconfig.xml schema.xml admin-extra.html</gettableFiles>
 name|assertFalse
 argument_list|(
 name|handler
@@ -642,7 +642,7 @@ argument_list|()
 operator|.
 name|contains
 argument_list|(
-literal|"scheam.xml"
+literal|"schema.xml"
 operator|.
 name|toUpperCase
 argument_list|(
