6b81fac610add5171d827560ca924f77c8ec195f
Uwe Schindler <uschindler@apache.org>
2013-03-01 13:12:16 +0000

    LUCENE-4808: Add workaround for a JDK 8 "class library bug" which is still under discussion and may *not* be fixed
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1451584 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 202d3af9d0b5d9a2d262625c51ae7407487f6e65

--- lucene/analysis/common/src/test/org/apache/lucene/analysis/core/TestRandomChains.java
+++ lucene/analysis/common/src/test/org/apache/lucene/analysis/core/TestRandomChains.java
@@ -1991,6 +1991,8 @@ range|:
 name|analysisClasses
 control|)
 block|{
+comment|// TODO: Fix below code to use c.isAnnotationPresent(). It was changed
+comment|// to the null check to work around a bug in JDK 8 b78 (see LUCENE-4808).
 specifier|final
 name|int
 name|modifiers
@@ -2040,12 +2042,14 @@ argument_list|()
 operator|||
 name|c
 operator|.
-name|isAnnotationPresent
+name|getAnnotation
 argument_list|(
 name|Deprecated
 operator|.
 name|class
 argument_list|)
+operator|!=
+literal|null
 operator|||
 operator|!
 operator|(
@@ -2105,12 +2109,14 @@ argument_list|()
 operator|||
 name|ctor
 operator|.
-name|isAnnotationPresent
+name|getAnnotation
 argument_list|(
 name|Deprecated
 operator|.
 name|class
 argument_list|)
+operator|!=
+literal|null
 operator|||
 name|brokenConstructors
 operator|.
