f660ee74ee38c7702ac09b10f52710d8277d2de3
Michael McCandless <mikemccand@apache.org>
2013-03-01 12:51:21 +0000

    LUCENE-4806: change facet delim character to use 3 bytes instead of 1 (in UTF-8)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1451578 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ae89bfccb711f6b9e37fb65a4621177b8cf0ba5d

--- lucene/facet/src/java/org/apache/lucene/facet/params/FacetIndexingParams.java
+++ lucene/facet/src/java/org/apache/lucene/facet/params/FacetIndexingParams.java
@@ -112,7 +112,7 @@ specifier|final
 name|char
 name|DEFAULT_FACET_DELIM_CHAR
 init|=
-literal|'\uF749'
+literal|'\u001F'
 decl_stmt|;
 DECL|field|partitionSize
 specifier|private
--- lucene/facet/src/java/org/apache/lucene/facet/taxonomy/directory/DirectoryTaxonomyWriter.java
+++ lucene/facet/src/java/org/apache/lucene/facet/taxonomy/directory/DirectoryTaxonomyWriter.java
@@ -2963,9 +2963,7 @@ name|CategoryPath
 argument_list|(
 name|value
 argument_list|,
-name|Consts
-operator|.
-name|DEFAULT_DELIMITER
+name|delimiter
 argument_list|)
 decl_stmt|;
 specifier|final
