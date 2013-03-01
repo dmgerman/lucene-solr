ebc7ea97bd18f6a035955fad28c6251ca5ad7006
Chris M. Hostetter <hossman@apache.org>
2013-03-01 23:17:37 +0000

    SOLR-4458: Sort directions (asc, desc) are now case insensitive
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1451765 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 05cc3a47060080018b9e054d2c25ead62f603339

--- solr/core/src/test/org/apache/solr/search/QueryParsingTest.java
+++ solr/core/src/test/org/apache/solr/search/QueryParsingTest.java
@@ -337,13 +337,14 @@ name|sort
 argument_list|)
 expr_stmt|;
 comment|//only 1 thing in the list, no Sort specified
+comment|// SOLR-4458 - using different case variations of asc and desc
 name|sort
 operator|=
 name|QueryParsing
 operator|.
 name|parseSort
 argument_list|(
-literal|"score asc"
+literal|"score aSc"
 argument_list|,
 name|req
 argument_list|)
@@ -391,7 +392,7 @@ name|QueryParsing
 operator|.
 name|parseSort
 argument_list|(
-literal|"weight desc"
+literal|"weight dEsC"
 argument_list|,
 name|req
 argument_list|)
@@ -452,7 +453,7 @@ name|QueryParsing
 operator|.
 name|parseSort
 argument_list|(
-literal|"weight desc,bday asc"
+literal|"weight desc,bday ASC"
 argument_list|,
 name|req
 argument_list|)
@@ -766,7 +767,7 @@ name|QueryParsing
 operator|.
 name|parseSort
 argument_list|(
-literal|"weight         desc,            bday         asc"
+literal|"weight         DESC,            bday         asc"
 argument_list|,
 name|req
 argument_list|)
