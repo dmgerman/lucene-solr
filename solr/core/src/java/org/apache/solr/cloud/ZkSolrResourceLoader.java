4747e41c1a9feca2c4c1f4d691bc93c16b91fc22
Steven Rowe <sarowe@apache.org>
2013-04-01 16:16:03 +0000

    SOLR-4658: In preparation for REST API requests that can modify the schema, a "managed schema" is introduced.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1463182 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b1e4ba6843e78e92168e3ac315ea8238095a39d6

--- solr/core/src/java/org/apache/solr/cloud/ZkSolrResourceLoader.java
+++ solr/core/src/java/org/apache/solr/cloud/ZkSolrResourceLoader.java
@@ -548,6 +548,16 @@ return|return
 name|collectionZkPath
 return|;
 block|}
+DECL|method|getZkController
+specifier|public
+name|ZkController
+name|getZkController
+parameter_list|()
+block|{
+return|return
+name|zkController
+return|;
+block|}
 block|}
 end_class
 
