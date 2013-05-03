4e62678c6a8f2746246cd8649755357a13f01839
Michael McCandless <mikemccand@apache.org>
2013-05-03 11:17:17 +0000

    make SearcherAndTaxonomy ctor public
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1478729 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: aec9d12e9b6670779431447bd2be6145d91d8e6e

--- lucene/facet/src/java/org/apache/lucene/facet/search/SearcherTaxonomyManager.java
+++ lucene/facet/src/java/org/apache/lucene/facet/search/SearcherTaxonomyManager.java
@@ -228,7 +228,9 @@ specifier|final
 name|DirectoryTaxonomyReader
 name|taxonomyReader
 decl_stmt|;
+comment|/** Create a SearcherAndTaxonomy */
 DECL|method|SearcherAndTaxonomy
+specifier|public
 name|SearcherAndTaxonomy
 parameter_list|(
 name|IndexSearcher
