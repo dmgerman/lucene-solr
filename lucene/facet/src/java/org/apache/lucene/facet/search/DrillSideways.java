dbbb84811e0c58f5bf5544c9b8f3685287a5b245
Shai Erera <shaie@apache.org>
2013-05-28 11:23:05 +0000

    LUCENE-5020: Make DrillSidewaysResult ctor public
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1486872 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 90dd61de5cccd36348bae53a4e0e782ec969d5a4

--- lucene/facet/src/java/org/apache/lucene/facet/search/DrillSideways.java
+++ lucene/facet/src/java/org/apache/lucene/facet/search/DrillSideways.java
@@ -2258,17 +2258,19 @@ argument_list|,
 name|fsp
 argument_list|)
 decl_stmt|;
+return|return
+operator|new
+name|DrillSidewaysResult
+argument_list|(
 name|r
 operator|.
-name|hits
-operator|=
+name|facetResults
+argument_list|,
 name|hitCollector
 operator|.
 name|topDocs
 argument_list|()
-expr_stmt|;
-return|return
-name|r
+argument_list|)
 return|;
 block|}
 else|else
@@ -2347,17 +2349,19 @@ argument_list|,
 name|fsp
 argument_list|)
 decl_stmt|;
+return|return
+operator|new
+name|DrillSidewaysResult
+argument_list|(
 name|r
 operator|.
-name|hits
-operator|=
+name|facetResults
+argument_list|,
 name|hitCollector
 operator|.
 name|topDocs
 argument_list|()
-expr_stmt|;
-return|return
-name|r
+argument_list|)
 return|;
 block|}
 comment|/** Override this to use a custom drill-down {@link    *  FacetsAccumulator}. */
@@ -2430,7 +2434,7 @@ return|return
 literal|false
 return|;
 block|}
-comment|/** Represents the returned result from a drill sideways    *  search. */
+comment|/**    * Represents the returned result from a drill sideways search. Note that if    * you called    * {@link DrillSideways#search(DrillDownQuery, Collector, FacetSearchParams)},    * then {@link #hits} will be {@code null}.    */
 DECL|class|DrillSidewaysResult
 specifier|public
 specifier|static
@@ -2450,10 +2454,12 @@ decl_stmt|;
 comment|/** Hits. */
 DECL|field|hits
 specifier|public
+specifier|final
 name|TopDocs
 name|hits
 decl_stmt|;
 DECL|method|DrillSidewaysResult
+specifier|public
 name|DrillSidewaysResult
 parameter_list|(
 name|List
