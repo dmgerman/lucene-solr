b157f631fd0b24034e1f38c51b236c38c48e037f
Michael McCandless <mikemccand@apache.org>
2013-06-03 14:23:55 +0000

    improve javadocs
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1489004 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 72d36243a9ff94a72a49f988c22f12edc50a3b8f

--- lucene/misc/src/java/org/apache/lucene/util/fst/UpToTwoPositiveIntOutputs.java
+++ lucene/misc/src/java/org/apache/lucene/util/fst/UpToTwoPositiveIntOutputs.java
@@ -19,7 +19,7 @@ comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more
 end_comment
 
 begin_comment
-comment|/**  * An FST {@link Outputs} implementation where each output  * is one or two non-negative long values.  If it's a  * single output, Long is returned; else, TwoLongs.  Order  * is preserved in the TwoLongs case, ie .first is the first  * input/output added to Builder, and .second is the  * second.  You cannot store 0 output with this (that's  * reserved to mean "no output")!  *  * NOTE: the resulting FST is not guaranteed to be minimal!  * See {@link Builder}.  *  * @lucene.experimental  */
+comment|/**  * An FST {@link Outputs} implementation where each output  * is one or two non-negative long values.  If it's a  * single output, Long is returned; else, TwoLongs.  Order  * is preserved in the TwoLongs case, ie .first is the first  * input/output added to Builder, and .second is the  * second.  You cannot store 0 output with this (that's  * reserved to mean "no output")!  *  *<p>NOTE: the only way to create a TwoLongs output is to  * add the same input to the FST twice in a row.  This is  * how the FST maps a single input to two outputs (e.g. you  * cannot pass a TwoLongs to {@link Builder#add}.  If you  * need more than two then use {@link ListOfOutputs}, but if  * you only have at most 2 then this implementation will  * require fewer bytes as it steals one bit from each long  * value.  *  *<p>NOTE: the resulting FST is not guaranteed to be minimal!  * See {@link Builder}.  *  * @lucene.experimental  */
 end_comment
 
 begin_import
