0b01d939d79f0149e40146b18cd0503222360bfc
Shai Erera <shaie@apache.org>
2013-06-25 18:39:28 +0000

    LUCENE-5078: Allow TfIDFSimilarity implementations to encode norm values into more than a single byte
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1496579 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3cf2f34181f24a6e9ecead2c4826861105bc5ff5

--- lucene/queries/src/java/org/apache/lucene/queries/function/valuesource/NormValueSource.java
+++ lucene/queries/src/java/org/apache/lucene/queries/function/valuesource/NormValueSource.java
@@ -149,7 +149,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**   * Function that returns {@link TFIDFSimilarity#decodeNormValue(byte)}  * for every document.  *<p>  * Note that the configured Similarity for the field must be  * a subclass of {@link TFIDFSimilarity}  * @lucene.internal */
+comment|/**   * Function that returns {@link TFIDFSimilarity#decodeNormValue(long)}  * for every document.  *<p>  * Note that the configured Similarity for the field must be  * a subclass of {@link TFIDFSimilarity}  * @lucene.internal */
 end_comment
 
 begin_class
