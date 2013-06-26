65c0c08bd4004b85120acf9c7b7092ea1a7b83f6
Yonik Seeley <yonik@apache.org>
2013-06-26 18:48:52 +0000

    LUCENE-5079: IndexWriter.hasUncommittedChanges()
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1497039 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 146d2be826a650593221d7ecd27c1f29703a2e49

--- lucene/core/src/java/org/apache/lucene/index/IndexWriter.java
+++ lucene/core/src/java/org/apache/lucene/index/IndexWriter.java
@@ -577,6 +577,7 @@ decl_stmt|;
 comment|// increments every time a change is completed
 DECL|field|lastCommitChangeCount
 specifier|private
+specifier|volatile
 name|long
 name|lastCommitChangeCount
 decl_stmt|;
@@ -8373,6 +8374,20 @@ name|commitInternal
 argument_list|()
 expr_stmt|;
 block|}
+comment|/** Returns true if there are changes that have not been committed */
+DECL|method|hasUncommittedChanges
+specifier|public
+specifier|final
+name|boolean
+name|hasUncommittedChanges
+parameter_list|()
+block|{
+return|return
+name|changeCount
+operator|!=
+name|lastCommitChangeCount
+return|;
+block|}
 DECL|method|commitInternal
 specifier|private
 specifier|final
@@ -8564,16 +8579,16 @@ literal|"\""
 argument_list|)
 expr_stmt|;
 block|}
-name|lastCommitChangeCount
-operator|=
-name|pendingCommitChangeCount
-expr_stmt|;
 name|segmentInfos
 operator|.
 name|updateGeneration
 argument_list|(
 name|pendingCommit
 argument_list|)
+expr_stmt|;
+name|lastCommitChangeCount
+operator|=
+name|pendingCommitChangeCount
 expr_stmt|;
 name|rollbackSegments
 operator|=
