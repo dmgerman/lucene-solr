a289aa4e03d2c04b7baca1741c88b23d9f73aedb
Grant Ingersoll <gsingers@apache.org>
2013-07-05 15:30:42 +0000

    SOLR-5003: add rowid (line number) option to CSV Loader
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1500046 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 218856ac6d05181f8c31b7a068cce9a187a3c3d6

--- solr/core/src/java/org/apache/solr/handler/loader/CSVLoaderBase.java
+++ solr/core/src/java/org/apache/solr/handler/loader/CSVLoaderBase.java
@@ -439,6 +439,15 @@ name|LITERALS_PREFIX
 init|=
 literal|"literal."
 decl_stmt|;
+DECL|field|ROW_ID
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|ROW_ID
+init|=
+literal|"rowid"
+decl_stmt|;
 DECL|field|colonSplit
 specifier|private
 specifier|static
@@ -527,6 +536,13 @@ name|FieldAdder
 index|[]
 name|adders
 decl_stmt|;
+DECL|field|rowId
+name|String
+name|rowId
+init|=
+literal|null
+decl_stmt|;
+comment|// if not null, add a special field by the name given with the line number/row id as the value
 DECL|field|skipLines
 name|int
 name|skipLines
@@ -1239,6 +1255,15 @@ literal|"'"
 argument_list|)
 throw|;
 block|}
+name|rowId
+operator|=
+name|params
+operator|.
+name|get
+argument_list|(
+name|ROW_ID
+argument_list|)
+expr_stmt|;
 comment|// if only encapsulator or escape is set, disable the other escaping mechanism
 if|if
 condition|(
@@ -1971,6 +1996,7 @@ name|length
 argument_list|()
 argument_list|)
 decl_stmt|;
+comment|//TODO: need to look at this in light of schemaless
 name|SchemaField
 name|sf
 init|=
@@ -2562,7 +2588,7 @@ parameter_list|)
 throws|throws
 name|IOException
 block|{
-comment|// the line number is passed simply for error reporting in MT mode.
+comment|// the line number is passed for error reporting in MT mode as well as for optional rowId.
 comment|// first, create the lucene document
 for|for
 control|(
@@ -2654,6 +2680,23 @@ argument_list|(
 name|fn
 argument_list|,
 name|val
+argument_list|)
+expr_stmt|;
+block|}
+if|if
+condition|(
+name|rowId
+operator|!=
+literal|null
+condition|)
+block|{
+name|doc
+operator|.
+name|addField
+argument_list|(
+name|rowId
+argument_list|,
+name|line
 argument_list|)
 expr_stmt|;
 block|}
