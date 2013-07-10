cfe636e1e3166dce7b22c7f46d7a4f4f510b567f
Han Jiang <han@apache.org>
2013-07-10 15:56:08 +0000

    LUCENE-3069: use more compact outputs i/o
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene3069@1501811 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9181dbc0eb00ccb91d253834fe040c3456e9a958

--- lucene/core/src/java/org/apache/lucene/codecs/temp/TempFSTTermsReader.java
+++ lucene/core/src/java/org/apache/lucene/codecs/temp/TempFSTTermsReader.java
@@ -32,6 +32,26 @@ begin_import
 import|import
 name|java
 operator|.
+name|io
+operator|.
+name|PrintWriter
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|File
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
 name|util
 operator|.
 name|Collections
@@ -396,6 +416,22 @@ name|apache
 operator|.
 name|lucene
 operator|.
+name|util
+operator|.
+name|fst
+operator|.
+name|Util
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
 name|codecs
 operator|.
 name|FieldsProducer
@@ -438,16 +474,6 @@ name|TempFSTTermsReader
 extends|extends
 name|FieldsProducer
 block|{
-DECL|field|postingsReader
-specifier|final
-name|TempPostingsReaderBase
-name|postingsReader
-decl_stmt|;
-DECL|field|in
-specifier|final
-name|IndexInput
-name|in
-decl_stmt|;
 DECL|field|fields
 specifier|final
 name|TreeMap
@@ -467,12 +493,23 @@ name|TermsReader
 argument_list|>
 argument_list|()
 decl_stmt|;
+DECL|field|postingsReader
+specifier|final
+name|TempPostingsReaderBase
+name|postingsReader
+decl_stmt|;
+DECL|field|in
+specifier|final
+name|IndexInput
+name|in
+decl_stmt|;
 DECL|field|DEBUG
 name|boolean
 name|DEBUG
 init|=
 literal|false
 decl_stmt|;
+comment|//String tmpname;
 DECL|method|TempFSTTermsReader
 specifier|public
 name|TempFSTTermsReader
@@ -509,6 +546,7 @@ operator|.
 name|TERMS_EXTENSION
 argument_list|)
 decl_stmt|;
+comment|//tmpname = termsFileName;
 name|this
 operator|.
 name|postingsReader
@@ -1189,6 +1227,9 @@ name|longsSize
 argument_list|)
 argument_list|)
 expr_stmt|;
+comment|//PrintWriter pw = new PrintWriter(new File("../graphs/ohohoh."+tmpname+".xxx.txt"));
+comment|//Util.toDot(dict, pw, false, false);
+comment|//pw.close();
 block|}
 comment|// nocommit: implement intersect
 comment|// nocommit: why do we need this comparator overridden again and again?
