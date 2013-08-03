4bebeb459ae1dd5376176f4f1d022eb255813420
Uwe Schindler <uschindler@apache.org>
2013-08-03 22:49:10 +0000

    SOLR-5113: Disable test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1510073 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 64653851102bd76b46b65274ab0b5e430b744ffa

--- solr/core/src/test/org/apache/solr/cloud/CollectionsAPIDistributedZkTest.java
+++ solr/core/src/test/org/apache/solr/cloud/CollectionsAPIDistributedZkTest.java
@@ -646,6 +646,16 @@ end_import
 
 begin_import
 import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
 name|javax
 operator|.
 name|management
@@ -969,6 +979,11 @@ end_comment
 begin_class
 annotation|@
 name|Slow
+annotation|@
+name|Ignore
+argument_list|(
+literal|"SOLR-5113"
+argument_list|)
 DECL|class|CollectionsAPIDistributedZkTest
 specifier|public
 class|class
