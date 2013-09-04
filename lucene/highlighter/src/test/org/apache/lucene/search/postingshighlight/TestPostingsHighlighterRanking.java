3f264ea5f7e560f459110780945a03d13c1bffcc
Han Jiang <han@apache.org>
2013-09-04 08:53:30 +0000

    revert noisy changes
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene3069@1519947 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 12f60cb5351b6d5890a644eaa284e6fe94f00627

--- lucene/highlighter/src/test/org/apache/lucene/search/postingshighlight/TestPostingsHighlighterRanking.java
+++ lucene/highlighter/src/test/org/apache/lucene/search/postingshighlight/TestPostingsHighlighterRanking.java
@@ -400,14 +400,6 @@ block|,
 literal|"MockSep"
 block|,
 literal|"MockRandom"
-block|,
-literal|"TempFixedIntBlock"
-block|,
-literal|"TempVariableIntBlock"
-block|,
-literal|"TempSep"
-block|,
-literal|"TempRandom"
 block|}
 argument_list|)
 DECL|class|TestPostingsHighlighterRanking
