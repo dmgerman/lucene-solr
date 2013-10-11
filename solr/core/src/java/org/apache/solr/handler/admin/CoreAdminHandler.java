648b537834ff54d6a15b0c7eba572812f216257c
Mark Robert Miller <markrmiller@apache.org>
2013-10-11 01:37:24 +0000

    SOLR-5317: Fix race  when persisting cores in solr.xml
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1531163 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 4a894a888213216a8c995fb43b8cc5a47791978a

--- solr/core/src/java/org/apache/solr/handler/admin/CoreAdminHandler.java
+++ solr/core/src/java/org/apache/solr/handler/admin/CoreAdminHandler.java
@@ -492,6 +492,20 @@ name|apache
 operator|.
 name|solr
 operator|.
+name|core
+operator|.
+name|SolrXMLCoresLocator
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|solr
+operator|.
 name|handler
 operator|.
 name|RequestHandlerBase
@@ -3063,6 +3077,7 @@ name|dcore
 argument_list|)
 expr_stmt|;
 block|}
+comment|// make sure we can write out the descriptor first
 name|coreContainer
 operator|.
 name|getCoresLocator
@@ -3099,6 +3114,29 @@ argument_list|,
 literal|false
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|coreContainer
+operator|.
+name|getCoresLocator
+argument_list|()
+operator|instanceof
+name|SolrXMLCoresLocator
+condition|)
+block|{
+comment|// hack - in this case we persist once more because a core create race might
+comment|// have dropped entries.
+name|coreContainer
+operator|.
+name|getCoresLocator
+argument_list|()
+operator|.
+name|create
+argument_list|(
+name|coreContainer
+argument_list|)
+expr_stmt|;
+block|}
 name|rsp
 operator|.
 name|add
