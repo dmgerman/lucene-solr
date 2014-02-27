2fe0b926c2eb23bc25e547949066385ebdcb2f66
Mark Robert Miller <markrmiller@apache.org>
2014-02-27 02:23:20 +0000

    SOLR-5776: Suppress SSL
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1572408 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d98ee29e8e76f9eeb0e8526e509846a07cd66487

--- solr/solrj/src/test/org/apache/solr/client/solrj/embedded/SolrExampleJettyTest.java
+++ solr/solrj/src/test/org/apache/solr/client/solrj/embedded/SolrExampleJettyTest.java
@@ -28,6 +28,20 @@ name|apache
 operator|.
 name|solr
 operator|.
+name|SolrTestCaseJ4
+operator|.
+name|SuppressSSL
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|solr
+operator|.
 name|client
 operator|.
 name|solrj
@@ -103,6 +117,13 @@ comment|/**  * TODO? perhaps use:  *  http://docs.codehaus.org/display/JETTY/Ser
 end_comment
 
 begin_class
+annotation|@
+name|SuppressSSL
+argument_list|(
+name|bugUrl
+operator|=
+literal|"https://issues.apache.org/jira/browse/SOLR-5776"
+argument_list|)
 DECL|class|SolrExampleJettyTest
 specifier|public
 class|class
