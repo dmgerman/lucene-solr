9cd98a1029efe22e1d4876ff7590ef1ed94a3eef
Mark Robert Miller <markrmiller@apache.org>
2014-03-28 16:55:24 +0000

    SOLR-5914: Need to use ensureClosed boolean everywhere in SolrTestCaseJ4.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/solr5914@1582826 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 64ad96afb1db8d59e13f1a997926c73edd582e42

--- solr/test-framework/src/java/org/apache/solr/SolrTestCaseJ4.java
+++ solr/test-framework/src/java/org/apache/solr/SolrTestCaseJ4.java
@@ -1372,6 +1372,8 @@ operator|+
 name|cname
 argument_list|,
 name|rootTmpDir
+argument_list|,
+name|ensureClosed
 argument_list|)
 expr_stmt|;
 name|System
@@ -3163,6 +3165,13 @@ name|tag
 operator|)
 argument_list|,
 name|rootTmpDir
+argument_list|,
+operator|!
+operator|(
+name|LEAVE_TEST_TMP_DIR
+operator|||
+name|LEAVE_TEST_TMP_DIR_ANNOTATION
+operator|)
 argument_list|)
 return|;
 block|}
