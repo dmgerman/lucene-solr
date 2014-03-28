88f6345cfc08335d41508bb18e8b0619e1ac916d
Mark Robert Miller <markrmiller@apache.org>
2014-03-28 01:20:31 +0000

    SOLR-5914: Fix map-reduce contrib tests use of solrhome.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/solr5914@1582575 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 30711a3c349e500d448ed55b217c761cb7ad732b

--- solr/contrib/map-reduce/src/test/org/apache/solr/hadoop/MorphlineGoLiveMiniMRTest.java
+++ solr/contrib/map-reduce/src/test/org/apache/solr/hadoop/MorphlineGoLiveMiniMRTest.java
@@ -1074,12 +1074,8 @@ decl_stmt|;
 DECL|field|solrHomeDirectory
 specifier|private
 specifier|static
-specifier|final
 name|File
 name|solrHomeDirectory
-init|=
-name|createTempDir
-argument_list|()
 decl_stmt|;
 annotation|@
 name|Override
@@ -1151,6 +1147,11 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+name|solrHomeDirectory
+operator|=
+name|createTempDir
+argument_list|()
+expr_stmt|;
 name|assumeTrue
 argument_list|(
 literal|"Currently this test can only be run without the lucene test security policy in place"
