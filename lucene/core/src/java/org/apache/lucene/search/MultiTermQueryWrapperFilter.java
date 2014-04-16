3f5c3d00a1c706a31182548e214507f1cb9cb51f
Michael McCandless <mikemccand@apache.org>
2014-04-16 10:11:14 +0000

    fix AnalyzingInfixSuggester's javadocs to make it clearer that the index is 'private' to the suggester and should not be the same index used for normal searching
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1587851 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 26967b01148b4cd778de9ed0c5a93bbce80bb4d2

--- lucene/core/src/java/org/apache/lucene/search/MultiTermQueryWrapperFilter.java
+++ lucene/core/src/java/org/apache/lucene/search/MultiTermQueryWrapperFilter.java
@@ -302,6 +302,12 @@ name|getField
 argument_list|()
 return|;
 block|}
+DECL|field|rewriteTermCount
+specifier|public
+specifier|static
+name|long
+name|rewriteTermCount
+decl_stmt|;
 comment|/**    * Returns a DocIdSet with documents that should be permitted in search    * results.    */
 annotation|@
 name|Override
@@ -424,6 +430,9 @@ literal|null
 decl_stmt|;
 do|do
 block|{
+name|rewriteTermCount
+operator|++
+expr_stmt|;
 comment|// System.out.println("  iter termCount=" + termCount + " term=" +
 comment|// enumerator.term().toBytesString());
 name|docsEnum
