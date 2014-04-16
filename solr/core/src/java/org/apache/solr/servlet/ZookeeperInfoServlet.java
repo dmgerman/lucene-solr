45d397b53c1e38dd99822c77abee609d35605cc1
Noble Paul <noble@apache.org>
2014-04-16 11:38:14 +0000

    SOLR-5473 forbidden API usage
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1587876 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: bae74d6649eaa48a3e485f618af98796833c3233

--- solr/core/src/java/org/apache/solr/servlet/ZookeeperInfoServlet.java
+++ solr/core/src/java/org/apache/solr/servlet/ZookeeperInfoServlet.java
@@ -94,6 +94,16 @@ name|java
 operator|.
 name|util
 operator|.
+name|Locale
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
 name|Map
 import|;
 end_import
@@ -2140,6 +2150,10 @@ name|String
 operator|.
 name|format
 argument_list|(
+name|Locale
+operator|.
+name|ROOT
+argument_list|,
 literal|"/collections/%s/state"
 argument_list|,
 name|collection
