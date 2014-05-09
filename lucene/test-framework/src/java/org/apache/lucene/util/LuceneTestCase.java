4ce964bc1d04b963689906ffb2527e0f1440f443
Robert Muir <rmuir@apache.org>
2014-05-09 12:04:38 +0000

    sync the buggy methods in LiveIWC, instead of all operations from all RIWs...
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1593517 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a1a0b561cd4876bc955a57e1adfd7b04c14e7a58

--- lucene/test-framework/src/java/org/apache/lucene/util/LuceneTestCase.java
+++ lucene/test-framework/src/java/org/apache/lucene/util/LuceneTestCase.java
@@ -5183,7 +5183,6 @@ comment|// if you want it in LiveIndexWriterConfig: it must and will be tested h
 DECL|method|maybeChangeLiveIndexWriterConfig
 specifier|public
 specifier|static
-specifier|synchronized
 name|void
 name|maybeChangeLiveIndexWriterConfig
 parameter_list|(
