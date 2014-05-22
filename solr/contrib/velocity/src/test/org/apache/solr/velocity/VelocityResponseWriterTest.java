cc574f2a1c623b4289c4714c8c3f9affe80b90c8
Dawid Weiss <dweiss@apache.org>
2014-05-22 06:15:26 +0000

    LUCENE-5650: Enforce read-only access to any path outside the temporary folder via security manager
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1596767 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: cf5a0caeb61c701d7c57d63f0e747913976b3940

--- solr/contrib/velocity/src/test/org/apache/solr/velocity/VelocityResponseWriterTest.java
+++ solr/contrib/velocity/src/test/org/apache/solr/velocity/VelocityResponseWriterTest.java
@@ -90,16 +90,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Before
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|BeforeClass
 import|;
 end_import
@@ -121,16 +111,6 @@ operator|.
 name|io
 operator|.
 name|StringWriter
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
-name|IOException
 import|;
 end_import
 
