46e1ad9acf79b83d26f54227319cb2f225df2b5a
Robert Muir <rmuir@apache.org>
2014-06-21 13:15:57 +0000

    LUCENE-5778: support hunspell morphological description fields
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1604354 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 532d9f246b829d69b548b5716c591b925ed47ca3

--- lucene/analysis/common/src/test/org/apache/lucene/analysis/hunspell/TestAllDictionaries.java
+++ lucene/analysis/common/src/test/org/apache/lucene/analysis/hunspell/TestAllDictionaries.java
@@ -114,6 +114,22 @@ name|lucene
 operator|.
 name|util
 operator|.
+name|LuceneTestCase
+operator|.
+name|SuppressSysoutChecks
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|util
+operator|.
 name|RamUsageTester
 import|;
 end_import
@@ -137,6 +153,13 @@ annotation|@
 name|Ignore
 argument_list|(
 literal|"enable manually"
+argument_list|)
+annotation|@
+name|SuppressSysoutChecks
+argument_list|(
+name|bugUrl
+operator|=
+literal|"prints important memory utilization stats per dictionary"
 argument_list|)
 DECL|class|TestAllDictionaries
 specifier|public
@@ -1077,7 +1100,7 @@ block|{
 name|String
 name|toTest
 init|=
-literal|"hu_HU.zip"
+literal|"zu_ZA.zip"
 decl_stmt|;
 for|for
 control|(
--- lucene/analysis/common/src/test/org/apache/lucene/analysis/hunspell/TestAllDictionaries2.java
+++ lucene/analysis/common/src/test/org/apache/lucene/analysis/hunspell/TestAllDictionaries2.java
@@ -122,6 +122,22 @@ begin_import
 import|import
 name|org
 operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|util
+operator|.
+name|LuceneTestCase
+operator|.
+name|SuppressSysoutChecks
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
 name|junit
 operator|.
 name|Ignore
@@ -137,6 +153,13 @@ annotation|@
 name|Ignore
 argument_list|(
 literal|"enable manually"
+argument_list|)
+annotation|@
+name|SuppressSysoutChecks
+argument_list|(
+name|bugUrl
+operator|=
+literal|"prints important memory utilization stats per dictionary"
 argument_list|)
 DECL|class|TestAllDictionaries2
 specifier|public
