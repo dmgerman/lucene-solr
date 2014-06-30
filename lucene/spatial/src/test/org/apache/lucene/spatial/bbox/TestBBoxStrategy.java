379d12e210f18957aeddc968df1e059db508fa72
David Wayne Smiley <dsmiley@apache.org>
2014-06-30 20:09:56 +0000

    LUCENE-5771: Remove BBoxStrategy's support for Overlaps because it never actually did work.
    This is a partial commit for this issue -- just the BBox portion so as not to interfere with LUCENE-5779.  Trunk only (bbox isn't in 4x yet).
    
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1606905 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7df563e4098aa527afb5c1385180f71a409de250

--- lucene/spatial/src/test/org/apache/lucene/spatial/bbox/TestBBoxStrategy.java
+++ lucene/spatial/src/test/org/apache/lucene/spatial/bbox/TestBBoxStrategy.java
@@ -90,6 +90,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -171,6 +181,11 @@ return|;
 block|}
 annotation|@
 name|Test
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Overlaps not supported"
+argument_list|)
 DECL|method|testBasicOperaions
 specifier|public
 name|void
