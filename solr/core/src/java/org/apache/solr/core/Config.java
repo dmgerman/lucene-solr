90c145134a83189b7f3ac49168f168957956861e
Michael McCandless <mikemccand@apache.org>
2014-09-21 09:44:11 +0000

    LUCENE-5952: better error messages when version fails to parse; use simpler string tokenizer; don't check major versions at such a low level
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1626550 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f8a74cb587971582cd9c592723039a2fd30fbe67

--- solr/core/src/java/org/apache/solr/core/Config.java
+++ solr/core/src/java/org/apache/solr/core/Config.java
@@ -380,6 +380,16 @@ begin_import
 import|import
 name|java
 operator|.
+name|text
+operator|.
+name|ParseException
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
 name|util
 operator|.
 name|Arrays
@@ -2666,8 +2676,8 @@ expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
-name|IllegalArgumentException
-name|iae
+name|ParseException
+name|pe
 parameter_list|)
 block|{
 throw|throw
@@ -2682,7 +2692,7 @@ name|SERVER_ERROR
 argument_list|,
 literal|"Invalid luceneMatchVersion.  Should be of the form 'V.V.V' (e.g. 4.8.0)"
 argument_list|,
-name|iae
+name|pe
 argument_list|)
 throw|;
 block|}
