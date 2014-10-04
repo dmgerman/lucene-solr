a551b09b7f552b0b71f1141399ac754e285684ea
Robert Muir <rmuir@apache.org>
2014-10-04 15:51:31 +0000

    LUCENE-5969: javadocs
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene5969@1629406 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 206fb874bc54dd2301c517d7665fa24442114535

--- lucene/core/src/java/org/apache/lucene/codecs/lucene50/Lucene50CompoundFormat.java
+++ lucene/core/src/java/org/apache/lucene/codecs/lucene50/Lucene50CompoundFormat.java
@@ -193,6 +193,12 @@ name|Lucene50CompoundFormat
 extends|extends
 name|CompoundFormat
 block|{
+comment|/** Sole constructor. */
+DECL|method|Lucene50CompoundFormat
+specifier|public
+name|Lucene50CompoundFormat
+parameter_list|()
+block|{   }
 annotation|@
 name|Override
 DECL|method|getCompoundReader
