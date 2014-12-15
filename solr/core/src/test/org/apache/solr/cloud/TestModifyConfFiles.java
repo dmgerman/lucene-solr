8a623f95a473155d5a9c4c6b7bf95e2f3337c3e8
Alan Woodward <romseygeek@apache.org>
2014-12-15 18:25:53 +0000

    SOLR-6849: Fix another @Slow test
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1645712 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 4c76f8d13db7ae73e0369762e2efd95ff991a877

--- solr/core/src/test/org/apache/solr/cloud/TestModifyConfFiles.java
+++ solr/core/src/test/org/apache/solr/cloud/TestModifyConfFiles.java
@@ -164,6 +164,22 @@ name|StandardCharsets
 import|;
 end_import
 
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|internal
+operator|.
+name|matchers
+operator|.
+name|StringContains
+operator|.
+name|containsString
+import|;
+end_import
+
 begin_class
 DECL|class|TestModifyConfFiles
 specifier|public
@@ -279,14 +295,17 @@ name|Exception
 name|e
 parameter_list|)
 block|{
-name|assertEquals
+name|assertThat
 argument_list|(
 name|e
 operator|.
 name|getMessage
 argument_list|()
 argument_list|,
+name|containsString
+argument_list|(
 literal|"Input stream list was null for admin file write operation."
+argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
