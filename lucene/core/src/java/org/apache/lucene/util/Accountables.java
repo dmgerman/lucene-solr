5b5de9d54b3dcce6a73569e3fdb3aa6b7032c2a2
Adrien Grand <jpountz@apache.org>
2014-12-16 14:57:58 +0000

    Fix compiler warning.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1645962 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1a5b889c860231941af344a777182fddce50a296

--- lucene/core/src/java/org/apache/lucene/util/Accountables.java
+++ lucene/core/src/java/org/apache/lucene/util/Accountables.java
@@ -463,8 +463,6 @@ parameter_list|,
 specifier|final
 name|Iterable
 argument_list|<
-name|?
-extends|extends
 name|Accountable
 argument_list|>
 name|children
@@ -501,12 +499,6 @@ name|getChildResources
 parameter_list|()
 block|{
 return|return
-operator|(
-name|Iterable
-argument_list|<
-name|Accountable
-argument_list|>
-operator|)
 name|children
 return|;
 block|}
