32cca9d447c7f8b654298c7582890fa7d1865ba6
Mark Robert Miller <markrmiller@apache.org>
2015-01-13 15:12:46 +0000

    SOLR-6943: HdfsDirectoryFactory should fall back to system props for most of it's config  if it is not found in solrconfig.xml.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1651373 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1696206de684a6699938ea3d2458e3fa9658f292

--- solr/core/src/test/org/apache/solr/util/MockCoreContainer.java
+++ solr/core/src/test/org/apache/solr/util/MockCoreContainer.java
@@ -12,6 +12,10 @@ name|util
 package|;
 end_package
 
+begin_comment
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
 begin_import
 import|import
 name|org
@@ -26,9 +30,19 @@ name|CoreContainer
 import|;
 end_import
 
-begin_comment
-comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
-end_comment
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|solr
+operator|.
+name|core
+operator|.
+name|CoreDescriptor
+import|;
+end_import
 
 begin_class
 DECL|class|MockCoreContainer
@@ -37,7 +51,57 @@ class|class
 name|MockCoreContainer
 extends|extends
 name|CoreContainer
-block|{    }
+block|{
+DECL|class|MockCoreDescriptor
+specifier|public
+specifier|static
+class|class
+name|MockCoreDescriptor
+extends|extends
+name|CoreDescriptor
+block|{
+DECL|method|MockCoreDescriptor
+specifier|public
+name|MockCoreDescriptor
+parameter_list|()
+block|{
+name|super
+argument_list|(
+operator|new
+name|MockCoreContainer
+argument_list|()
+argument_list|,
+literal|"mock"
+argument_list|,
+literal|"path"
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+DECL|method|MockCoreContainer
+specifier|public
+name|MockCoreContainer
+parameter_list|()
+block|{
+name|super
+argument_list|(
+operator|new
+name|Object
+argument_list|()
+argument_list|)
+expr_stmt|;
+block|}
+DECL|method|getCoreRootDirectory
+specifier|public
+name|String
+name|getCoreRootDirectory
+parameter_list|()
+block|{
+return|return
+literal|"coreroot"
+return|;
+block|}
+block|}
 end_class
 
 end_unit
