078e7d81cdfa5fab986e4d92f168890a9dabc0b0
Michael McCandless <mikemccand@apache.org>
2015-02-01 21:46:13 +0000

    LUCENE-6214: don't deadlock when tragedy strikes during getReader and another thread is committing
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1656366 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 80e5fbc7a8c23d1da013c246ab389b6e69098fe7

--- lucene/core/src/java/org/apache/lucene/index/IndexWriter.java
+++ lucene/core/src/java/org/apache/lucene/index/IndexWriter.java
@@ -1231,16 +1231,16 @@ literal|false
 decl_stmt|;
 try|try
 block|{
-synchronized|synchronized
-init|(
-name|fullFlushLock
-init|)
-block|{
 name|boolean
 name|success
 init|=
 literal|false
 decl_stmt|;
+synchronized|synchronized
+init|(
+name|fullFlushLock
+init|)
+block|{
 try|try
 block|{
 name|anyChanges
@@ -1264,10 +1264,6 @@ name|incrementAndGet
 argument_list|()
 expr_stmt|;
 block|}
-name|success
-operator|=
-literal|true
-expr_stmt|;
 comment|// Prevent segmentInfos from changing while opening the
 comment|// reader; in theory we could instead do similar retry logic,
 comment|// just like we do when loading segments_N
@@ -1326,34 +1322,40 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-block|}
-catch|catch
-parameter_list|(
-name|AbortingException
-decl||
-name|OutOfMemoryError
-name|tragedy
-parameter_list|)
-block|{
-name|tragicEvent
-argument_list|(
-name|tragedy
-argument_list|,
-literal|"getReader"
-argument_list|)
+name|success
+operator|=
+literal|true
 expr_stmt|;
-comment|// never reached but javac disagrees:
-return|return
-literal|null
-return|;
 block|}
 finally|finally
 block|{
+comment|// Done: finish the full flush!
+name|docWriter
+operator|.
+name|finishFullFlush
+argument_list|(
+name|this
+argument_list|,
+name|success
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
-operator|!
 name|success
 condition|)
+block|{
+name|processEvents
+argument_list|(
+literal|false
+argument_list|,
+literal|true
+argument_list|)
+expr_stmt|;
+name|doAfterFlush
+argument_list|()
+expr_stmt|;
+block|}
+else|else
 block|{
 if|if
 condition|(
@@ -1375,32 +1377,6 @@ literal|"hit exception during NRT reader"
 argument_list|)
 expr_stmt|;
 block|}
-block|}
-if|if
-condition|(
-name|tragedy
-operator|==
-literal|null
-condition|)
-block|{
-comment|// Done: finish the full flush! (unless we hit OOM or something)
-name|docWriter
-operator|.
-name|finishFullFlush
-argument_list|(
-name|success
-argument_list|)
-expr_stmt|;
-name|processEvents
-argument_list|(
-literal|false
-argument_list|,
-literal|true
-argument_list|)
-expr_stmt|;
-name|doAfterFlush
-argument_list|()
-expr_stmt|;
 block|}
 block|}
 block|}
@@ -1459,6 +1435,26 @@ name|success2
 operator|=
 literal|true
 expr_stmt|;
+block|}
+catch|catch
+parameter_list|(
+name|AbortingException
+decl||
+name|OutOfMemoryError
+name|tragedy
+parameter_list|)
+block|{
+name|tragicEvent
+argument_list|(
+name|tragedy
+argument_list|,
+literal|"getReader"
+argument_list|)
+expr_stmt|;
+comment|// never reached but javac disagrees:
+return|return
+literal|null
+return|;
 block|}
 finally|finally
 block|{
@@ -2088,6 +2084,12 @@ name|boolean
 name|create
 parameter_list|)
 block|{
+comment|// Make sure no new readers can be opened if another thread just closed us:
+name|ensureOpen
+argument_list|(
+literal|false
+argument_list|)
+expr_stmt|;
 assert|assert
 name|info
 operator|.
@@ -6442,13 +6444,13 @@ init|=
 literal|false
 decl_stmt|;
 comment|/* hold the full flush lock to prevent concurrency commits / NRT reopens to      * get in our way and do unnecessary work. -- if we don't lock this here we might      * get in trouble if */
+comment|/*      * We first abort and trash everything we have in-memory      * and keep the thread-states locked, the lockAndAbortAll operation      * also guarantees "point in time semantics" ie. the checkpoint that we need in terms      * of logical happens-before relationship in the DW. So we do      * abort all in memory structures       * We also drop global field numbering before during abort to make      * sure it's just like a fresh index.      */
+try|try
+block|{
 synchronized|synchronized
 init|(
 name|fullFlushLock
 init|)
-block|{
-comment|/*        * We first abort and trash everything we have in-memory        * and keep the thread-states locked, the lockAndAbortAll operation        * also guarantees "point in time semantics" ie. the checkpoint that we need in terms        * of logical happens-before relationship in the DW. So we do        * abort all in memory structures         * We also drop global field numbering before during abort to make        * sure it's just like a fresh index.        */
-try|try
 block|{
 name|docWriter
 operator|.
@@ -6521,6 +6523,13 @@ expr_stmt|;
 block|}
 finally|finally
 block|{
+name|docWriter
+operator|.
+name|unlockAllAfterAbortAll
+argument_list|(
+name|this
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 operator|!
@@ -6551,6 +6560,7 @@ block|}
 block|}
 block|}
 block|}
+block|}
 catch|catch
 parameter_list|(
 name|OutOfMemoryError
@@ -6564,17 +6574,6 @@ argument_list|,
 literal|"deleteAll"
 argument_list|)
 expr_stmt|;
-block|}
-finally|finally
-block|{
-name|docWriter
-operator|.
-name|unlockAllAfterAbortAll
-argument_list|(
-name|this
-argument_list|)
-expr_stmt|;
-block|}
 block|}
 block|}
 comment|/** Aborts running merges.  Be careful when using this    *  method: when you abort a long-running merge, you lose    *  a lot of work that must later be redone. */
@@ -8892,6 +8891,8 @@ name|docWriter
 operator|.
 name|finishFullFlush
 argument_list|(
+name|this
+argument_list|,
 name|flushSuccess
 argument_list|)
 expr_stmt|;
@@ -9733,6 +9734,8 @@ name|docWriter
 operator|.
 name|finishFullFlush
 argument_list|(
+name|this
+argument_list|,
 name|flushSuccess
 argument_list|)
 expr_stmt|;
