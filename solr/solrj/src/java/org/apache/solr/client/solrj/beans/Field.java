ef2b76283d78a3bbc7ac186a10e14e933530bac5
Noble Paul <noble@apache.org>
2015-02-14 19:04:22 +0000

    SOLR-1945: Add support for child docs in DocumentObjectBinder
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1659845 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 74387f8d68f830c2d4a7b03ef4287c289801efff

--- solr/solrj/src/java/org/apache/solr/client/solrj/beans/Field.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/beans/Field.java
@@ -49,6 +49,26 @@ import|;
 end_import
 
 begin_import
+import|import static
+name|org
+operator|.
+name|apache
+operator|.
+name|solr
+operator|.
+name|client
+operator|.
+name|solrj
+operator|.
+name|beans
+operator|.
+name|DocumentObjectBinder
+operator|.
+name|DEFAULT
+import|;
+end_import
+
+begin_import
 import|import
 name|java
 operator|.
@@ -73,7 +93,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  *  * @since solr 1.3  */
+comment|/**  * This class can be used to annotate a field or a setter an any class  * and SlrJ would help you convert to SolrInputDocument and from SolrDocument  *  * @since solr 1.3  */
 end_comment
 
 begin_annotation_defn
@@ -96,15 +116,13 @@ specifier|public
 annotation_defn|@interface
 name|Field
 block|{
-DECL|field|DEFAULT
-specifier|public
-specifier|static
-specifier|final
-name|String
-name|DEFAULT
-init|=
-literal|"#default"
-decl_stmt|;
+DECL|field|false
+name|boolean
+name|child
+parameter_list|()
+default|default
+literal|false
+function_decl|;
 DECL|field|DEFAULT
 name|String
 name|value
