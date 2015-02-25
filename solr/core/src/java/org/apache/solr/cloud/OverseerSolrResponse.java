57fdfe829e08d81e37d81c4b3e39eb28400a9f59
Alan Woodward <romseygeek@apache.org>
2015-02-25 11:14:02 +0000

    SOLR-7145: SolrRequest is now parametrized by its response type
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1662198 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 732f9aeecc0d0a329b49360a4bc36f7740c0ce6b

--- solr/core/src/java/org/apache/solr/cloud/OverseerSolrResponse.java
+++ solr/core/src/java/org/apache/solr/cloud/OverseerSolrResponse.java
@@ -62,6 +62,11 @@ name|responseList
 init|=
 literal|null
 decl_stmt|;
+DECL|field|elapsedTime
+specifier|private
+name|long
+name|elapsedTime
+decl_stmt|;
 DECL|method|OverseerSolrResponse
 specifier|public
 name|OverseerSolrResponse
@@ -83,9 +88,8 @@ name|long
 name|getElapsedTime
 parameter_list|()
 block|{
-comment|// TODO Auto-generated method stub
 return|return
-literal|0
+name|elapsedTime
 return|;
 block|}
 annotation|@
@@ -107,6 +111,24 @@ operator|.
 name|responseList
 operator|=
 name|rsp
+expr_stmt|;
+block|}
+annotation|@
+name|Override
+DECL|method|setElapsedTime
+specifier|public
+name|void
+name|setElapsedTime
+parameter_list|(
+name|long
+name|elapsedTime
+parameter_list|)
+block|{
+name|this
+operator|.
+name|elapsedTime
+operator|=
+name|elapsedTime
 expr_stmt|;
 block|}
 annotation|@
