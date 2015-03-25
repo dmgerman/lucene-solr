d428ce1c64d6fdcf70b990b69433b6fb11735476
Shai Erera <shaie@apache.org>
2015-03-25 19:57:05 +0000

    check coll==null in CollectionRef.toString()
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1669214 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b741482d583215148f03d34d7e3646be48d5e761

--- solr/solrj/src/java/org/apache/solr/common/cloud/ClusterState.java
+++ solr/solrj/src/java/org/apache/solr/common/cloud/ClusterState.java
@@ -1170,7 +1170,7 @@ name|sb
 operator|.
 name|append
 argument_list|(
-literal|" collections:"
+literal|"collections:"
 operator|+
 name|collectionStates
 argument_list|)
@@ -2208,12 +2208,26 @@ name|String
 name|toString
 parameter_list|()
 block|{
+if|if
+condition|(
+name|coll
+operator|!=
+literal|null
+condition|)
+block|{
 return|return
 name|coll
 operator|.
 name|toString
 argument_list|()
 return|;
+block|}
+else|else
+block|{
+return|return
+literal|"null DocCollection ref"
+return|;
+block|}
 block|}
 block|}
 block|}
