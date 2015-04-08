3e607e8ee73469c735d67547b8c0529daf8d585a
Alan Woodward <romseygeek@apache.org>
2015-04-08 15:28:13 +0000

    SOLR-1387: Move contains() method to SimpleFacets
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1672106 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3c0ff1de91193e1ffbb53dcb16d6d2a83d2e6bf5

--- lucene/demo/src/test/org/apache/lucene/demo/facet/TestSimpleFacetsExample.java
+++ lucene/demo/src/test/org/apache/lucene/demo/facet/TestSimpleFacetsExample.java
@@ -20,16 +20,6 @@ end_comment
 
 begin_import
 import|import
-name|java
-operator|.
-name|util
-operator|.
-name|List
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
@@ -63,6 +53,16 @@ operator|.
 name|junit
 operator|.
 name|Test
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|List
 import|;
 end_import
 
