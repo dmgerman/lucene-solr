6c2a5657090bab134a897166f41d45c9f3043594
David Wayne Smiley <dsmiley@apache.org>
2015-04-13 12:01:38 +0000

    LUCENE-6196: Geo3d initial checkin
    Deltas from Karl's first upload: change of package, some hashCode() impls, a few toString() impls, some javadoc formatting.   New Geo3dRtTest.  Geo3dShape throws an exception if not geo.
    
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/lucene6196@1673165 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: bde056c763231cd4f35acbc708cfc07dc181b1fc

---
+++ lucene/spatial/src/java/org/apache/lucene/spatial/spatial4j/geo3d/package-info.java
@@ -0,0 +1,24 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/** Shapes implemented using 3D planar geometry. */
+end_comment
+
+begin_package
+DECL|package|org.apache.lucene.spatial.spatial4j.geo3d
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|spatial
+operator|.
+name|spatial4j
+operator|.
+name|geo3d
+package|;
+end_package
+
+end_unit
+
