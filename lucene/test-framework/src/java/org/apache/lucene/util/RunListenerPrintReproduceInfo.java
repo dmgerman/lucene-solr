3fc94af4f6153b31539780cfece2476f709f5613
Robert Muir <rmuir@apache.org>
2015-04-17 11:21:21 +0000

    LUCENE-6432: add SuppressReproduceLine
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1674277 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 44a97f67b752d2be51e5283139e819b0ffd3ebc6

--- lucene/test-framework/src/java/org/apache/lucene/util/RunListenerPrintReproduceInfo.java
+++ lucene/test-framework/src/java/org/apache/lucene/util/RunListenerPrintReproduceInfo.java
@@ -202,6 +202,12 @@ specifier|private
 name|boolean
 name|printDiagnosticsAfterClass
 decl_stmt|;
+comment|/** true if we should skip the reproduce string (diagnostics are independent) */
+DECL|field|suppressReproduceLine
+specifier|private
+name|boolean
+name|suppressReproduceLine
+decl_stmt|;
 annotation|@
 name|Override
 DECL|method|testRunStarted
@@ -243,6 +249,19 @@ operator|.
 name|getTargetClass
 argument_list|()
 decl_stmt|;
+name|suppressReproduceLine
+operator|=
+name|targetClass
+operator|.
+name|isAnnotationPresent
+argument_list|(
+name|LuceneTestCase
+operator|.
+name|SuppressReproduceLine
+operator|.
+name|class
+argument_list|)
+expr_stmt|;
 name|testClassesRun
 operator|.
 name|add
@@ -634,6 +653,13 @@ name|String
 name|testName
 parameter_list|)
 block|{
+if|if
+condition|(
+name|suppressReproduceLine
+condition|)
+block|{
+return|return;
+block|}
 if|if
 condition|(
 name|TEST_LINE_DOCS_FILE
