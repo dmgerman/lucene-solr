4616cc96da6c28c344bd2b86d622e9ce349a927b
Mark Robert Miller <markrmiller@apache.org>
2015-04-24 15:08:11 +0000

    SOLR-7311: Add some infrastructure and tests to make sure Solr works well in the face of Name Node high availability and failover.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1675883 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ed9e1fc1b5557360fdcd9c90aa3ff3802152c05a

--- solr/core/src/test/org/apache/solr/cloud/hdfs/HdfsThreadLeakTest.java
+++ solr/core/src/test/org/apache/solr/cloud/hdfs/HdfsThreadLeakTest.java
@@ -269,6 +269,8 @@ name|getAbsolutePath
 argument_list|()
 argument_list|,
 literal|false
+argument_list|,
+literal|false
 argument_list|)
 expr_stmt|;
 block|}
@@ -337,13 +339,15 @@ parameter_list|()
 throws|throws
 name|IOException
 block|{
-name|URI
+name|String
 name|uri
 init|=
-name|dfsCluster
+name|HdfsTestUtil
 operator|.
 name|getURI
-argument_list|()
+argument_list|(
+name|dfsCluster
+argument_list|)
 decl_stmt|;
 name|Path
 name|path
@@ -352,9 +356,6 @@ operator|new
 name|Path
 argument_list|(
 name|uri
-operator|.
-name|toString
-argument_list|()
 argument_list|)
 decl_stmt|;
 name|Configuration
