0c24838621b2533b011443b25fa4a42aa58177f0
Chris M. Hostetter <hossman@apache.org>
2015-05-08 18:44:03 +0000

    SOLR-7461: stats.field now supports individual local params for 'countDistinct' and 'distinctValues', 'calcdistinct' is still supported as an alias for both options
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1678422 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d479fd7be145a6a16e2bf57cff1264e7f2f147b5

--- solr/core/src/java/org/apache/solr/handler/component/StatsValuesFactory.java
+++ solr/core/src/java/org/apache/solr/handler/component/StatsValuesFactory.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0   * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -487,6 +487,7 @@ specifier|protected
 name|boolean
 name|computeCalcDistinct
 decl_stmt|;
+comment|// needed for either countDistinct or distinctValues
 DECL|field|computeMin
 specifier|final
 specifier|protected
@@ -647,7 +648,16 @@ name|calculateStats
 argument_list|(
 name|Stat
 operator|.
-name|calcdistinct
+name|countDistinct
+argument_list|)
+operator|||
+name|statsField
+operator|.
+name|calculateStats
+argument_list|(
+name|Stat
+operator|.
+name|distinctValues
 argument_list|)
 expr_stmt|;
 name|this
@@ -1525,7 +1535,7 @@ name|includeInResponse
 argument_list|(
 name|Stat
 operator|.
-name|calcdistinct
+name|distinctValues
 argument_list|)
 condition|)
 block|{
@@ -1538,6 +1548,19 @@ argument_list|,
 name|distinctValues
 argument_list|)
 expr_stmt|;
+block|}
+if|if
+condition|(
+name|statsField
+operator|.
+name|includeInResponse
+argument_list|(
+name|Stat
+operator|.
+name|countDistinct
+argument_list|)
+condition|)
+block|{
 name|res
 operator|.
 name|add
--- solr/core/src/test/org/apache/solr/TestDistributedSearch.java
+++ solr/core/src/test/org/apache/solr/TestDistributedSearch.java
@@ -5647,6 +5647,49 @@ literal|"{!min=true calcdistinct=true}"
 operator|+
 name|i1
 argument_list|)
+block|,
+name|params
+argument_list|(
+literal|"stats.calcdistinct"
+argument_list|,
+literal|"false"
+argument_list|,
+literal|"f."
+operator|+
+name|i1
+operator|+
+literal|".stats.calcdistinct"
+argument_list|,
+literal|"false"
+argument_list|,
+literal|"stats.field"
+argument_list|,
+literal|"{!min=true countDistinct=true distinctValues=true}"
+operator|+
+name|i1
+argument_list|)
+block|,
+name|params
+argument_list|(
+literal|"stats.field"
+argument_list|,
+literal|"{!min=true countDistinct=true distinctValues=true}"
+operator|+
+name|i1
+argument_list|)
+block|,
+name|params
+argument_list|(
+literal|"yes"
+argument_list|,
+literal|"true"
+argument_list|,
+literal|"stats.field"
+argument_list|,
+literal|"{!min=$yes countDistinct=$yes distinctValues=$yes}"
+operator|+
+name|i1
+argument_list|)
 block|,       }
 control|)
 block|{
@@ -5956,6 +5999,27 @@ argument_list|,
 literal|"stats.field"
 argument_list|,
 literal|"{!min=true calcdistinct=false}"
+operator|+
+name|i1
+argument_list|)
+block|,
+name|params
+argument_list|(
+literal|"stats.calcdistinct"
+argument_list|,
+literal|"true"
+argument_list|,
+literal|"f."
+operator|+
+name|i1
+operator|+
+literal|".stats.calcdistinct"
+argument_list|,
+literal|"true"
+argument_list|,
+literal|"stats.field"
+argument_list|,
+literal|"{!min=true countDistinct=false distinctValues=false}"
 operator|+
 name|i1
 argument_list|)
@@ -6635,7 +6699,7 @@ literal|"Sanity check failed: either test broke, or test changed, or you adjuste
 operator|+
 literal|" (adjust constant accordingly if intentional)"
 argument_list|,
-literal|4235
+literal|5082
 argument_list|,
 name|numTotalStatQueries
 argument_list|)
@@ -8846,6 +8910,11 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+name|ignoreException
+argument_list|(
+literal|"parameter cannot be negative"
+argument_list|)
+expr_stmt|;
 try|try
 block|{
 name|SolrQuery
@@ -8969,6 +9038,9 @@ argument_list|()
 argument_list|)
 expr_stmt|;
 block|}
+name|resetExceptionIgnores
+argument_list|()
+expr_stmt|;
 block|}
 block|}
 end_class
