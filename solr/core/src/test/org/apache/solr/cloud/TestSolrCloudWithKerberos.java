7ec100fae183c00a9cf3b1830cdf7dab0aa8730e
Anshum Gupta <anshum@apache.org>
2015-05-26 16:44:54 +0000

    SOLR-7468: Ignoring the older test as the new one tests exactly the same thing but with less moving parts and bootstrapping.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1681792 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e461ab66f17626722bd5b0bba9149e0e125a2245

--- solr/core/src/test/org/apache/solr/cloud/TestSolrCloudWithKerberos.java
+++ solr/core/src/test/org/apache/solr/cloud/TestSolrCloudWithKerberos.java
@@ -306,11 +306,23 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
 
 begin_class
+annotation|@
+name|Ignore
 annotation|@
 name|SolrTestCaseJ4
 operator|.
