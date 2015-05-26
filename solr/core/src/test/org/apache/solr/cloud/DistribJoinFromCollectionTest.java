f418d794d26bfd0c355fd65c48953d019878a28c
Shalin Shekhar Mangar <shalin@apache.org>
2015-05-26 18:33:33 +0000

    SOLR-7595: Allow method chaining for all CollectionAdminRequests in Solrj
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1681808 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a94d110aa4491e46326cc5e1d53903137e74e97a

--- solr/core/src/test/org/apache/solr/cloud/DistribJoinFromCollectionTest.java
+++ solr/core/src/test/org/apache/solr/cloud/DistribJoinFromCollectionTest.java
@@ -1235,14 +1235,12 @@ name|CollectionAdminRequest
 operator|.
 name|Delete
 argument_list|()
-decl_stmt|;
-name|req
 operator|.
 name|setCollectionName
 argument_list|(
 name|c
 argument_list|)
-expr_stmt|;
+decl_stmt|;
 name|req
 operator|.
 name|process
