16d53f8e30ed77b32ae6779ffcdcab4fa3952280
Dawid Weiss <dweiss@apache.org>
2015-07-16 11:29:55 +0000

    Locale-sensitive format is banned.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/branches/solr7787@1691367 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 4404d92e4897dbaef17f7f3931a40abb6c97db76

--- solr/core/src/test/org/apache/solr/util/hll/BitVectorTest.java
+++ solr/core/src/test/org/apache/solr/util/hll/BitVectorTest.java
@@ -20,6 +20,16 @@ end_package
 
 begin_import
 import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Locale
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -702,6 +712,10 @@ name|String
 operator|.
 name|format
 argument_list|(
+name|Locale
+operator|.
+name|ROOT
+argument_list|,
 literal|"expected more elements: width=%s, count=%s"
 argument_list|,
 name|width
@@ -733,6 +747,10 @@ name|String
 operator|.
 name|format
 argument_list|(
+name|Locale
+operator|.
+name|ROOT
+argument_list|,
 literal|"expected no more elements: width=%s, count=%s"
 argument_list|,
 name|width
