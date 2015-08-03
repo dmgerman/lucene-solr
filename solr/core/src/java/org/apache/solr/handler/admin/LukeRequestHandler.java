fbe7882698ac35c6515152d668999259f529bc8e
Erik Hatcher <ehatcher@apache.org>
2015-08-03 17:33:00 +0000

    SOLR-7799: fix mistake of docs count for terms being inside the new if condition inadvertently
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1693943 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5b21274051fb0a3facebbb05fb79d0f0bc72298a

--- solr/core/src/java/org/apache/solr/handler/admin/LukeRequestHandler.java
+++ solr/core/src/java/org/apache/solr/handler/admin/LukeRequestHandler.java
@@ -3353,6 +3353,7 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
+block|}
 name|fieldMap
 operator|.
 name|add
@@ -3365,7 +3366,6 @@ name|getDocCount
 argument_list|()
 argument_list|)
 expr_stmt|;
-block|}
 block|}
 if|if
 condition|(
