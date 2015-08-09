a4683bcf3416389b201f5738b2dbfdbaae964c57
Uwe Schindler <uschindler@apache.org>
2015-08-09 12:13:00 +0000

    LUCENE-6417: make params final (for Java 7)
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1694879 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3115cd07fcde22ee929246de153443624337255e

--- lucene/expressions/src/java/org/apache/lucene/expressions/js/JavascriptCompiler.java
+++ lucene/expressions/src/java/org/apache/lucene/expressions/js/JavascriptCompiler.java
@@ -1063,12 +1063,15 @@ specifier|private
 name|void
 name|generateClass
 parameter_list|(
+specifier|final
 name|ParseTree
 name|parseTree
 parameter_list|,
+specifier|final
 name|ClassWriter
 name|classWriter
 parameter_list|,
+specifier|final
 name|Map
 argument_list|<
 name|String
