10992d950cb82fb2836f7521fe50cdd4d23cb738
Michael McCandless <mikemccand@apache.org>
2015-08-29 18:56:05 +0000

    fix test to catch any Throwable, not just IOExc, and then print the IW infoStream
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1700068 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 85b6c90b3a74cf1592ad0b1159451e45e9e5a6ce

--- lucene/test-framework/src/java/org/apache/lucene/store/BaseLockFactoryTestCase.java
+++ lucene/test-framework/src/java/org/apache/lucene/store/BaseLockFactoryTestCase.java
@@ -1248,8 +1248,8 @@ comment|// FIFO).
 block|}
 catch|catch
 parameter_list|(
-name|Exception
-name|e
+name|Throwable
+name|t
 parameter_list|)
 block|{
 name|hitException
@@ -1264,13 +1264,13 @@ name|println
 argument_list|(
 literal|"Stress Test Index Writer: creation hit unexpected exception: "
 operator|+
-name|e
+name|t
 operator|.
 name|toString
 argument_list|()
 argument_list|)
 expr_stmt|;
-name|e
+name|t
 operator|.
 name|printStackTrace
 argument_list|(
@@ -1310,8 +1310,8 @@ expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
-name|IOException
-name|e
+name|Throwable
+name|t
 parameter_list|)
 block|{
 name|hitException
@@ -1326,13 +1326,13 @@ name|println
 argument_list|(
 literal|"Stress Test Index Writer: addDoc hit unexpected exception: "
 operator|+
-name|e
+name|t
 operator|.
 name|toString
 argument_list|()
 argument_list|)
 expr_stmt|;
-name|e
+name|t
 operator|.
 name|printStackTrace
 argument_list|(
@@ -1365,8 +1365,8 @@ expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
-name|IOException
-name|e
+name|Throwable
+name|t
 parameter_list|)
 block|{
 name|hitException
@@ -1381,13 +1381,13 @@ name|println
 argument_list|(
 literal|"Stress Test Index Writer: close hit unexpected exception: "
 operator|+
-name|e
+name|t
 operator|.
 name|toString
 argument_list|()
 argument_list|)
 expr_stmt|;
-name|e
+name|t
 operator|.
 name|printStackTrace
 argument_list|(
