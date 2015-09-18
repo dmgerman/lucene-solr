4d99bf916d6ac04a3fbffa147039b37aaeeff4c4
Shalin Shekhar Mangar <shalin@apache.org>
2015-09-18 19:32:20 +0000

    Renamed a few variables to be more explicit
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1703914 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c0da2bc3b6b4bba7bee45a7ba9b472606fb48328

--- solr/core/src/java/org/apache/solr/handler/IndexFetcher.java
+++ solr/core/src/java/org/apache/solr/handler/IndexFetcher.java
@@ -1530,17 +1530,17 @@ name|stop
 init|=
 literal|false
 decl_stmt|;
-DECL|field|useInternal
+DECL|field|useInternalCompression
 specifier|private
 name|boolean
-name|useInternal
+name|useInternalCompression
 init|=
 literal|false
 decl_stmt|;
-DECL|field|useExternal
+DECL|field|useExternalCompression
 specifier|private
 name|boolean
-name|useExternal
+name|useExternalCompression
 init|=
 literal|false
 decl_stmt|;
@@ -1785,7 +1785,7 @@ argument_list|(
 name|COMPRESSION
 argument_list|)
 decl_stmt|;
-name|useInternal
+name|useInternalCompression
 operator|=
 name|INTERNAL
 operator|.
@@ -1794,7 +1794,7 @@ argument_list|(
 name|compress
 argument_list|)
 expr_stmt|;
-name|useExternal
+name|useExternalCompression
 operator|=
 name|EXTERNAL
 operator|.
@@ -1877,7 +1877,7 @@ name|httpBasicAuthUser
 argument_list|,
 name|httpBasicAuthPassword
 argument_list|,
-name|useExternal
+name|useExternalCompression
 argument_list|)
 expr_stmt|;
 block|}
@@ -9210,7 +9210,7 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|useInternal
+name|useInternalCompression
 condition|)
 block|{
 name|params
@@ -9348,7 +9348,7 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|useInternal
+name|useInternalCompression
 condition|)
 block|{
 name|is
