da8c50bcf5b722339897b21ccae7bca42c8e9b8b
Uwe Schindler <uschindler@apache.org>
2015-10-18 10:03:52 +0000

    Supress unchecked warning
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1709249 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: cf0c29a018c27dd6e2eef61ed89b450cd055dad0

--- lucene/core/src/java/org/apache/lucene/search/Multiset.java
+++ lucene/core/src/java/org/apache/lucene/search/Multiset.java
@@ -318,6 +318,11 @@ return|;
 block|}
 annotation|@
 name|Override
+annotation|@
+name|SuppressWarnings
+argument_list|(
+literal|"unchecked"
+argument_list|)
 DECL|method|remove
 specifier|public
 name|boolean
