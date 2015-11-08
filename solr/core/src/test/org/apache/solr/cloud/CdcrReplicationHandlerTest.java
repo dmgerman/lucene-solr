61af2b213d5fae1521b126d057866bef3d674182
Erick Erickson <erick@apache.org>
2015-11-08 05:18:53 +0000

    SOLR-6273: Took out inadvertent copyright comment
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1713207 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 4caac507123df5ff28167765576a6b4a34c94ac9

--- solr/core/src/test/org/apache/solr/cloud/CdcrReplicationHandlerTest.java
+++ solr/core/src/test/org/apache/solr/cloud/CdcrReplicationHandlerTest.java
@@ -1,8 +1,4 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
-begin_comment
-comment|/*  * Copyright (c) 2015 Renaud Delbru. All Rights Reserved.  */
-end_comment
-
 begin_package
 DECL|package|org.apache.solr.cloud
 package|package
