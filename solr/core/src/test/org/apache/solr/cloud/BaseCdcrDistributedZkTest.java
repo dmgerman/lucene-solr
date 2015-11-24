754932d14ad62d478a5429ba3ee62be4929504dc
Erick Erickson <erick@apache.org>
2015-11-24 18:50:14 +0000

    SOLR-8263: Reverting commit, missed latest patch from Renaud
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1716233 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d9129621aa06de87f56abab1831306f7ce45fd04

--- solr/core/src/test/org/apache/solr/cloud/BaseCdcrDistributedZkTest.java
+++ solr/core/src/test/org/apache/solr/cloud/BaseCdcrDistributedZkTest.java
@@ -1482,7 +1482,7 @@ throw|throw
 operator|new
 name|AssertionError
 argument_list|(
-literal|"Timeout while trying to assert number of documents @ "
+literal|"Timeout while trying to assert number of documents on collection: "
 operator|+
 name|collection
 argument_list|,
