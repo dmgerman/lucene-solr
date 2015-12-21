801ad893d935313fe8df0b17f9b457ed088515e6
Mark Robert Miller <markrmiller@apache.org>
2015-12-21 14:00:38 +0000

    SOLR-8371: The Future field should now be a local variable.
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1721158 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a38a69a979f2c1828ba987b3ef48125a054796e9

--- solr/core/src/java/org/apache/solr/update/DefaultSolrCoreState.java
+++ solr/core/src/java/org/apache/solr/update/DefaultSolrCoreState.java
@@ -442,12 +442,6 @@ specifier|volatile
 name|RecoveryStrategy
 name|recoveryStrat
 decl_stmt|;
-DECL|field|future
-specifier|private
-specifier|volatile
-name|Future
-name|future
-decl_stmt|;
 DECL|field|lastReplicationSuccess
 specifier|private
 specifier|volatile
@@ -1408,8 +1402,12 @@ argument_list|(
 name|recoveringAfterStartup
 argument_list|)
 expr_stmt|;
+name|Future
+argument_list|<
+name|?
+argument_list|>
 name|future
-operator|=
+init|=
 name|cc
 operator|.
 name|getUpdateShardHandler
@@ -1422,7 +1420,7 @@ name|submit
 argument_list|(
 name|recoveryStrat
 argument_list|)
-expr_stmt|;
+decl_stmt|;
 try|try
 block|{
 name|future
