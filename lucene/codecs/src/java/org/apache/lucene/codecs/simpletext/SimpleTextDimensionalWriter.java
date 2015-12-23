f432708eebacfe98efb89cecfa5ccc26376b733e
Michael McCandless <mikemccand@apache.org>
2015-12-23 22:36:16 +0000

    also fix SimpleTextDimValues to skip writing if merged segment had deleted all docs with the dimensional field
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1721583 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3430150819e36558a19794bb788f851b1259fafb

--- lucene/codecs/src/java/org/apache/lucene/codecs/simpletext/SimpleTextDimensionalWriter.java
+++ lucene/codecs/src/java/org/apache/lucene/codecs/simpletext/SimpleTextDimensionalWriter.java
@@ -1063,6 +1063,17 @@ block|}
 block|}
 argument_list|)
 expr_stmt|;
+comment|// We could have 0 points on merge since all docs with dimensional fields may be deleted:
+if|if
+condition|(
+name|writer
+operator|.
+name|getPointCount
+argument_list|()
+operator|>
+literal|0
+condition|)
+block|{
 name|indexFPs
 operator|.
 name|put
@@ -1079,6 +1090,7 @@ name|dataOut
 argument_list|)
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 DECL|method|write
 specifier|private
