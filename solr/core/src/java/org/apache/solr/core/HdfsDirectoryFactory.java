f50ff286d2e6eb531650ffabfc4100484fabd62a
Christine Poerschke <cpoerschke@apache.org>
2016-01-08 15:59:36 +0000

    SOLR-8505: core/DirectoryFactory.LOCK_TYPE_HDFS - add & use it instead of String literals
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1723751 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2580956b8b00e7fbd52d2db418a512a8afb32117

--- solr/core/src/java/org/apache/solr/core/HdfsDirectoryFactory.java
+++ solr/core/src/java/org/apache/solr/core/HdfsDirectoryFactory.java
@@ -1153,16 +1153,22 @@ operator|==
 name|rawLockType
 condition|)
 block|{
+name|rawLockType
+operator|=
+name|DirectoryFactory
+operator|.
+name|LOCK_TYPE_HDFS
+expr_stmt|;
 name|LOG
 operator|.
 name|warn
 argument_list|(
-literal|"No lockType configured, assuming 'hdfs'."
-argument_list|)
-expr_stmt|;
+literal|"No lockType configured, assuming '"
+operator|+
 name|rawLockType
-operator|=
-literal|"hdfs"
+operator|+
+literal|"'."
+argument_list|)
 expr_stmt|;
 block|}
 specifier|final
@@ -1187,7 +1193,9 @@ name|lockType
 condition|)
 block|{
 case|case
-literal|"hdfs"
+name|DirectoryFactory
+operator|.
+name|LOCK_TYPE_HDFS
 case|:
 return|return
 name|HdfsLockFactory
