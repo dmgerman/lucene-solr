59db78f30c1ce5a70f5f732f270d0a724fa0d824
Joel Bernstein <jbernste@apache.org>
2016-01-15 18:55:59 +0000

    SOLR-8513,SOLR-8502: Implement ResultSetImpl.getMetaData()
    git-svn-id: https://svn.apache.org/repos/asf/lucene/dev/trunk@1724870 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 99da9e8fe3d22b687bf6d441e1bfe9f1b9f6e806

--- solr/solrj/src/java/org/apache/solr/client/solrj/io/sql/ResultSetImpl.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/io/sql/ResultSetImpl.java
@@ -1140,11 +1140,13 @@ parameter_list|()
 throws|throws
 name|SQLException
 block|{
-throw|throw
+return|return
 operator|new
-name|UnsupportedOperationException
-argument_list|()
-throw|;
+name|ResultSetMetaDataImpl
+argument_list|(
+name|this
+argument_list|)
+return|;
 block|}
 annotation|@
 name|Override
