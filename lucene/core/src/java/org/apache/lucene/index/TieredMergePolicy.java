74c28920796c413b34ed8ab0fb370dfe34d0d7d2
Christine Poerschke <cpoerschke@apache.org>
2016-02-01 17:11:47 +0000

    LUCENE-7005: TieredMergePolicy tweaks (>= vs. >, @see get vs. set)
    Former-commit-id: fce97a6ca6e14b3d0216957cee1621327c452dfc

--- lucene/core/src/java/org/apache/lucene/index/TieredMergePolicy.java
+++ lucene/core/src/java/org/apache/lucene/index/TieredMergePolicy.java
@@ -377,7 +377,7 @@ return|return
 name|this
 return|;
 block|}
-comment|/** Returns the current maxMergedSegmentMB setting.    *    * @see #getMaxMergedSegmentMB */
+comment|/** Returns the current maxMergedSegmentMB setting.    *    * @see #setMaxMergedSegmentMB */
 DECL|method|getMaxMergedSegmentMB
 specifier|public
 name|double
@@ -461,7 +461,7 @@ throw|throw
 operator|new
 name|IllegalArgumentException
 argument_list|(
-literal|"floorSegmentMB must be>= 0.0 (got "
+literal|"floorSegmentMB must be> 0.0 (got "
 operator|+
 name|v
 operator|+
