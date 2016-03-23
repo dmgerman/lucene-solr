fa8099d49bf94aaa67175dafab37768365ed415f
Mike McCandless <mikemccand@apache.org>
2016-03-23 06:20:26 -0400

    disable query cache when this test uses threads, so seeds reliably reproduce
    Former-commit-id: 390166fa25a4ff75cc295e2290618d30fa789f19

--- lucene/spatial/src/test/org/apache/lucene/spatial/util/BaseGeoPointTestCase.java
+++ lucene/spatial/src/test/org/apache/lucene/spatial/util/BaseGeoPointTestCase.java
@@ -4386,6 +4386,22 @@ argument_list|,
 literal|false
 argument_list|)
 decl_stmt|;
+if|if
+condition|(
+name|useThreads
+condition|)
+block|{
+comment|// We must disable query cache otherwise test seed may not reproduce since different
+comment|// threads may or may not get a cache hit or miss depending on order the JVM
+comment|// schedules the threads:
+name|s
+operator|.
+name|setQueryCache
+argument_list|(
+literal|null
+argument_list|)
+expr_stmt|;
+block|}
 comment|// Make sure queries are thread safe:
 name|int
 name|numThreads
@@ -5017,21 +5033,21 @@ literal|"  docID="
 operator|+
 name|docID
 operator|+
-literal|" centerLon="
-operator|+
-name|centerLon
-operator|+
 literal|" centerLat="
 operator|+
 name|centerLat
 operator|+
-literal|" pointLon="
+literal|" centerLon="
 operator|+
-name|pointLon
+name|centerLon
 operator|+
 literal|" pointLat="
 operator|+
 name|pointLat
+operator|+
+literal|" pointLon="
+operator|+
+name|pointLon
 operator|+
 literal|" distanceMeters="
 operator|+
