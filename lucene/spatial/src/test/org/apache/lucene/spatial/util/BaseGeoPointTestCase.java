dc14a2f97911dbbc0b87ac9cadc49d6df0e419ac
Mike McCandless <mikemccand@apache.org>
2016-03-28 14:13:44 -0400

    fix nocommit
    Former-commit-id: 565525fdb7ff323be5df11f80460b01ab2724080

--- lucene/spatial/src/test/org/apache/lucene/spatial/util/BaseGeoPointTestCase.java
+++ lucene/spatial/src/test/org/apache/lucene/spatial/util/BaseGeoPointTestCase.java
@@ -6313,7 +6313,6 @@ operator|.
 name|close
 argument_list|()
 expr_stmt|;
-comment|// nocommit can we wrap again?
 comment|// We can't wrap with "exotic" readers because points needs to work:
 name|IndexSearcher
 name|s
@@ -6321,8 +6320,6 @@ init|=
 name|newSearcher
 argument_list|(
 name|r
-argument_list|,
-literal|false
 argument_list|)
 decl_stmt|;
 specifier|final
