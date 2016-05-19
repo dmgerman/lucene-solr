b8a33769e79d852b804c15a6b10492768f0bc5a4
Alan Woodward <alan.woodward@romseysoftware.co.uk>
2016-05-19 14:49:22 +0100

    SOLR-9113: Fix re-registration race
    Former-commit-id: 6113e1f2fabf6668b4bdbd7640af45bebcc2e505

--- solr/solrj/src/java/org/apache/solr/common/cloud/ZkStateReader.java
+++ solr/solrj/src/java/org/apache/solr/common/cloud/ZkStateReader.java
@@ -6998,13 +6998,6 @@ operator|.
 name|stateWatchers
 argument_list|)
 expr_stmt|;
-name|v
-operator|.
-name|stateWatchers
-operator|.
-name|clear
-argument_list|()
-expr_stmt|;
 return|return
 name|v
 return|;
@@ -7029,11 +7022,9 @@ name|liveNodes
 argument_list|,
 name|collectionState
 argument_list|)
-operator|==
-literal|false
 condition|)
 block|{
-name|registerCollectionStateWatcher
+name|removeCollectionStateWatcher
 argument_list|(
 name|collection
 argument_list|,
