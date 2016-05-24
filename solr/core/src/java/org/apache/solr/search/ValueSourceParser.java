243badafb3ec5658f1d14816833b2c848b6fc7d5
Chris Hostetter <hossman@apache.org>
2016-05-24 15:45:52 -0700

    SOLR-9119: several static methods in ValueSourceParser have been made private
    Former-commit-id: e9f65fe0f8d2cf6fecdf5b3ba08adc71c0bc16be

--- solr/core/src/java/org/apache/solr/search/ValueSourceParser.java
+++ solr/core/src/java/org/apache/solr/search/ValueSourceParser.java
@@ -908,10 +908,29 @@ parameter_list|)
 throws|throws
 name|SyntaxError
 function_decl|;
-comment|/* standard functions */
+comment|/** standard functions supported by default, filled in static class initialization */
+DECL|field|standardVSParsers
+specifier|private
+specifier|static
+specifier|final
+name|Map
+argument_list|<
+name|String
+argument_list|,
+name|ValueSourceParser
+argument_list|>
+name|standardVSParsers
+init|=
+operator|new
+name|HashMap
+argument_list|<>
+argument_list|()
+decl_stmt|;
+comment|/** standard functions supported by default */
 DECL|field|standardValueSourceParsers
 specifier|public
 specifier|static
+specifier|final
 name|Map
 argument_list|<
 name|String
@@ -920,14 +939,16 @@ name|ValueSourceParser
 argument_list|>
 name|standardValueSourceParsers
 init|=
-operator|new
-name|HashMap
-argument_list|<>
-argument_list|()
+name|Collections
+operator|.
+name|unmodifiableMap
+argument_list|(
+name|standardVSParsers
+argument_list|)
 decl_stmt|;
 comment|/** Adds a new parser for the name and returns any existing one that was overridden.    *  This is not thread safe.    */
 DECL|method|addParser
-specifier|public
+specifier|private
 specifier|static
 name|ValueSourceParser
 name|addParser
@@ -940,7 +961,7 @@ name|p
 parameter_list|)
 block|{
 return|return
-name|standardValueSourceParsers
+name|standardVSParsers
 operator|.
 name|put
 argument_list|(
@@ -952,7 +973,7 @@ return|;
 block|}
 comment|/** Adds a new parser for the name and returns any existing one that was overridden.    *  This is not thread safe.    */
 DECL|method|addParser
-specifier|public
+specifier|private
 specifier|static
 name|ValueSourceParser
 name|addParser
@@ -962,7 +983,7 @@ name|p
 parameter_list|)
 block|{
 return|return
-name|standardValueSourceParsers
+name|standardVSParsers
 operator|.
 name|put
 argument_list|(
@@ -988,13 +1009,13 @@ name|String
 name|dest
 parameter_list|)
 block|{
-name|standardValueSourceParsers
+name|standardVSParsers
 operator|.
 name|put
 argument_list|(
 name|dest
 argument_list|,
-name|standardValueSourceParsers
+name|standardVSParsers
 operator|.
 name|get
 argument_list|(
