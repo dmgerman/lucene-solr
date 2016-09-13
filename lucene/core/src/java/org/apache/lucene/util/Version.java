6d02e29be0118aefe36944ca220835b494f6b941
Shalin Shekhar Mangar <shalin@apache.org>
2016-09-13 17:24:40 +0530

    Adding 6.2.1 version
    Former-commit-id: f670a2c837c30fc665b039fb77e5f03730947d5f

--- lucene/core/src/java/org/apache/lucene/util/Version.java
+++ lucene/core/src/java/org/apache/lucene/util/Version.java
@@ -127,6 +127,26 @@ argument_list|,
 literal|0
 argument_list|)
 decl_stmt|;
+comment|/**    * Match settings and bugs in Lucene's 6.2.1 release.    * @deprecated Use latest    */
+annotation|@
+name|Deprecated
+DECL|field|LUCENE_6_2_1
+specifier|public
+specifier|static
+specifier|final
+name|Version
+name|LUCENE_6_2_1
+init|=
+operator|new
+name|Version
+argument_list|(
+literal|6
+argument_list|,
+literal|2
+argument_list|,
+literal|1
+argument_list|)
+decl_stmt|;
 comment|/**    * Match settings and bugs in Lucene's 6.3.0 release.    * @deprecated Use latest    */
 annotation|@
 name|Deprecated
