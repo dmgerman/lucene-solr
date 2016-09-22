12ba2c575385cd06bac7b50cacbf1a10fb554bfa
Noble Paul <noble@apache.org>
2016-09-22 15:32:24 +0530

    SOLR-8995: Use lamdas in  URPs
    Former-commit-id: 262a3ac5c5667be56486977bf70af281acd76129

--- solr/core/src/java/org/apache/solr/update/processor/ParseBooleanFieldUpdateProcessorFactory.java
+++ solr/core/src/java/org/apache/solr/update/processor/ParseBooleanFieldUpdateProcessorFactory.java
@@ -70,20 +70,6 @@ end_import
 
 begin_import
 import|import
-name|com
-operator|.
-name|google
-operator|.
-name|common
-operator|.
-name|collect
-operator|.
-name|ImmutableSet
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
