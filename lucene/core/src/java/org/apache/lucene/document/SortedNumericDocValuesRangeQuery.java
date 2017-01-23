96aa25aac8f42b3ce0eb37e74dd48d23fb73bd1a
Adrien Grand <jpountz@gmail.com>
2017-01-23 10:31:44 +0100

    LUCENE-7643,SOLR-10013: Reenable the single-value optimization.
    Former-commit-id: 6693c261e5782bc49dea92002745a91215c4166e

--- lucene/core/src/java/org/apache/lucene/document/SortedNumericDocValuesRangeQuery.java
+++ lucene/core/src/java/org/apache/lucene/document/SortedNumericDocValuesRangeQuery.java
@@ -46,6 +46,20 @@ name|lucene
 operator|.
 name|index
 operator|.
+name|DocValues
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|lucene
+operator|.
+name|index
+operator|.
 name|IndexReader
 import|;
 end_import
@@ -600,10 +614,13 @@ specifier|final
 name|NumericDocValues
 name|singleton
 init|=
-literal|null
+name|DocValues
+operator|.
+name|unwrapSingleton
+argument_list|(
+name|values
+argument_list|)
 decl_stmt|;
-comment|// TODO: LUCENE-7649, re-consider optimization that broke SOLR-10013
-comment|// final NumericDocValues singleton = DocValues.unwrapSingleton(values);
 specifier|final
 name|TwoPhaseIterator
 name|iterator
@@ -615,11 +632,6 @@ operator|!=
 literal|null
 condition|)
 block|{
-assert|assert
-literal|false
-operator|:
-literal|"imposible code -- or: someone re-enabled singleton optinization w/o reading the whole method"
-assert|;
 name|iterator
 operator|=
 operator|new
