eacca79e3bf7774707b51ad6517e51ffe5356362
Christine Poerschke <cpoerschke@apache.org>
2017-02-09 14:55:06 +0000

    SOLR-9912: Add facet.excludeTerms parameter support. (Jonny Marks, David Smiley, Christine Poerschke)
    Former-commit-id: 0b817e6e495c40496b7cedc6f06060e43e5e2afc

--- solr/solrj/src/java/org/apache/solr/common/params/FacetParams.java
+++ solr/solrj/src/java/org/apache/solr/common/params/FacetParams.java
@@ -385,6 +385,18 @@ name|FACET_CONTAINS
 operator|+
 literal|".ignoreCase"
 decl_stmt|;
+comment|/**    * Only return constraints of a facet field excluding the given string.    */
+DECL|field|FACET_EXCLUDETERMS
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|FACET_EXCLUDETERMS
+init|=
+name|FACET
+operator|+
+literal|".excludeTerms"
+decl_stmt|;
 comment|/**    * When faceting by enumerating the terms in a field,    * only use the filterCache for terms with a df&gt;= to this parameter.    */
 DECL|field|FACET_ENUM_CACHE_MINDF
 specifier|public
