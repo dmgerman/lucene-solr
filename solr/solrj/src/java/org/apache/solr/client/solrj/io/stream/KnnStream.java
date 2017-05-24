db3707f63e6e8f0b6096107acb775c0e762ef741
Joel Bernstein <jbernste@apache.org>
2017-05-24 08:06:01 -0400

    SOLR-10731: Fix precommit
    Former-commit-id: f4872c9296b509951a30e9d9618a3730f44bbefb

--- solr/solrj/src/java/org/apache/solr/client/solrj/io/stream/KnnStream.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/io/stream/KnnStream.java
@@ -110,16 +110,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|Set
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|stream
 operator|.
 name|Collectors
